13c80cbff53bcd82ce9a9c4e4b41abb0c5835a41
Robert Scott <rbscott@untangle.com>
2005-03-28 23:49:34 +0000

    Merging release with last minute changes from release 1.2 36:62
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@63 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -2,8 +2,8 @@
 
 sudo rsync -rlptvz -e ssh /var/www/metavize root@www.metavize.com:/var/www.release/
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release.sh root@www.metavize.com:~/
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-transform_*"
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-transform_*"
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-transform_*"
-ssh www.metavize.com -lroot "~/deb-scan-release.sh /var/www.release/metavize "
+ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-*"
+ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-*"
+ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-*"
+ssh www.metavize.com -lroot "sh ~/deb-scan-release.sh /var/www.release/metavize "
 
--- scripts/push-trial.sh
+++ scripts/push-trial.sh
@@ -4,5 +4,5 @@ sudo rsync -rlptvz -e ssh /var/www/metavize root@www.metavize.com:/var/www.trial
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-trial.sh root@www.metavize.com:~/
 ssh www.metavize.com -lroot "rm /var/www.trial/metavize/pool/metavize/t/test-transform_*"
 ssh www.metavize.com -lroot "rm /var/www.trial/metavize/pool/metavize/f/fprot-transform_*"
-ssh www.metavize.com -lroot "~/deb-scan-trial.sh /var/www.trial/metavize "
+ssh www.metavize.com -lroot "sh ~/deb-scan-trial.sh /var/www.trial/metavize "
 
