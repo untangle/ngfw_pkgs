6a98368bf3d40759d8ec4bead6e8514589aa3c9e
John Irwin <jdi@untangle.com>
2005-04-07 04:05:15 +0000

    added clam
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@220 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -3,6 +3,7 @@
 sudo rsync -rlptvz -e ssh /var/www/metavize root@release.metavize.com:/var/www.release/
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release.sh root@release.metavize.com:~/
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-*"
+ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/c/clam-*"
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-*"
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-*"
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/v/virus-transform_*"
