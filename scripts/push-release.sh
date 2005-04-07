b7bb733b14ab77e56552e1f06df4c9db862ed89b
John Irwin <jdi@untangle.com>
2005-04-07 03:36:20 +0000

    push to bebe
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@218 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-sudo rsync -rlptvz -e ssh /var/www/metavize root@www.metavize.com:/var/www.release/
-scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release.sh root@www.metavize.com:~/
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-*"
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-*"
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-*"
-ssh www.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/v/virus-transform_*"
-ssh www.metavize.com -lroot "sh ~/deb-scan-release.sh /var/www.release/metavize "
+sudo rsync -rlptvz -e ssh /var/www/metavize root@release.metavize.com:/var/www.release/
+scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release.sh root@release.metavize.com:~/
+ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-*"
+ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-*"
+ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-*"
+ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/v/virus-transform_*"
+ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/k/kernel-dev*"
+ssh release.metavize.com -lroot "sh ~/deb-scan-release.sh /var/www.release/metavize "
 
