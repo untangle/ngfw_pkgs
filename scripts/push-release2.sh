642ba78245a19b5785ee59f513f7a771905f246c
Dirk Morris <dmorris@untangle.com>
2005-05-06 00:24:45 +0000

    clam is legit
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@617 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release2.sh
+++ scripts/push-release2.sh
@@ -3,7 +3,6 @@
 sudo rsync -rlptvz -e ssh /var/www/metavize root@release2.metavize.com:/var/www.release2/
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release2.sh root@release2.metavize.com:~/
 ssh release2.metavize.com -lroot "rm /var/www.release2/metavize/pool/metavize/t/test-*"
-ssh release2.metavize.com -lroot "rm /var/www.release2/metavize/pool/metavize/c/clam-*"
 ssh release2.metavize.com -lroot "rm /var/www.release2/metavize/pool/metavize/f/fprot-*"
 ssh release2.metavize.com -lroot "rm /var/www.release2/metavize/pool/metavize/s/sophos-*"
 ssh release2.metavize.com -lroot "rm /var/www.release2/metavize/pool/metavize/v/virus-transform_*" &>/dev/null
