830ccfad3f1fd0c015e6a38a4eda5fffb8e5fd38
Dirk Morris <dmorris@untangle.com>
2005-05-10 01:07:52 +0000

    fixes
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@644 8ebf8b4e-06f3-0310-961f-89f74618be54

--- pump-mv/files/usr/bin/mvvm-pump.sh
+++ pump-mv/files/usr/bin/mvvm-pump.sh
@@ -3,22 +3,31 @@
 mkdir -p /var/log/mvvm &> /dev/null
 
 if [ "$1" = "up" ] ; then
-    echo "mvvm-pump.sh: Interface Up. Restarting" >> /var/log/mvvm/wrapper.log 2>&1
-    if [ -f @PREFIX@/etc/init.d/mvvm ] ; then 
-        @PREFIX@/etc/init.d/mvvm restart
+    if [ -f /tmp/fakemode-restart ] ; then
+        rm -f /tmp/fakemode-restart
+        echo "mvvm-pump.sh: Interface Up. Restarting" >> /var/log/mvvm/wrapper.log 2>&1
+        if [ -f /etc/init.d/mvvm ] ; then 
+            /etc/init.d/mvvm restart
+        fi
+    else
+        echo "mvvm-pump.sh: Interface Up. mcli updateAddress" >> /var/log/mvvm/wrapper.log 2>&1
+        if [ -f /etc/init.d/mvvm ] ; then 
+            /usr/bin/mcli updateAddress &> /dev/null &
+        fi
     fi
 fi
 
 if [ "$1" = "renewal" ] ; then
     echo "mvvm-pump.sh: DHCP Renewal. mcli updateAddress" >> /var/log/mvvm/wrapper.log 2>&1
-    if [ -f @PREFIX@/usr/bin/mcli ] ; then 
-        @PREFIX@/usr/bin/mcli updateAddress &> /dev/null &
+    if [ -f /usr/bin/mcli ] ; then 
+        /usr/bin/mcli updateAddress &> /dev/null &
     fi
 fi
 
 if [ "$1" = "down" ] ; then
     echo "mvvm-pump.sh: Interface Down. Restarting" >> /var/log/mvvm/wrapper.log 2>&1
-    if [ -f @PREFIX@/etc/init.d/mvvm ] ; then 
-        @PREFIX@/etc/init.d/mvvm restart
+    if [ -f /etc/init.d/mvvm ] ; then 
+        /etc/init.d/mvvm restart
+        touch /tmp/fakemode-restart
     fi
 fi
