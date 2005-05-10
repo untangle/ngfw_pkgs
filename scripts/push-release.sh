ded658061db35cea77296f9b1d8dc1d5792ae0e3
Dirk Morris <dmorris@untangle.com>
2005-05-10 23:51:38 +0000

    cleaned
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@662 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -3,10 +3,8 @@
 sudo rsync -rlptvz -e ssh /var/www/metavize root@release.metavize.com:/var/www.release/
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release.sh root@release.metavize.com:~/
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-*"
-ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/c/clam-*"
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-*"
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-*"
-ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/v/virus-transform_*" &>/dev/null
 ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/k/kernel-dev*"
 ssh release.metavize.com -lroot "sh ~/deb-scan-release.sh /var/www.release/metavize "
 
