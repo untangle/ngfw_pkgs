2c150666d3b3e2c9be5ad007f1b5dae2c2897a43
Dirk Morris <dmorris@untangle.com>
2005-05-10 02:52:11 +0000

    cleaned, released
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@652 8ebf8b4e-06f3-0310-961f-89f74618be54

--- pump-mv/files/usr/bin/mvvm-pump.sh
+++ pump-mv/files/usr/bin/mvvm-pump.sh
@@ -1,33 +1,52 @@
 #!/bin/sh
 
+#
+# Error checking
+#
 mkdir -p /var/log/mvvm &> /dev/null
-
-if [ "$1" = "up" ] ; then
-    if [ -f /tmp/fakemode-restart ] ; then
-        rm -f /tmp/fakemode-restart
-        echo "mvvm-pump.sh: Interface Up. Restarting" >> /var/log/mvvm/wrapper.log 2>&1
-        if [ -f /etc/init.d/mvvm ] ; then 
-            /etc/init.d/mvvm restart
-        fi
-    else
-        echo "mvvm-pump.sh: Interface Up. mcli updateAddress" >> /var/log/mvvm/wrapper.log 2>&1
-        if [ -f /etc/init.d/mvvm ] ; then 
-            /usr/bin/mcli updateAddress &> /dev/null &
-        fi
-    fi
+if [ ! -f /etc/init.d/mvvm ] ; then 
+    echo "mvvm-pump.sh: /etc/init.d/mvvm missing (`date`)" >> /var/log/mvvm/wrapper.log 2>&1
+    exit 1
+fi
+if [ ! -f /usr/bin/mcli ] ; then 
+    echo "mvvm-pump.sh: /usr/bin/mcli missing (`date`)" >> /var/log/mvvm/wrapper.log 2>&1
+    exit 1
 fi
 
-if [ "$1" = "renewal" ] ; then
-    echo "mvvm-pump.sh: DHCP Renewal. mcli updateAddress" >> /var/log/mvvm/wrapper.log 2>&1
-    if [ -f /usr/bin/mcli ] ; then 
+
+#
+# Up script
+# If in fake mode, restart
+# Otherwise just update address
+#
+if [ "$1" = "up" ] ; then
+    if [ -f /tmp/fakemode ] ; then
+        rm -f /tmp/fakemode
+        echo "mvvm-pump.sh: Interface Up. Restarting (`date`)" >> /var/log/mvvm/wrapper.log 2>&1
+        /etc/init.d/mvvm restart
+    else
+        echo "mvvm-pump.sh: Interface Up. mcli updateAddress (`date`)" >> /var/log/mvvm/wrapper.log 2>&1
         /usr/bin/mcli updateAddress &> /dev/null &
     fi
 fi
 
+#
+# Renewal Script
+# Tell mvvm of new address
+#
+if [ "$1" = "renewal" ] ; then
+    echo "mvvm-pump.sh: DHCP Renewal. mcli updateAddress (`date`)" >> /var/log/mvvm/wrapper.log 2>&1
+    /usr/bin/mcli updateAddress &> /dev/null &
+fi
+
+#
+# Down Script
+# if already down (in fake mode) do nothing, otherwise restart (go into fakemode)
+# 
 if [ "$1" = "down" ] ; then
-    echo "mvvm-pump.sh: Interface Down. Restarting" >> /var/log/mvvm/wrapper.log 2>&1
-    if [ -f /etc/init.d/mvvm ] ; then 
+    if [ ! -f /tmp/fakemode ] ; then
+        echo "mvvm-pump.sh: Interface Down. Restarting (`date`)" >> /var/log/mvvm/wrapper.log 2>&1
         /etc/init.d/mvvm restart
-        touch /tmp/fakemode-restart
+        touch /tmp/fakemode
     fi
 fi
