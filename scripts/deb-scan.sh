0de05bb75067a50c6f5b7f86f99bf32b5eaf9264
Dirk Morris <dmorris@untangle.com>
2005-06-21 00:56:13 +0000

    rearranging
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@1093 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/deb-scan.sh
+++ scripts/deb-scan.sh
@@ -1,13 +1,10 @@
 #! /bin/sh
 
-if [ -z $DEBDEST ] ; then
-    DIR=metavize
-else
-    DIR=$DEBDEST
+if [ $# -lt 1 ] ; then
+        usage: "$0 <dir ex: /var/www/metavize>"
 fi
 
-DESTINATION=/var/www/$DIR
-echo $DESTINATION
+DESTINATION=$1
 DISTRIBUTION=testing
 COMPONENT=metavize
 OVERRIDE=override.${DISTRIBUTION}.${COMPONENT}
@@ -20,13 +17,13 @@ cp -a `dirname $0`/${OVERRIDE} ${DESTINATION}/indices/
 #
 cd ${DESTINATION}
 
-echo "Building package list..."
+echo "Building package list for \"${DESTINATION}\""
 
 dpkg-scanpackages pool/${COMPONENT} indices/${OVERRIDE} > \
 	dists/${DISTRIBUTION}/${COMPONENT}/binary-${ARCH}/Packages 2> /tmp/out 
 if [ $? -ne 0 ] ; then exit ; fi
 
-grep -v "newer version" /tmp/out | grep -v "ignored data" | grep -v "is repeat"
+grep -v "newer version" /tmp/out | grep -v "ignored" | grep -v "is repeat"
 
 cat dists/${DISTRIBUTION}/${COMPONENT}/binary-${ARCH}/Packages | \
 	gzip -9 > dists/${DISTRIBUTION}/${COMPONENT}/binary-${ARCH}/Packages.gz
---
+++ scripts/push-release-alpha.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo rsync -rlptvz -e ssh /var/www/metavize root@release-alpha.metavize.com:/var/www.release-alpha/
+scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan-release-alpha.sh root@release-alpha.metavize.com:~/
+ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/t/test-*"
+ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/f/fprot-*"
+ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/s/sophos-*"
+ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/v/virus-transform_*" &>/dev/null
+ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/k/kernel-dev*"
+ssh release-alpha.metavize.com -lroot "sh ~/deb-scan.sh /var/www.release-alpha/metavize "
+
