0612d48dd4f8a5029ee82fa0ee8012d046d43c67
Dirk Morris <dmorris@untangle.com>
2005-06-27 00:47:02 +0000

    incremented pubversion
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@1178 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release-alpha.sh
+++ scripts/push-release-alpha.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-sudo rsync -rlptvz -e ssh /var/www/metavize root@release-alpha.metavize.com:/var/www.release-alpha/
+sudo rsync -rlptvz -e ssh /var/www/metavize \
+    --exclude 'spam-*' \
+    --exclude 'test-*' \
+    --exclude 'fprot-*' \
+    --exclude 'sophos-*' \
+    --exclude 'virus-transform*' \
+    --exclude 'spam-*' \
+    --exclude 'kernel-dev*' \
+    root@release-alpha.metavize.com:/var/www.release-alpha/
+
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan.sh root@release-alpha.metavize.com:~/
-ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/s/spam-*"
-ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/t/test-*"
-ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/f/fprot-*"
-ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/s/sophos-*"
-ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/v/virus-transform_*" &>/dev/null
-ssh release-alpha.metavize.com -lroot "rm /var/www.release-alpha/metavize/pool/metavize/k/kernel-dev*"
+
+ssh release-alpha.metavize.com -lroot "rm -f /var/www.release-alpha/metavize/pool/metavize/s/spam-*"
+ssh release-alpha.metavize.com -lroot "rm -f /var/www.release-alpha/metavize/pool/metavize/t/test-*"
+ssh release-alpha.metavize.com -lroot "rm -f /var/www.release-alpha/metavize/pool/metavize/f/fprot-*"
+ssh release-alpha.metavize.com -lroot "rm -f /var/www.release-alpha/metavize/pool/metavize/s/sophos-*"
+ssh release-alpha.metavize.com -lroot "rm -f /var/www.release-alpha/metavize/pool/metavize/v/virus-transform*"
+ssh release-alpha.metavize.com -lroot "rm -f /var/www.release-alpha/metavize/pool/metavize/k/kernel-dev*"
 ssh release-alpha.metavize.com -lroot "sh ~/deb-scan.sh /var/www.release-alpha/metavize "
 
--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-sudo rsync -rlptvz -e ssh /var/www/metavize root@release.metavize.com:/var/www.release/
+sudo rsync -rlptvz -e ssh /var/www/metavize \
+    --exclude 'spam-*' \
+    --exclude 'test-*' \
+    --exclude 'fprot-*' \
+    --exclude 'sophos-*' \
+    --exclude 'virus-transform*' \
+    --exclude 'spam-*' \
+    --exclude 'kernel-dev*' \
+    root@release.metavize.com:/var/www.release/
+
 scp ~/work/pkgs/scripts/override.testing.metavize ~/work/pkgs/scripts/deb-scan.sh root@release.metavize.com:~/
-ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/t/test-*"
-ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/f/fprot-*"
-ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/s/sophos-*"
-ssh release.metavize.com -lroot "rm /var/www.release/metavize/pool/metavize/k/kernel-dev*"
+ssh release.metavize.com -lroot "rm -f /var/www.release/metavize/pool/metavize/s/spam-*"
+ssh release.metavize.com -lroot "rm -f /var/www.release/metavize/pool/metavize/t/test-*"
+ssh release.metavize.com -lroot "rm -f /var/www.release/metavize/pool/metavize/f/fprot-*"
+ssh release.metavize.com -lroot "rm -f /var/www.release/metavize/pool/metavize/s/sophos-*"
+ssh release.metavize.com -lroot "rm -f /var/www.release/metavize/pool/metavize/v/virus-transform*"
+ssh release.metavize.com -lroot "rm -f /var/www.release/metavize/pool/metavize/k/kernel-dev*"
 ssh release.metavize.com -lroot "sh ~/deb-scan.sh /var/www.release/metavize "
 
