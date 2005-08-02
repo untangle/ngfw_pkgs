9fd6fdfb169ac4f66be852f617015fbff6db3f7d
Dirk Morris <dmorris@untangle.com>
2005-08-02 03:56:28 +0000

    added arg check
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/alpine/pkgs@1752 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $# -lt 2 ] ; then
+    echo "Usage: push.sh /var/www/dmorris /var/www/metavize"
+    exit 2
+fi
+
 echo "-------------------------"
 sudo rsync -rlptvz $1 $2
 echo "-------------------------"
