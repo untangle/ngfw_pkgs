fd55ef3e01743d9457c3cf30bd8594f501186e44
John Irwin <jdi@untangle.com>
2005-08-11 21:21:42 +0000

    fix bug 722
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/alpine/release-2.5/pkgs@2052 8ebf8b4e-06f3-0310-961f-89f74618be54

--- kiosk-mv/files/home/kiosk/utils/screensaver-off.sh
+++ kiosk-mv/files/home/kiosk/utils/screensaver-off.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 xset s off
+xset -dpms
 
 sed -i "s/xset s.*/xset s off/" /home/kiosk/.xinitrc
+sed -i "s/xset .dpms/xset -dpms/" /home/kiosk/.xinitrc
--- kiosk-mv/files/home/kiosk/utils/screensaver-on.sh
+++ kiosk-mv/files/home/kiosk/utils/screensaver-on.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 xset s on
+xset +dpms
 
 sed -i "s/xset s.*/xset s on/" /home/kiosk/.xinitrc
+sed -i "s/xset .dpms/xset +dpms/" /home/kiosk/.xinitrc
