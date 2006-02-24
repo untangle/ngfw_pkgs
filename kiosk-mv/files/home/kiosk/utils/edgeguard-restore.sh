d71415579e4303ba78e9b3de417d5acac184a59a
John Irwin <jdi@untangle.com>
2006-02-24 21:51:34 +0000

    make it essential
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@4861 8ebf8b4e-06f3-0310-961f-89f74618be54

--- kiosk-mv/files/home/kiosk/utils/edgeguard-restore.sh
+++ kiosk-mv/files/home/kiosk/utils/edgeguard-restore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Xdialog --default-no --wrap --title "Restore" --yesno "Are you sure want to run Recovery? It will stop Edgeguard services." 15 60
+Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop Edgeguard services." 15 60
 
 if [ $? -eq 0 ]; then
     xterm -e "sudo /usr/bin/edgeguard-restore"
