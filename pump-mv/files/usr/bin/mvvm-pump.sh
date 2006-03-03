5e15ee7eb806172293ec1839b8a56644d9dc158f
Robert Scott <rbscott@untangle.com>
2006-03-03 02:20:06 +0000

    fixing to restart reload the interface on down(for some reason pump doesn't do this by default
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@4994 8ebf8b4e-06f3-0310-961f-89f74618be54

--- pump-mv/files/usr/bin/mvvm-pump.sh
+++ pump-mv/files/usr/bin/mvvm-pump.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+BOGUS_ADDRESS_BASE="169.254.210.50"
+BOGUS_NETMASK="255.255.0.0"
+
+## DHCP auto renew script, used to kill any processes lingering around
+## This is hardcoded, so if it changes, it must be updated in the other location.
+DHCP_AUTO_RENEW="/usr/share/metavize/networking/dhcp-auto-renew"
+
+INTERFACE=$2
+
 #
 # Error checking
 #
@@ -14,7 +23,7 @@ fi
 # If in fake mode, restart
 # Otherwise just update address
 #
-if [ "$1" = "up" ] ; then
+if [ "x$1" = "xup" ] ; then
     echo "mvvm-pump.sh: Interface Up. mcli updateAddress (`date`)" >> /var/log/mvvm/iptables.log 2>&1
     /usr/bin/mcli updateAddress >> /var/log/mvvm/iptables.log 2>&1 &
 fi
@@ -23,7 +32,7 @@ fi
 # Renewal Script
 # Tell mvvm of new address
 #
-if [ "$1" = "renewal" ] ; then
+if [ "x$1" = "xrenewal" ] ; then
     echo "mvvm-pump.sh: DHCP Renewal. mcli updateAddress (`date`)" >> /var/log/mvvm/iptables.log 2>&1
     /usr/bin/mcli updateAddress >> /var/log/mvvm/iptables.log 2>&1 &
 fi
@@ -32,7 +41,23 @@ fi
 # Down Script
 # if already down (in fake mode) do nothing, otherwise restart (go into fakemode)
 # 
-if [ "$1" = "down" ] ; then
+if [ "x$1" = "xdown" ] ; then
     echo "mvvm-pump.sh: Interface Down. Restarting (`date`)" >> /var/log/mvvm/iptables.log 2>&1
+    
+    ## If it fails, bring the interface up with a bogus address
+    if [ -n "${INTERFACE}" ]; then
+        ifconfig ${INTERFACE} ${BOGUS_ADDRESS_BASE} netmask ${BOGUS_NETMASK}
+    else
+        echo "mvvm-pump.sh: Interface is not set, unable to set to bogus address" \
+            >> /var/log/mvvm/iptables.log 2>&1
+    fi
+
     /usr/bin/mcli updateAddress >> /var/log/mvvm/iptables.log 2>&1 &
+    
+    if [ -f ${DHCP_AUTO_RENEW} ] && [ -n "${INTERFACE}" ]; then
+        nohup sh ${DHCP_AUTO_RENEW} ${INTERFACE} &
+    else
+        echo "mvvm-pump.sh: Auto renew script doesn't exist or interface is unset, exiting" \
+            >> /var/log/mvvm/iptables.log 2>&1
+    fi
 fi
