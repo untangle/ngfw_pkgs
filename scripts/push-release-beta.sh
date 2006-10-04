3f207a3e104605fd49c50ef4c669e88204daf78c
Dirk Morris <dmorris@untangle.com>
2006-10-04 19:51:11 +0000

    rename
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@7374 8ebf8b4e-06f3-0310-961f-89f74618be54

--- scripts/push-release-beta.sh
+++ scripts/push-release-beta.sh
@@ -15,19 +15,19 @@ sudo rsync -rlpvz -e ssh /var/www/stable/ \
     --exclude 'kav-*' \
     --exclude 'Packages' \
     --exclude 'Packages.gz' \
-    root@release-alpha.metavize.com:/var/www.release-alpha/metavize
+    root@release.metavize.com:/var/www/beta
 
 scp \
     ~/work/pkgs/scripts/override.testing.metavize \
     ~/work/pkgs/scripts/deb-scan.sh  \
     ~/work/pkgs/scripts/clean-packages.sh \
-    root@release-alpha.metavize.com:~/
+    root@release.metavize.com:~/
 
 # Cleaning is bad.  Very very bad.  Clean dogfood first, but leave release-alpha full of
 # packages.
 #echo -e "\n\nCleaning...\n\n"
-#ssh release-alpha.metavize.com -lroot "sh ~/clean-packages.sh /var/www.release-alpha/metavize 3 delete"
+#ssh release.metavize.com -lroot "sh ~/clean-packages.sh /var/www.release/metavize 3 delete"
 
 echo -e "\n\nBuilding Package List...\n\n"
-ssh release-alpha.metavize.com -lroot "sh ~/deb-scan.sh /var/www.release-alpha/metavize "
+ssh release.metavize.com -lroot "sh ~/deb-scan.sh /var/www/beta"
 
