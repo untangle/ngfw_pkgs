0ae6c4f9b537811a38613645a4b99e74d4ad4fcb
Aaron Read <amread@untangle.com>
2006-10-31 20:50:49 +0000

    untanglenetworks.com -> untangle.com
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@7741 8ebf8b4e-06f3-0310-961f-89f74618be54

--- kiosk-mv/files/home/kiosk/utils/edgeguard-off.sh
+++ kiosk-mv/files/home/kiosk/utils/edgeguard-off.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Xdialog --default-no --wrap --title "Power off" --yesno "Are you sure you want to turn off Metavize EdgeGuard?" 15 60
+Xdialog --default-no --wrap --title "Power off" --yesno "Are you sure you want to turn off the Untangle Server?" 15 60
 
 if [ $? -eq 0 ]; then
         sudo shutdown -h now
