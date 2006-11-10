48fc05e9893326eeb369c0bd663e027675b1f573
Dirk Morris <dmorris@untangle.com>
2006-11-10 01:43:02 +0000

    text fix
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@8022 8ebf8b4e-06f3-0310-961f-89f74618be54

--- kiosk-mv/files/home/kiosk/utils/edgeguard-reboot.sh
+++ kiosk-mv/files/home/kiosk/utils/edgeguard-reboot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Xdialog --default-no --wrap --title "Reboot" --yesno "Are you sure you want to reboot Untangle Server?" 15 60
+Xdialog --default-no --wrap --title "Reboot" --yesno "Are you sure you want to reboot the Untangle Server?" 15 60
 
 if [ $? -eq 0 ]; then
         sudo /sbin/reboot
