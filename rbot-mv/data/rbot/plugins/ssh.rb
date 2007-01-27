f67848eeeb42ea9da96112d27a3fc931fddcdfe3
Sebastien Delafond <sdelafond@gmail.com>
2007-01-27 01:10:09 +0000

    Tear down the forwarding channel when an SSH connection ends.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@8662 8ebf8b4e-06f3-0310-961f-89f74618be54

--- rbot-mv/data/rbot/plugins/ssh.rb
+++ rbot-mv/data/rbot/plugins/ssh.rb
@@ -1,5 +1,6 @@
 require 'net/ssh'
 require 'net/ssh/util/prompter'
+require 'net/ssh/service/forward/remote-network-handler'
 require 'net/ssh/transport/ossl/key-factory'
 
 class Net::SSH::Util::Prompter
@@ -8,14 +9,35 @@ class Net::SSH::Util::Prompter
 
   @@passphrase = ""
 
-  class << self
-    def passphrase=(passphrase)
-      @@passphrase = passphrase
-    end
+  def self.passphrase=(passphrase)
+    @@passphrase = passphrase
   end
 
   def password(prompt)
     return @@passphrase
+  end
+end
+
+class Net::SSH::Service::Forward::RemoteNetworkHandler
+  # We redefine the behavior of .setup so that we can inform
+  # the user when the channel has actually been established
+  # and only then
+
+  @@m = nil
+  @@sshplugin = nil
+
+  def self.init(p, m) # class method
+    @@sshplugin = p
+    @@m = m
+  end
+
+  def setup(remote_port)
+    @@m.reply "Forwarding channel established on port #{remote_port}"
+  end
+
+  def on_close( channel )
+    @@m.reply "SSH connection closed -> tearing down the forwarding channel"
+    @@sshplugin.close
   end
 end
 
@@ -37,13 +59,13 @@ class SSHPlugin < Plugin
   end
 
   def pickRandomPort
-#    @port = 1025 + rand(10000) # a random port to use for forwarding
-    @port = 1234
+    @port = 1025 + rand(10000) # a random port to use for forwarding
+#    @port = 1234
     @portAttempts += 1
   end
 
   def enable(m, params)
-    @m = m # FIXME? so handleException has access to it
+    @m = m
 
     if @isEstablished
       m.reply "The forwarding channel is already enabled on port #{@port}" 
@@ -51,25 +73,26 @@ class SSHPlugin < Plugin
     end
 
     Net::SSH::Util::Prompter.passphrase = params[:passphrase]
+    # FIXME: find a better design pattern than this crap !!!
+    Net::SSH::Service::Forward::RemoteNetworkHandler.init self, m
 
     @thread = Thread.new do # start the forwarded channel in a thread
       begin
         pickRandomPort
 
         Net::SSH.start(@host, @user,
-  #                     :log => "/tmp/foo", # FIXME
-  #                     :verbose => :info,
                        :auth_methods => [ "publickey" ],
                        :keys => [ @key ] ) do |@session|
-
-          # "0.0.0.0" is for not binding only on lo, so support can
+          # "0.0.0.0" is for binding on all interfaces, so support can
           # use the forwarded channel from any box on the Untangle
           # network
           @session.forward.remote_to(22, 'localhost', @port, '0.0.0.0')
 
-          # FIXME: move to a callback functions, like on_success or something
-          @m.reply "Forwarding channel established on port #{@port}"
+          # At this point maybe the forwarding channel isn't
+          # established, but if it's the case we'll hit an exception
+          # and set @isEstablished  back to false
           @isEstablished = true
+          @portAttempts = 0
 
           # next line doesn't work as advertised in Net::SSH doc
           # @session.loop { !@needToStop }
@@ -77,56 +100,60 @@ class SSHPlugin < Plugin
         end
       rescue OpenSSL::PKey::RSAError => e
         if e.message =~ /Neither PUB key nor PRIV key/
-          @m.reply "Forwarding channel not setup: invalid passphrase"
+          m.reply "Forwarding channel not setup: invalid passphrase"
         else
-          handleException e
+          handleException m, e
         end
       rescue Net::SSH::AuthenticationFailed => e
         if e.message =~ /#{@user}/
-          @m.reply "Forwarding channel not setup: could not find SSH key"
+          m.reply "Forwarding channel not setup: could not find SSH key"
         else
-          handleException e
+          handleException m, e
         end
       rescue Net::SSH::Exception => e
         if e.message =~ /closed by remote host/
-          @m.reply "Forwarding channel closed on port #{@port}"
+          m.reply "Forwarding channel closed on port #{@port}"
         elsif e.message =~ /remote port #{@port} could not be forwarded to local host/
           while @portAttempts < 10
             retry
           end
-          @m.reply "Could not find a free port to setup forwarding channel on, giving up"
+          m.reply "Could not find a free port to setup forwarding channel on, giving up"
         else
-          handleException e
+          handleException m, e
         end
       rescue Exception => e
-        handleException e
+        handleException m, e
       ensure
         begin
           @session.close
         rescue # do nothing...
         end
         @isEstablished = false
+        @portAttempts = 0
       end
     end
+  end
+
+  def close
+    @session.close
   end
 
   def disable(m, params)
     if @isEstablished
       m.reply "Received request to disable forwarding channel on port #{@port}"
 #      @needToStop = true
-      @session.close
-      @thread.join
+      close
     else
       m.reply "Forwarding channel is not established, ignoring request"
     end
   end
 
-  def handleException(e)
-    @m.reply "An exception happened: #{e.class} -> #{e.message}"
+  def handleException(m, e)
+    m.reply "An exception happened: #{e.class} -> #{e.message}"
      e.backtrace.each { |line|
-       @m.reply "  #{line}"
+       m.reply "  #{line}"
      }
-    @m.reply "End of exception backtrace"
+    m.reply "End of exception backtrace"
   end
 
   def help(plugin, topic="")
