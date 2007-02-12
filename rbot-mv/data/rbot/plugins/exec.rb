85ef226387082a207cc2e02a273db11b5ea8d4e1
Dirk Morris <dmorris@untangle.com>
2007-02-12 21:59:10 +0000

    Added support for join_on_test
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@8865 8ebf8b4e-06f3-0310-961f-89f74618be54

--- rbot-mv/data/rbot/plugins/exec.rb
+++ rbot-mv/data/rbot/plugins/exec.rb
@@ -4,17 +4,35 @@ class ExecPlugin < Plugin
     super()
   end
 
+  def runSystemCommand(command)
+    output = `sudo su -c "#{command}" 2>&1`
+    rc = $? >> 8
+    return output, rc
+  end
+
   def exec(m, params)
     begin
       command = params[:command].join(" ")
-      output = `sudo su -c "#{command}" 2>&1`
-      rc = $? >> 8
+      output, rc = runSystemCommand(command)
       m.reply output
       m.reply "RC = #{rc}"
     rescue Exception => e
       handleException(m, e)
     end
   end
+
+  def joinOnTest(m, params)
+    begin
+      command = params[:command].join(" ")
+      output, rc = runSystemCommand(command)
+      if rc == 0
+        @bot.join(params[:channel])
+      end
+    rescue Exception => e
+      handleException(m, e)
+    end
+  end
+
 
   def handleException(m, e)
     m.reply "An exception happened: #{e.class} -> #{e.message}"
@@ -27,6 +45,7 @@ class ExecPlugin < Plugin
   def help(plugin, topic="")
     <<-eos
       ssh exec *command => Run command
+      ssh join_on_test #channel *command => if command succeeds, join #channel
     eos
   end
 
@@ -34,3 +53,4 @@ end
 
 plugin = ExecPlugin.new
 plugin.map 'exec *command', :action => 'exec'
+plugin.map 'join_on_test :channel *command', :action => 'joinOnTest'
