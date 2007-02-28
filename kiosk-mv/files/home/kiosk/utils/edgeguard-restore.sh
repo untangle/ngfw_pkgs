992f51341eb461d167298fd31605cbe4b8ca7209
John Irwin <jdi@untangle.com>
2007-02-28 00:44:18 +0000

    fix 2313,2314
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@9012 8ebf8b4e-06f3-0310-961f-89f74618be54

--- kiosk-mv/files/home/kiosk/utils/edgeguard-restore.sh
+++ kiosk-mv/files/home/kiosk/utils/edgeguard-restore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop Edgeguard services." 15 60
+Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop Untangle services." 15 60
 
 if [ $? -eq 0 ]; then
-    xterm -e "sudo /usr/bin/edgeguard-restore"
+    xterm -T "Untangle Recovery" -e "sudo /usr/bin/edgeguard-restore"
 else
     echo "No shutting down"
 fi
