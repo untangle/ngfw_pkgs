221ea953fc58b69e245f328a15df5a85fee77133
Aaron Read <amread@untangle.com>
2007-04-20 19:51:55 +0000

    works with cvs
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@9813 8ebf8b4e-06f3-0310-961f-89f74618be54

--- diffserver/diffserver.rb
+++ diffserver/diffserver.rb
@@ -1,27 +1,45 @@
-#!/usr/bin/ruby
+#!/usr/bin/env /usr/bin/ruby
 
-def head_version(name)
-  /head: ([0-9]+.[0-9]+)/ =~ `rlog -h #{name}`
-  $1
-end
+require 'fcgi'
+
+CVS_ROOT = '/var/lib/diffserver/cvs'
+WORK_DIR = '/var/lib/diffserver/list'
+CVS_DIFF = "cvs -d #{CVS_ROOT} diff -u"
+
+HEADER = "Content-Type: text/plain\n\n"
 
 def get_update(name, bl_version)
   rcs_version = bl_version.sub(/:/, '.')
-  rcs_head = head_version(name)
-  if rcs_version != rcs_head then
-    puts "[#{name} #{rcs_head.sub(/\./, ':')} update]"
 
-    f = IO.popen("rcsdiff -r#{rcs_version} -r#{rcs_head} #{name} 2>/dev/null",
-                 mode="r") do |io|
-      io.each_line do |l|
-        if /^> (.*)$/ =~ l then
-          puts "+#{$1}"
-        elsif /^< (.*)$/ =~ l then
-          puts "-#{$1}"
+  f = IO.popen("#{CVS_DIFF} -r#{rcs_version} -rHEAD #{WORK_DIR}/#{name}",
+               mode='r') do |io|
+    first_line = true
+    io.each_line do |l|
+      l.strip!
+      if first_line then
+        if /^\+\+\+.*([0-9]+\.[0-9]+)$/ =~ l then
+          print "[#{name} #{$1.sub(/\./, ':')} update]\n"
+          first_line = false
         end
+      elsif /^[+-]/ =~ l then
+        print "#{l}\tc\n"
       end
     end
   end
 end
 
-get_update(ARGV[0], ARGV[1])
+FCGI.each_cgi do |cgi|
+  version = cgi.params["version"][0]
+
+  print HEADER
+
+  if version then
+    version.split(',').each do |v|
+      if /(.*):([0-9]+:[0-9]+)/ =~ v
+        name = $1
+        bl_version =  $2
+        get_update(name, bl_version)
+      end
+    end
+  end
+end
