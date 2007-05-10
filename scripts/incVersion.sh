bec6d37dbfc171f856b77a7261c16022898a6d36
Dirk Morris <dmorris@untangle.com>
2007-05-10 18:23:14 +0000

    .
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@10106 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ scripts/incVersion.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+[[ ! $# -eq 1 ]] && echo "Usage: $0 distribution" && exit 1
+
+distribution=${1}
+
+echo -n "Setting version, setting distribution to \"$distribution\"..."
+
+revision=`svn info . | awk '/Revision: / { print $2 }'`
+timestamp=`svn info | awk '/Last Changed Date: / { gsub(/-/, "", $4) ; print $4 }'`
+hasLocalChanges=`svn status | grep -v -E '^\?'`
+
+if [[ -f ../VERSION ]] ; then
+  versionFile=../VERSION
+else
+  versionFile=../../VERSION
+fi
+
+baseVersion=`cat $versionFile`~svn${timestamp}r${revision}
+
+if [[ -z "$hasLocalChanges" ]] ; then
+  upstreamVersion=$baseVersion
+else
+  upstreamVersion=${baseVersion}+$USER`date +"%Y%m%dT%H%M"`
+  distribution=$USER
+fi
+
+previousUpstreamVersion=`dpkg-parsechangelog | awk '/Version: / { gsub(/-.*/, "", $2) ; print $2 }'`
+
+if [[ "${upstreamVersion}" == "$previousUpstreamVersion" ]] ; then
+  dchArg="-i"
+else
+  dchArg="-v ${upstreamVersion}-1"
+fi
+
+dch $dchArg -D ${distribution} "auto build"
+
+echo " done."
+
--- scripts/incoming-queue.rb
+++ scripts/incoming-queue.rb
@@ -29,10 +29,14 @@ end
 
 class DebianUpload
 
+  @@doEmailSuccess = false
+  @@doEmailFailure = false
+
   attr_reader :files, :distribution, :uploader, :version
 
-  def initialize(file)
+  def initialize(file, move = true)
     @file = file
+    @move = move
     @files = [ @file ]
   end
 
@@ -56,51 +60,54 @@ class DebianUpload
         puts output
         email(@emailRecipientsFailure,
               "Upload of #{File.basename(@name)} failed",
-              output)
+              output) if @@doEmailFailure
         return
       end
 
-      output = `command 2>&1`
+      output = `#{@command} 2>&1`
       if $? != 0
         output = "Something went wrong when adding #{@file}, leaving it in incoming/\n\n" + output
+        puts output
         email(@emailRecipientsFailure + [@uploader, @maintainer],
               "Upload of #{@name} failed",
-              output)
+              output) if @@doEmailFailure
         return
       end
-
-      @files.each { |file|
-        File.rename(file, "#{PROCESSED}/#{File.basename(file)}")
-      }
+      
+      if @move
+        @files.each { |file|
+          File.rename(file, "#{PROCESSED}/#{File.basename(file)}")
+        }
+      end
 
       puts output
-
       email(@emailRecipientsSuccess, "Upload of #{@file} succeeded",
             @files.inject("") { |result, e|
               result += e.gsub(/#{INCOMING}\//, "") + "\n"
-            })
+            }) if @@doEmailSuccess
     rescue Exception => e
+      puts e.message + "\n" + e.backtrace.join("\n")
       email(@emailRecipientsFailure,
             "Upload of #{@name} failed",
-            e.message + "\n" + e.backtrace.join("\n"))
+            e.message + "\n" + e.backtrace.join("\n")) if @@doEmailFailure
     end
   end
 end
 
 class PackageUpload < DebianUpload
-  def initialize(file)
-    super(file)
+  def initialize(file, move)
+    super(file, move)
     @name = file.sub(/_.*/, "")
     @distribution = DEFAULT_DISTRIBUTION
-    @command = "reprepro -Vb #{REP} includedeb #{@distribution} #{@file}"
+    @command = "reprepro -V -b #{REP} --component upstream includedeb #{@distribution} #{@file}"
     @emailRecipientsSuccess = DEFAULT_MAIL_RECIPIENTS
     @emailRecipientsFailure = DEFAULT_MAIL_RECIPIENTS
   end
 end
 
 class ChangeFileUpload < DebianUpload
-  def initialize(file)
-    super(file)
+  def initialize(file, move)
+    super(file, move)
     filesSection = false
     File.open(file).each { |line|
       line.strip!
@@ -133,14 +140,29 @@ class ChangeFileUpload < DebianUpload
   end
 end
 
-Dir["#{INCOMING}/*.changes"].each { |file|
-  cfu = ChangeFileUpload.new(file)
-  puts cfu
-  cfu.addToRepository
-}
+if ARGV.length == 1
+  INCOMING = ARGV[0]
+  move = false
+else
+  move = true
+end
 
-Dir["#{INCOMING}/*.deb"].each { |file|
-  pu = PackageUpload.new(file)
-  puts pu
-  pu.addToRepository
-}
+if File.directory?(INCOMING)
+  Dir["#{INCOMING}/*.changes"].each { |file|
+    cfu = ChangeFileUpload.new(file, move)
+    cfu.addToRepository
+  }
+
+  Dir["#{INCOMING}/*.deb"].each { |file|
+    pu = PackageUpload.new(file, move)
+    pu.addToRepository
+  }
+else
+  if INCOMING =~ /\.changes$/
+    cfu = ChangeFileUpload.new(INCOMING, move)
+    cfu.addToRepository
+  else
+    pu = PackageUpload.new(INCOMING, move)
+    pu.addToRepository
+  end
+end
