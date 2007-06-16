9bbbc78dac2b68b28a7983160f8dcc964d285d94
Dirk Morris <dmorris@untangle.com>
2007-06-16 22:42:10 +0000

    must check return code of passwd
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@11050 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 if [ ! -f /etc/password-set ] ; then
+    # set the password for the first time
     sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
-    sudo su root -c "touch /etc/password-set"
+
+    # if successful set a flag so we won't set the password next time
+    if [ $? -eq 0 ] ; then
+        sudo su root -c "touch /etc/password-set"
+    fi
 fi
     
 /usr/bin/xterm -T Password -e su root
