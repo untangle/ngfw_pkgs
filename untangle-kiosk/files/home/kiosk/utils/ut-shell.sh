981eb3cfec0bb7f6e8a3a72621b299aa97648b09
Dirk Morris <dmorris@untangle.com>
2007-06-17 22:32:39 +0000

    changed to grep for no password
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@11053 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-if [ ! -f /etc/password-set ] ; then
+if [ ! -z `sudo su -c 'grep "^root:\*:" /etc/shadow'` ] ; then
     # set the password for the first time
     sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
-
-    # if successful set a flag so we won't set the password next time
-    if [ $? -eq 0 ] ; then
-        sudo su root -c "touch /etc/password-set"
-    fi
 fi
     
 /usr/bin/xterm -T Password -e su root
