ecbfb93c81fa0073021c3af08a559061c03d1ac0
Dirk Morris <dmorris@untangle.com>
2007-06-21 17:22:29 +0000

    remove invalid path to launch xterm
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@11324 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
@@ -4,5 +4,11 @@ if [ ! -z `sudo su -c 'grep "^root:\*:" /etc/shadow'` ] ; then
     # set the password for the first time
     sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
 fi
+
+# Also check for the old default password
+if [ ! -z `sudo su -c 'grep "^root:YKN4WuGxhHpIw:" /etc/shadow'` ] ; then
+    # set the password for the first time
+    sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
+fi
     
-/usr/bin/xterm -T Password -e su root
+xterm -T Password -e su root
