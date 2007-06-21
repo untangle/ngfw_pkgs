6c3bbcfda57f0df570a0ebee2409025a051b7e73
John Irwin <jdi@untangle.com>
2007-06-21 19:24:32 +0000

    moved to private
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@11339 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-support-agent/lib/rbot/post-config.rb
+++
@@ -1,8 +0,0 @@
-# write out our datadir so we can reference it at runtime
-File.open("pkgconfig.rb", "w") {|f|
-  f.puts "module Irc"
-  f.puts "  module PKGConfig"
-  f.puts "    DATADIR = '#{config('datadir')}/rbot'"
-  f.puts "  end"
-  f.puts "end"
-}
