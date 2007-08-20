e1982c2db4d060c313a1d2734b1e1d1c5f838964
Sebastien Delafond <sdelafond@gmail.com>
2007-08-20 19:49:58 +0000

    Merging      12060 From         svn://chef/branch/prod/release-5.0/work/pkgs Timestamp    2007-08-20 12:41:07 -0700 Log message: Also get rid of the the old root passwd from the ISO staging process.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12061 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/home/kiosk/utils/ut-shell.sh
@@ -10,5 +10,11 @@ if [ ! -z `sudo su -c 'grep "^root:YKN4WuGxhHpIw:" /etc/shadow'` ] ; then
     # set the password for the first time
     sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
 fi
+
+# Also check for the old default password
+if [ ! -z `sudo su -c 'grep '^root:$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC.:' /etc/shadow'` ] ; then
+    # set the password for the first time
+    sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
+fi
     
 xterm -T xterm -e su root
