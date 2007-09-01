34fa2c307b30f746e770b6c31bae3fb23ed1a670
Robert Scott <rbscott@untangle.com>
2007-09-01 20:33:10 +0000

    have a little piece that lists out the interfaces
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12097 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-net-urchin/files/var/lib/rails/untangle-net-urchin/app/controllers/interface_controller.rb
@@ -0,0 +1,83 @@
+class InterfaceController < ApplicationController
+  ## DDD These are subject to internationalization DDD
+  DefaultInterfaceMapping = {
+    # default Display name plus the index
+    "eth0" => [ "External", 1 ],
+    "eth1" => [ "Internal", 2 ],
+    "eth2" => [ "DMZ", 3 ]
+  }
+
+  def index
+    list
+    render :action => 'list'
+  end
+
+  def list
+    @title = "Interface List"
+    @description = "List of all of the available interfaces."
+    
+    @interfaces = Interface.find_all
+
+    if ( @interfaces.nil? || @interfaces.empty? )       
+      @interfaces = loadInterfaces
+      ## Save all of the new interfaces
+      @interfaces.each { |interface| interface.save }
+    end
+  end
+
+  def reload
+    ## DDD This makes it really hard to remap interfaces and save settings. DDD ###
+    ## DDD since the settings are stored inside of the interface.           DDD ###
+    Interface.destroy_all
+
+    return redirect_to( :action => 'list' )
+  end
+
+  def static
+    interface_id = params[:id]
+    return redirect_to( :action => 'list' ) if interface_id.nil?
+    @interface = Interface.find( interface_id )
+    return redirect_to( :action => 'list' ) if interface_id.nil?
+    @title = "Static Interface Configuration"
+    
+    ## Retrieve the static configuration, creating a new one if necessary.
+    @staticConfig = @interface.intf_static
+    @staticConfig = IntfStatic.new if @staticConfig.nil?
+  end
+
+  private
+  
+  ## Load all physical devices
+
+  ## DDD some of this code may be debian specific DDD
+  def loadInterfaces
+    ## Create an empty array
+    interfaceArray = []
+
+    ## Find all of the physical interfaces
+    currentIdex = DefaultInterfaceMapping.size - 1
+    `find /sys/devices -name 'net:*'`.each do |sys_device_path| 
+      name=sys_device_path.sub( /.*net:/, "" ).strip
+      interface = Interface.new
+      parameters = DefaultInterfaceMapping[name]
+
+      ## Use the os name if it doesn't have a predefined virtual name
+      parameters = [ name, currentIndex += 1 ] if parameters.nil?
+      interface.name  = parameters[0]
+      interface.index = parameters[1]
+
+      ## Set the mac address
+      File.open( "/sys/class/net/#{name}/address", "r" ) { |f| interface.mac_address = f.readline.strip }
+      
+      ## Add the interface.
+      interfaceArray << interface
+    end
+
+    ## Update the index for each one
+    interfaceArray.each do |interface|
+      
+    end
+
+    interfaceArray
+  end
+end
---
+++ untangle-net-urchin/files/var/lib/rails/untangle-net-urchin/db/migrate/001_network.rb
@@ -0,0 +1,93 @@
+class Network < ActiveRecord::Migration
+  def self.up
+    ## Table for storing the configuration for each of the interfaces
+    create_table :interfaces do |table|
+      ## Virtual name of the interface
+      table.column :name, :string
+
+      ## Interface index
+      table.column :index, :integer
+
+      ## MAC Address of the interface
+      table.column :mac_address, :string
+      
+      ## BUS Identifier.
+      table.column :bus_id, :string
+
+      ## Duplexing on this interface (half, full or auto)
+      table.column :duplex, :string
+
+      ## Speed of the interface (10,100,1000 or auto)
+      table.column :speed, :string
+
+      ## Configuration type[static,dynamic,bridge,etc]
+      table.column :type, :string
+
+      ## Static configuration
+      table.column :intf_static_id, :integer
+
+      ## Dynamic configuration
+      table.column :intf_dynamic_id, :integer
+
+      ## Bridge configuration
+      table.column :intf_bridge_id, :integer
+    end
+
+    ## This is a single IP network.
+    create_table :ip_networks do |table|
+      table.column :ip, :string
+      table.column :netmask, :string
+      table.column :allow_ping, :string
+    end
+
+    ## static interface configuration.
+    create_table :intf_statics do |table|
+      table.column :mtu, :integer
+      table.column :media, :string
+      table.column :speed, :string
+      table.column :forward_traffic, :boolean
+      table.column :allow_ping, :boolean
+    end
+
+    ## Join between an IP network and a static configuration.
+    create_table :intf_statics_ip_networks, :id => false do |table|
+      table.column :intf_static_id, :integer
+      table.column :ip_network_id, :integer
+    end
+
+    ## dynamic interface configuration.
+    create_table :intf_dynamics do |table|
+      table.column :forward_traffic, :boolean
+      table.column :allow_ping, :boolean
+    end
+
+    ## Join between an IP network and a static configuration.
+    create_table :intf_dynamics_ip_networks, :id => false do |table|
+      table.column :intf_dynamic_id, :integer
+      table.column :ip_network_id, :integer
+    end
+
+    ## Bridge interface configuration
+    create_table :intf_bridges do |table|
+      ## Interface to bridge to
+      table.column :interface_id, :integer
+    end
+
+    ## Hostname parameters
+    create_table :hostnames do |table|
+      table.column :name, :string
+      
+    end
+  end
+
+  def self.down
+    drop_table :interfaces
+    drop_table :ip_networks
+    drop_table :intf_statics
+    drop_table :intf_statics_ip_networks
+    drop_table :intf_dynamics
+    drop_table :intf_dynamics_ip_networks
+    drop_table :intf_bridges
+    drop_table :hostnames
+  end
+end
