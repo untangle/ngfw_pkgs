b87a595bd35e43d76de3505c61650469cf211b3a
Ken Hilton <ken@untangle.com>
2007-10-05 21:30:54 +0000

    Corrected module name spellings
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12301 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-cli-client/files/common.rb
+++ untangle-cli-client/files/common.rb
@@ -8,7 +8,7 @@
 # @version 0.1
 #
 
-module UCLICommon
+module NUCLICommon
 
 # Error Messages
 
--- untangle-cli-client/files/util.rb
+++ untangle-cli-client/files/util.rb
@@ -8,7 +8,7 @@
 # @version 0.1
 #
 
-module UCLIUtil
+module NUCLIUtil
     
     def print!(msg)
         print msg
