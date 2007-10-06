531d9cc4578f92c5f2b1b553abf6ef926afa3534
Robert Scott <rbscott@untangle.com>
2007-10-06 21:49:00 +0000

    have to remove the [] because of serialization escaping
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12310 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-urchin/files/var/lib/rails/untangle-net-urchin/app/controllers/wizard_controller.rb
+++ untangle-net-urchin/files/var/lib/rails/untangle-net-urchin/app/controllers/wizard_controller.rb
@@ -1,54 +1,199 @@
 class WizardController < ApplicationController
-  WizardPages = [ "welcome", "interface-test", "interface[]", "review" ].freeze
+  InterfaceKey = "interface-config"
+
+  class SimpleStageHandler
+    def name
+      ""
+    end
+  end
+
+  class WizardStage
+    def initialize( id, name, is_array = false, handler = nil )
+      @id, @name, @is_array, @handler = id, name, is_array, handler 
+      @handler = SimpleStageHandler.new if @handler.nil?
+    end
+
+    ## ID that is to be used when this is referenced in list.
+    def list_id
+      "sl-#{@id}"
+    end
+
+    ## Name of the partial used to render this stage
+    def partial
+      @id.sub( /\[[0-9]+\]$/, "" ).gsub( "-", "_" )
+    end
+
+    attr_reader :id, :name, :is_array, :handler
+  end
+
+  class InterfaceStageHandler
+    def initialize( interface, index )
+      @interface = interface
+      
+      ## Review : Don't like that this is in two places here and InterfaceController.
+      mapping = InterfaceController::DefaultInterfaceMapping[@interface.os_name]
+      mapping = [ @interface.os_name, index ] if mapping.nil?
+      
+      @name, @index = mapping      
+    end
+
+    attr_reader :index, :interface, :name
+  end
+
+  WizardStages = 
+    [ WizardStage.new( "welcome", "Welcome" ),
+      WizardStage.new( "interface-test", "Detection" ),
+      WizardStage.new( InterfaceKey, "%s", true ),
+      WizardStage.new( "review", "Review" ),
+      WizardStage.new( "finish", "Finished" )
+    ].freeze
 
   def index
     @title = "Setup Wizard"
 
     @interfaces = networkManager.interfaces
+
+    ## These indexes don't have to be sequential (just for sorting)
+    index = InterfaceController::DefaultInterfaceMapping.size 
+    interfaceHandlers = @interfaces.map { |i| InterfaceStageHandler.new( i, index += 1 ) }
+    interfaceHandlers.sort! { |a,b| a.index <=> b.index }
+
+    ## Have to sort these 
+    @stages = expandStages( InterfaceKey => interfaceHandlers )
+
+    ## This should be in a global place
+    @cidr_options = OSLibrary::NetworkManager::CIDR.map { |k,v| [ format( "%-3s %s", k, v ) , k ] }
+
+    @cidr_options.sort! { |a,b| a[1].to_i <=> b[1].to_i }
   end
+  
+  def save
+    ## Iterate all of the interfaces
+    interfaceList = params[:interfaceList]
+
+    @error = nil
+    return @error = "Invalid interface list" if ( interfaceList.nil?  || interfaceList.empty? )
+
+    ## These are all of the interfaces that are presently available
+    interfaces = networkManager.interfaces
+    
+    ## This is the list of ruby objects to be saved
+    n_interfaceList = []
+
+    ## Start numbering them from 1
+    index = 0
+
+    ## Review : This is bad, it is just bad.
+    Interface.destroy_all
+
+    ## Create a new interface for each one (they are ordered)
+    interfaceList.each do |interface|
+      ## Retrive the os_name
+      os_name = params["#{interface}.os_name"]
+
+      ## Retrive the name
+      name = params["#{interface}.name"]
+
+      ## Retrieve the config type
+      config_type = params["#{interface}.type"]
+
+      ## interface is some gnarsty key
+      return @error = "Unknown interface #{interface} '#{os_name}' '#{name}'" if ( os_name.nil? || name.nil? )
+      
+      a = interfaces.select { |intf| intf.os_name == os_name }
+      return @error = "The interface #{os_name} is not available" if ( a.empty? || ( a.size > 1 ))
+      a = a[0]
+      
+      interfaces.delete( a )
+
+      n_intf = Interface.new
+      
+      ## Setup all of the parameters about the interface
+      n_intf.name, n_intf.index, n_intf.os_name = name, index += 1, os_name
+      n_intf.mac_address, n_intf.bus, n_intf.vendor = a.mac_address, a.bus, a.vendor
+      
+      ## Set the configuration
+      case config_type
+      when InterfaceHelper::ConfigType::STATIC then static( n_intf, interface )
+      when InterfaceHelper::ConfigType::DYNAMIC then dynamic( n_intf, interface )
+      when InterfaceHelper::ConfigType::BRIDGE then bridge( n_intf, interface )
+      else return @error = "Unknown configuration type #{config_type}"
+      end
+    end
+  end
+
+  private
 
   def networkManager
     ## REVIEW DebianSarge is hardcoded, need to move this out.
     OSLibrary.getOS( "DebianSarge" ).manager( "network_manager" )
   end
 
-  ## This is the navigational form
-  def nav
-    direction, current, interface_count = params[:direction], params[:current], params[:interface_count]
-    
-    raise "empty parameter" if direction.nil? || current.nil? || interface_count.nil?
+  ## This is the navigational form  
+  def expandStages( keys )
+    WizardStages.collect do |p| 
+      next p unless p.is_array
+      values = []
+      handlers = keys[p.id]
+      next nil if handlers.nil?
 
-    interface_count = interface_count.to_i
-    raise "invalid count" if interface_count == 0
+      handlers.size.times { |n| values << WizardStage.new( "#{p.id}[#{n}]", p.name, false, handlers[n] ) }
+      values
+    end.flatten.delete_if { |p| p.nil? }
+  end
 
-    current = current.sub( /^sl-/, "" )
+  def static( interface, interface_stage_id )
+    static = IntfStatic.new
+    network = IpNetwork.new
+    network.ip = params["#{interface_stage_id}-static.address"]
+    network.netmask = params["#{interface_stage_id}-static.netmask"]
+    network.allow_ping = true
+    static.ip_networks << network
     
-    raise "invalid direction" unless ( direction == "prev" || direction == "next" )
-    
-    ## This is the case where it is not of the interface configuration panels
-    pages = expandPages( interface_count )
-    index = pages.index( current )
-    raise "Current page '#{current}' is not valid" if index.nil?
-    
-    case direction
-    when "prev" then index -= 1
-    when "next" then index += 1
+    if interface.index == 1
+      ## Set the default gateway and dns
+      static.default_gateway = params["#{interface_stage_id}-static.default_gateway"]
+      static.dns_1 = params["#{interface_stage_id}-static.dns_1"]
+      static.dns_2 = params["#{interface_stage_id}-static.dns_2"]
+    else
+      ## Add a NAT policy (this not the external interface)
+      natPolicy = NatPolicy.new
+      natPolicy.ip = "0.0.0.0"
+      natPolicy.netmask = "0"
+      natPolicy.new_source = "auto"
     end
+
+    static.save
+    interface.config_type = InterfaceHelper::ConfigType::STATIC
+    interface.intf_static = static
+    interface.save
+  end
+
+  def dynamic( interface, interface_stage_id )
+    dynamic = IntfDynamic.new
+    dynamic.allow_ping = true
+
+    dynamic.save
+    interface.config_type = InterfaceHelper::ConfigType::DYNAMIC
+    interface.intf_dynamic = dynamic
+    interface.save
+  end
+
+  def bridge( interface, interface_stage_id )
+    bridge = IntfBridge.new
+    os_name = params["#{interface_stage_id}-bridge.bridge_interface"]
+    return logger.warn( "Bridge interface is not specified" ) if os_name.nil?
     
-    raise "At the first page" if index < 0
-    raise "At the final page" if index >= pages.size
+    bridge_interface = Interface.find( :os_name => os_name )
+    return logger.warn( "Unable to find the interface '#{os_name}'" ) if bridge_interface.nil?
+    
+    brdige.bridge_interface = bridge_interface
+    bridge_interface.bridge_interfaces << bridge
 
-    @current = pages[index]
+    interface.intf_bridge = bridge
+    interface.config_type = InterfaceHelper::ConfigType::BRIDGE
+    interface.save
   end
 
-  private
   
-  def expandPages( interface_count )
-    WizardPages.collect do |p| 
-      next p if /\[\]$/.match( p ).nil?
-      interfaces = []
-      interface_count.times { |n| interfaces << "interface[#{n}]" }
-      interfaces
-    end.flatten
-  end
 end
--- untangle-net-urchin/files/var/lib/rails/untangle-net-urchin/public/javascripts/wizard.js
+++ untangle-net-urchin/files/var/lib/rails/untangle-net-urchin/public/javascripts/wizard.js
@@ -1,15 +1,21 @@
-/* Go to the previous page, if possible */
-function prevPage()
+/* The current stage */
+var currentStage = 0;
+
+/* Next page listeners */
+var pageHandlers = new Array();
+
+/* Go to the previous stage, if possible */
+function prevStage()
 {
     /* Decrement the value by one */
-    changeCurrent( parseInt( document.forms.nav.current.value ) - 1);
+    changeCurrent( currentStage - 1);
 }
 
-/* Go to the next page, if possible. */
-function nextPage()
+/* Go to the next stage, if possible. */
+function nextStage()
 {
     /* Increment the value by one */
-    changeCurrent( parseInt( document.forms.nav.current.value ) + 1);
+    changeCurrent( currentStage + 1);
 }
 
 function changeCurrent( current )
@@ -24,15 +30,15 @@ function changeCurrent( current )
     /* All of the list ids are prefixed by sl- */
     var listId = "sl-" + current;
 
-    /* Should this print a message ? */
+    /* Should this print an error message ? */
     if ( current < 0 ) {
         current = 0;
     } else if ( current >= len ) {
         current = len - 1;
     }
 
-
     var j = 0;
+    var stage = null;
     for( var i = 0 ; i < len; i++ ) {
         /* Ignore anything that is not a list item */
         if ( children[i].nodeName != "LI" && children[i].nodeName != "li" ) continue;
@@ -46,16 +52,102 @@ function changeCurrent( current )
                 Element.show( panelId );
             } catch ( e ) { }
         } else {
+            
             children[i].className = className;
             try {
                 Element.hide( panelId );
             } catch ( e ) { }
         }
 
-        /* This is hte index of the actual list items */
+        /* Trigger the event for the panel right before (only trigger on next events) */
+        if (( j == currentStage ) && ( currentStage == ( current - 1 ))) stage = panelId;
+
+        /* This is the index of the actual list items */
         j++;
     }
 
+    if (( stage != null ) && pageHandlers[stage] != null ) pageHandlers[stage]();
+
     /* Update the value of the current item */
-    document.forms.nav.current.value = current;
+    currentStage = current;
+}
+
+function setConfigType( parent )
+{
+    var container = document.getElementById(parent);
+    /* Iterate all of the children and hide them */
+    
+    var children = container.childNodes;
+    var len = children.length;
+    
+    var configType = document.getElementById(parent + ".type" );
+    
+    if ( configType == null ) return alert( "Unable to find config type" );
+
+    configType = configType.value;
+
+    if ( configType == null ) return alert( "Unable to find config type" );
+
+    /* This is the convention for naming the child panels */
+    var panelId = parent + "-" + configType;
+
+    for ( var i = 0 ; i < len ; i++ ) {
+        if ( children[i].nodeName != "DIV" && children[i].nodeName != "div" ) continue;
+
+        if ( children[i].id == panelId ) Element.show( children[i].id );
+        else Element.hide( children[i].id );
+    }
+}
+
+
+function Interface( id, osName, name )
+{
+    this.id = id;
+    this.osName = osName;
+    this.name = name;
+}
+
+/* This is an array of all of the ids (in order) */
+var interfaceArray = new Array();
+
+/* This is a function for the interface management */
+function updateBridges( stageId )
+{
+    var html = "";
+    var len = interfaceArray.length;
+    var completed = false;
+    var select = null;
+    for ( var c = 0 ; c< len ; c++ ) {
+        var intf = interfaceArray[c];
+
+        /* You want to update the select immediately after the current one */
+        if ( completed ) {
+            select = intf.id + "-bridge.bridge_interface";
+            break;
+        }
+
+        /* This is the current configuration type for the interface */
+        var configType = document.getElementById( intf.id + ".type" );
+        
+        if ( configType == null ) continue;
+
+        /* Retrieve the current value */
+        configType = configType.value;
+
+        switch ( configType ) {
+        case 'static':
+        case 'dynamic':
+            html += "<option value='" + intf.osName + "'>" + intf.name + "</option>";
+            break;
+
+        default:
+            break;
+        }
+
+        /* Don't add interfaces past the current one */
+        if ( intf.id == stageId ) completed = true;
+    }
+    
+    /* Time to update the comboxbox */
+    if ( select != null && document.getElementById( select) != null ) Element.update( select, html );
 }
