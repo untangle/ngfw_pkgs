038293ab682c7c3c6ccd2b72b23ae37f01f239ec
Ken Hilton <ken@untangle.com>
2007-10-18 17:45:33 +0000

    Added caching to get_statistics.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12418 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-cli-server/files/webfilter.rb
+++ untangle-cli-server/files/webfilter.rb
@@ -14,6 +14,7 @@ require 'java'
 require 'proxy'
 require 'debug'
 
+require 'thread'
 require 'filternode'
 
 require 'common'
@@ -25,11 +26,14 @@ class Webfilter < UVMFilterNode
     NODE_NAME = "untangle-node-webfilter"
     WEBFILTER_MIB_ROOT = UVM_FILTERNODE_MIB_ROOT + ".1"
     NUM_STAT_COUNTERS = 16
+    STATS_CACHE_EXPIRY = 5
 
     def initialize
         @diag = Diag.new(DEFAULT_DIAG_LEVEL)
 	@diag.if_level(3) { puts! "Initializing WebFilter..." }
         super
+        @stats_cache = {}
+        @stats_cache_lock = Mutex.new
 	@diag.if_level(3) { puts! "Done initializing WebFilter..." }
     end
 
@@ -698,40 +702,53 @@ class Webfilter < UVMFilterNode
         end
         
         begin
-            node_ctx = @@uvmRemoteContext.nodeManager.nodeContext(tid)
-            nodeStats = node_ctx.getStats()
-            # TODO: add [nodeStats,timeNow] to hash keyed on tid so we can cache
+            nodeStats = nil
+            @stats_cache_lock.synchronize {
+                cached_stats = @stats_cache[tid]
+                if !cached_stats || ((Time.now.to_i - cached_stats[1]) > STATS_CACHE_EXPIRY)
+                    @diag.if_level(3) { puts! "Stat cache miss / expiry." }
+                    node_ctx = @@uvmRemoteContext.nodeManager.nodeContext(tid)
+                    nodeStats = node_ctx.getStats()
+                    @stats_cache[tid] = [nodeStats, Time.now.to_i]
+                else
+                    @diag.if_level(3) { puts! "Stat cache hit." }
+                    nodeStats = cached_stats[0]
+                end
+            }
+
             @diag.if_level(3) { puts! "Got node stats for #{tid}" ; p nodeStats }
             stats = ""
             if args[0]
                 oid = (args[0] == '-g') ? args[1] : oid_next(args[1], tid)
                 return nil unless oid
+                # Construct OID fragment to match on from >up to< the last two
+                # pieces of the effective OID, eg, xxx.1 => 1, xxx.18.2 ==> 18.2
+                int = "integer"; str = "string", c32 = "counter32"
                 webfilter_mib_pieces = WEBFILTER_MIB_ROOT.split('.')
                 oid_pieces = oid.split('.')
-                stat_id = oid_pieces[webfilter_mib_pieces.length-oid_pieces.length+1,2].join('.')
-                puts! stat_id
+                stat_id = oid_pieces[(webfilter_mib_pieces.length-oid_pieces.length)+1 ,2].join('.')
                 case stat_id
-                    when "1";  stat, type = nodeStats.tcpSessionCount(), "integer"
-                    when "2";  stat, type = nodeStats.tcpSessionTotal(), "integer"
-                    when "3";  stat, type = nodeStats.tcpSessionRequestTotal(), "integer"
-                    when "4";  stat, type = nodeStats.udpSessionCount(), "integer"
-                    when "5";  stat, type = nodeStats.udpSessionTotal(), "integer"
-                    when "6";  stat, type = nodeStats.udpSessionRequestTotal(), "integer"
-                    when "7";  stat, type = nodeStats.c2tBytes(), "integer"
-                    when "8";  stat, type = nodeStats.c2tChunks(), "integer"
-                    when "9";  stat, type = nodeStats.t2sBytes(), "integer"
-                    when "10"; stat, type = nodeStats.t2sChunks(), "integer"
-                    when "11"; stat, type = nodeStats.s2tBytes(), "integer"
-                    when "12"; stat, type = nodeStats.s2tChunks(), "integer"
-                    when "13"; stat, type = nodeStats.t2cBytes(), "integer"
-                    when "14"; stat, type = nodeStats.t2cChunks(), "integer"
-                    when "15"; stat, type = nodeStats.startDate(), "string"
-                    when "16"; stat, type = nodeStats.lastConfigureDate(), "string"
-                    when "17"; stat, type = nodeStats.lastActivityDate(), "string"
+                    when "1";  stat, type = nodeStats.tcpSessionCount(), int
+                    when "2";  stat, type = nodeStats.tcpSessionTotal(), int
+                    when "3";  stat, type = nodeStats.tcpSessionRequestTotal(), int
+                    when "4";  stat, type = nodeStats.udpSessionCount(), int
+                    when "5";  stat, type = nodeStats.udpSessionTotal(), int
+                    when "6";  stat, type = nodeStats.udpSessionRequestTotal(), int
+                    when "7";  stat, type = nodeStats.c2tBytes(), int
+                    when "8";  stat, type = nodeStats.c2tChunks(), int
+                    when "9";  stat, type = nodeStats.t2sBytes(), int
+                    when "10"; stat, type = nodeStats.t2sChunks(), int
+                    when "11"; stat, type = nodeStats.s2tBytes(), int
+                    when "12"; stat, type = nodeStats.s2tChunks(), int
+                    when "13"; stat, type = nodeStats.t2cBytes(), int
+                    when "14"; stat, type = nodeStats.t2cChunks(), int
+                    when "15"; stat, type = nodeStats.startDate(), str
+                    when "16"; stat, type = nodeStats.lastConfigureDate(), str
+                    when "17"; stat, type = nodeStats.lastActivityDate(), str
                     when /18\.\d+/
                         counter = oid_pieces[-1].to_i()-1
                         return nil unless counter < NUM_STAT_COUNTERS
-                        stat, type = nodeStats.getCount(counter), "counter32"
+                        stat, type = nodeStats.getCount(counter), c32
                 else
                     return nil
                 end
