a69b82f2e078fc7503140e894cacd2e987ef7d54
Ken Hilton <ken@untangle.com>
2007-10-30 21:28:44 +0000

    Added standard Untangle comment header.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12547 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-cli-client/files/common.rb
+++ untangle-cli-client/files/common.rb
@@ -1,12 +1,4 @@
 #!/usr/local/bin/ruby
-#
-# ucli_common.rb - UCLI Client Common Code
-#
-# Copyright (c) 2007 Untangle Inc., all rights reserved.
-#
-# @author <a href="mailto:ken@untangle.com">Ken Hilton</a>
-# @version 0.1
-#
 
 module NUCLICommon
 
