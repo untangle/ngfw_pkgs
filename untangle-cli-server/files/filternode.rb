d0f860f28974f8461bf14ac6ad8c1565025e8a51
Ken Hilton <ken@untangle.com>
2007-10-30 21:31:34 +0000

    Added standard Untangle comment header.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@12549 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-cli-server/files/filternode.rb
+++ untangle-cli-server/files/filternode.rb
@@ -1,12 +1,19 @@
-#!/usr/local/bin/ruby
 #
-# filternode.rb - Base class from which all specific UVM Filter Nodes are derived, e.g., 
-# WebFilter, etc.  Contains functionality shared by all such filters.
+# $HeadURL:$
+# Copyright (c) 2003-2007 Untangle, Inc. 
 #
-# Copyright (c) 2007 Untangle Inc., all rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2,
+# as published by the Free Software Foundation.
 #
-# @author <a href="mailto:ken@untangle.com">Ken Hilton</a>
-# @version 0.1
+# This program is distributed in the hope that it will be useful, but
+# AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+# NONINFRINGEMENT.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 require 'java'
 require 'proxy'
