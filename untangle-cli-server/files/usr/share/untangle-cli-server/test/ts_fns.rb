d05331ec6c7a0c45755888f7197b0a9e37156af1
Catalin Matei <cmatei@untangle.com>
2007-11-30 16:40:26 +0000

    Add a test suite for all FNs (the ones which have unit tests)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@13250 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-cli-server/files/usr/share/untangle-cli-server/test/ts_fns.rb
@@ -0,0 +1,8 @@
+$:.unshift File.join(File.dirname(__FILE__), ".")
+require 'test/unit'
+require 'tc_protofilter'
+require 'tc_ips'
+require 'tc_spamblocker'
+require 'tc_phish'
+require 'tc_spyware'
+require 'tc_virus'
