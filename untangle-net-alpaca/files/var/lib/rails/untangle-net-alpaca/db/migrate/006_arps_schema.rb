11a8c805334bc0c8a1e01453306f82376c6dbb46
Tom Belote <tbelote@untangle.com>
2007-12-05 00:01:25 +0000

    Bug 3386 db schema for arp
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@13398 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/db/migrate/006_arps_schema.rb
@@ -0,0 +1,21 @@
+class ArpsSchema < ActiveRecord::Migration
+  def self.up
+    ##Table for static arps
+    create_table :static_arps do |table|
+      table.column :rule_id,         :integer
+      table.column :hostname,          :string
+      table.column :hw_addr,         :string
+      table.column :name,            :string
+      table.column :category,        :string
+      table.column :description,     :string
+      table.column :live,            :boolean
+      table.column :alert,           :boolean
+      table.column :log,             :boolean
+      table.column :settings_id,     :boolean
+    end
+  end
+
+  def self.down
+    drop_table :static_arps
+  end
+end
