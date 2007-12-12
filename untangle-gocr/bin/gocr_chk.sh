03badfa3c5cbb64118aaf6e6431d7b384addefd0
Sebastien Delafond <sdelafond@gmail.com>
2007-12-12 19:47:48 +0000

    Retired.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@13631 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-gocr/bin/gocr_chk.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-GOCR=gocr
-if test -x ./gocr; then GOCR=./gocr; fi
-for ifile in $*; do
-  tfile=$(echo ${ifile} | sed 's/\.gz$//')
-  tfile=$(echo ${tfile} | sed 's/\.p.m$/\.txt/')
-  echo -ne "test ${ifile} ${tfile}               \n"
-  if ${GOCR} ${ifile} | diff -u ${tfile} -; then true; else echo ""; fi
-done
