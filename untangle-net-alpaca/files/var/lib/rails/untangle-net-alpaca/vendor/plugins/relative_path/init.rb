8e787f4d3c97b4613f27fd8586f183b2e4a824ee
Robert Scott <rbscott@untangle.com>
2007-12-20 01:41:57 +0000

    Relative path didn't really work too well
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@13891 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/vendor/plugins/relative_path/init.rb
+++
@@ -1,9 +0,0 @@
-#
-# relative path
-# This plugin enables you to use relative-path for url-for and 
-# redirect-to 
-#
-# MIT License
-# cuzic <cuzic@cuzic.com>
-# 
-require_dependency 'relative_path'
