6a05533faaaff99aa0d5e137239e6629f6272283
Tom Belote <tbelote@untangle.com>
2008-01-14 21:54:39 +0000

    fixes to spacing and do not send plain text password for pppoe, also fixes to setting dns server.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@14113 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/app/models/intf_pppoe.rb
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/app/models/intf_pppoe.rb
@@ -1,5 +1,23 @@
 class IntfPppoe < ActiveRecord::Base
+  HiddenPassword = "        "
   def accept( interface, visitor )
     visitor.intf_pppoe( interface, self )
   end
+
+  def password=( pass )
+    if ( pass != IntfPppoe::HiddenPassword )
+      write_attribute( :password, pass )
+    else
+      logger.debug( "Ignoring password set to HiddenPassword" )
+    end
+  end
+
+  def hidden_password
+    pass = read_attribute( :password )
+    if pass.nil? || pass.length == 0
+      return ""
+    end
+    return IntfPppoe::HiddenPassword
+  end
+
 end
