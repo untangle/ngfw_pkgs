e473b8c1779da0991230d36a181ca8660c338a8e
Tom Belote <tbelote@untangle.com>
2008-01-14 22:19:01 +0000

    3565 dont send back password
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@14114 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/app/models/ddclient_settings.rb
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/app/models/ddclient_settings.rb
@@ -1,2 +1,19 @@
 class DdclientSettings < ActiveRecord::Base
+  HiddenPassword = "        "
+
+  def password=( pass )
+    if ( pass != DdclientSettings::HiddenPassword )
+      write_attribute( :password, pass )
+    else
+      logger.debug( "Ignoring password set to HiddenPassword" )
+    end
+  end
+
+  def hidden_password
+    pass = read_attribute( :password )
+    if pass.nil? || pass.length == 0
+      return ""
+    end
+    return DdclientSettings::HiddenPassword
+  end
 end
