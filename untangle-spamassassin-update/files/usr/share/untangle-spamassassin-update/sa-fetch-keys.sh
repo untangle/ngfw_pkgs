f17effdc6ff6f0d14be0108f05b94daa319a2293
Sebastien Delafond <sdelafond@gmail.com>
2008-02-20 22:02:19 +0000

    Fetch GPG keys for sa-update in a cron job to, instead of only in the postinst; also restart SA after sa-update run in the cron.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@14783 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-spamassassin-update/files/usr/share/untangle-spamassassin-update/sa-fetch-keys.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+GPG_KEYS_DIR=/usr/share/untangle-spamassassin-update
+SA_GPG_KEY=${GPG_KEYS_DIR}/sa.gpg
+SARE_GPG_KEY=${GPG_KEYS_DIR}/sare.gpg
+
+# download + import the GPG keys for both the official SA and SARE
+curl --silent -o $SARE_GPG_KEY http://daryl.dostech.ca/sa-update/sare/GPG.KEY
+curl --silent -o $SA_GPG_KEY http://spamassassin.apache.org/updates/GPG.KEY
+chmod 700 /etc/spamassassin/sa-update-keys
+sa-update --import $SA_GPG_KEY --import $SARE_GPG_KEY 2> /dev/null
