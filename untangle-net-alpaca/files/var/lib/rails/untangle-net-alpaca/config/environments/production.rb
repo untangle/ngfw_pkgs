0e9b7c4784674208c9e2b07a7c8b7fdc90c8ade5
Robert Scott <rbscott@untangle.com>
2008-03-17 23:11:29 +0000

    Fixing the location of the log files in the alpaca
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@15288 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/config/environments/production.rb
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/config/environments/production.rb
@@ -31,5 +31,7 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
+config.log_path = '/var/log/untangle-net-alpaca/production.log'
+
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
