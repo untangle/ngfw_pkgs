a86336363e7980c47704e9277e5e4f781a4eaf58
Robert Scott <rbscott@untangle.com>
2008-05-06 22:22:15 +0000

    Automatically converting aliases that end in .0 to route entries Closes: #4116
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@16131 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/lib/os_library/routes_manager.rb
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/lib/os_library/routes_manager.rb
@@ -54,7 +54,11 @@ class OSLibrary::RoutesManager < Alpaca::OS::ManagerBase
   end
 
   def hook_write_files
-    network_routes = NetworkRoute.find( :all )
+    ## Retrieve all of the routes that are defined in interfaces by
+    ## bogus addresses that end in .0
+    network_routes = get_interface_routes
+    
+    network_routes += [ NetworkRoute.find( :all ) ].flatten
     
     cfg = []
 
@@ -65,13 +69,17 @@ class OSLibrary::RoutesManager < Alpaca::OS::ManagerBase
 
       next if IPAddr.parse_ip( target ).nil?
       netmask = IPAddr.parse_netmask( netmask )
-      next if IPAddr.parse_ip( gateway ).nil?
+      next if network_route.is_a?( NetworkRoute ) && IPAddr.parse_ip( gateway ).nil?
 
       ## Automatically mask off the necessary bits
       target = IPAddr.parse( "#{target}/#{netmask}" )
       next if target.nil? || netmask.nil?
 
-      cfg << "route add -net #{target} netmask #{netmask} gw #{gateway}"
+      if network_route.is_a?( InterfaceRoute )
+        cfg << "route add -net #{target} netmask #{netmask} dev #{gateway}"
+      else
+        cfg << "route add -net #{target} netmask #{netmask} gw #{gateway}"
+      end
     end
     
     os["override_manager"].write_file( ConfigFile, header, "\n", cfg.join( "\n" ), "\n" )
@@ -90,5 +98,86 @@ class OSLibrary::RoutesManager < Alpaca::OS::ManagerBase
 ## If you modify this file manually, your changes
 ## may be overriden
 EOF
+  end
+
+  def get_interface_routes
+    interface_routes = []
+    interfaces = Interface.find( :all )
+    route_visitor = InterfaceRouteVisitor.new
+
+    interfaces.each do |interface|
+      interface_routes += interface.current_config.accept( interface, route_visitor )
+    end
+
+    interface_routes
+  end
+
+  
+  class InterfaceRouteVisitor < Interface::ConfigVisitor
+    def intf_static( interface, config )
+      ## Create a copy of the array.
+      interface_routes( interface, config.ip_networks )
+    end
+
+    def intf_dynamic( interface, config )
+      interface_routes( interface, config.ip_networks )
+    end
+
+    def intf_bridge( interface, config )
+      []
+    end
+
+    def intf_pppoe( interface, config )
+      interface_routes( interface, config.ip_networks )
+    end
+
+    ## This should be moved into a central place as something close is
+    ## used in the network manager.
+    def interface_routes( interface, ip_networks )
+      bridged_interfaces = interface.bridged_interface_array
+      os_name = interface.os_name
+      os_name = "br.#{os_name}" if ( !bridged_interfaces.nil? && !bridged_interfaces.empty? )
+
+      ## Copy the array of ip_networks
+      ip_networks = [ ip_networks  ].flatten
+
+      ## Copy all of the ip networks
+      ip_networks = ip_networks.map { |i| i.clone }
+
+      ## Remove any interfaces that are not valid.
+      ip_networks.delete_if { |ip_network| !is_route_network?( ip_network ) }
+
+      ## Convert to Interface Routes
+      ip_networks.map { |ipn| InterfaceRoute.new( ipn, os_name ) }
+    end
+    
+    def is_route_network?( ip_network )
+      return false if ApplicationHelper.null?( ip_network.ip )
+      return false if ApplicationHelper.null?( ip_network.netmask )
+
+      return false if IPAddr.parse_ip( ip_network.ip ).nil?
+      return false if IPAddr.parse_netmask( ip_network.netmask ).nil?
+
+      return false if ( ip_network.ip == "0.0.0.0" || ip_network.ip == "255.255.255.255" )
+      return false if ( ip_network.netmask == "0.0.0.0" )
+      
+      return true if /\.0$/.match( ip_network.ip )
+      return true if /\.255$/.match( ip_network.ip )
+
+      false
+    end
+    
+  end
+
+
+  ## All kinds of badness
+  class InterfaceRoute
+    def initialize( ip_network, os_name )
+      @target = ip_network.ip
+      @netmask = ip_network.netmask
+      @gateway = os_name
+    end
+
+    attr_reader :target, :netmask, :gateway
   end
 end
