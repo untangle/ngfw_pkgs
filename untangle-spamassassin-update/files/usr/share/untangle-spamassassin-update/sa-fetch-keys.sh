160914066e77ab7a1f0c9713700e7d30c412f2a6
Sebastien Delafond <sdelafond@gmail.com>
2008-05-28 20:41:52 +0000

    Add SOUGHT rules (Closes: #3935); only restart SA if some rules were actually updated.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@16460 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-spamassassin-update/files/usr/share/untangle-spamassassin-update/sa-fetch-keys.sh
+++ untangle-spamassassin-update/files/usr/share/untangle-spamassassin-update/sa-fetch-keys.sh
@@ -3,11 +3,15 @@
 GPG_KEYS_DIR=/usr/share/untangle-spamassassin-update
 SA_GPG_KEY=${GPG_KEYS_DIR}/sa.gpg
 SARE_GPG_KEY=${GPG_KEYS_DIR}/sare.gpg
+SOUGHT_GPG_KEY=${GPG_KEYS_DIR}/sought.gpg
 
-# download + import the GPG keys for both the official SA and SARE
+# download + import the GPG keys for both the official SA, SARE & SOUGHT
 curl --silent -o $SARE_GPG_KEY http://daryl.dostech.ca/sa-update/sare/GPG.KEY
 curl --silent -o $SA_GPG_KEY http://spamassassin.apache.org/updates/GPG.KEY
+curl --silent -o $SOUGHT_GPG_KEY http://yerp.org/rules/GPG.KEY
+
 chmod 700 /etc/spamassassin/sa-update-keys
 
 sa-update --import $SA_GPG_KEY 2> /dev/null
 sa-update --import $SARE_GPG_KEY 2> /dev/null
+sa-update --import $SOUGHT_GPG_KEY 2> /dev/null
