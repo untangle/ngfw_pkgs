957f25b3ca6c8154beb23b641701748bbfed8166
Robert Scott <rbscott@untangle.com>
2008-06-01 23:29:53 +0000

    Inserting the first rev, builds and runs ... :)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@16508 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-barfight/src/bouncer/config.h
@@ -0,0 +1,110 @@
+/*
+ * $HeadURL: svn://chef/work/src/libnetcap/src/barfight_shield_cfg.h $
+ * Copyright (c) 2003-2007 Untangle, Inc. 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __BARFIGHT_BOUNCER_CONFIG_H_
+#define __BARFIGHT_BOUNCER_CONFIG_H_
+
+#include <json/json.h>
+
+typedef double nc_shield_score_t;
+
+typedef struct {
+    double lax;
+    double tight;
+    double closed;
+} nc_shield_limit_t;
+
+typedef struct {
+    /* Probability the fence post is used, if it isn't the level below is used */
+    double          prob;
+    nc_shield_score_t post;
+} nc_shield_post_t;
+
+typedef struct {
+    /* Percent to inhert at each node */
+    double           inheritance;
+    /* IP access should be limited */
+    nc_shield_post_t limited;
+    /* IP should not be given access */
+    nc_shield_post_t closed;
+    /* IP is using an excessive amount of resources and may be notified */
+    nc_shield_post_t error;
+} nc_shield_fence_t;
+
+typedef struct {
+    struct {
+        nc_shield_limit_t cpu_load;
+        nc_shield_limit_t sessions;
+        nc_shield_limit_t request_load;
+        nc_shield_limit_t session_load;
+        nc_shield_limit_t tcp_chk_load;
+        nc_shield_limit_t udp_chk_load;
+        nc_shield_limit_t icmp_chk_load;
+        nc_shield_limit_t evil_load;
+    } limit;
+    
+    struct {
+        double request_load;
+        double session_load;
+        double evil_load;
+        double tcp_chk_load;
+        double udp_chk_load;
+        double icmp_chk_load;
+        double active_sess;
+    } mult;
+
+    struct {
+        int low_water;
+        int high_water;
+
+        /** Parameters to control the removal of "dead" nodes */
+        /** Number of items to look through at the end of the LRU for inactive nodes */
+        int sieve_size;
+    
+        /** Rate at which active nodes should be moved to the front of the LRU */
+        double ip_rate;
+    } lru;
+        
+    struct {
+        nc_shield_fence_t relaxed;
+        nc_shield_fence_t lax;
+        nc_shield_fence_t tight;
+        nc_shield_fence_t closed;
+    } fence;
+
+    /* Used to track how often to print reputation debugging */
+    int print_delay;
+
+    /** If a reputation exceeds this threshold, debugging messages are
+     * printed out for a reputation. */
+    double rep_threshold;
+} bouncer_shield_config_t;
+
+/* Load the default shield configuration */
+int bouncer_shield_config_default( bouncer_shield_config_t* config );
+
+/* This parser buffer as a JSON object and loads it */
+int bouncer_shield_config_load( bouncer_shield_config_t* config, char* buffer, int buffer_len );
+
+int bouncer_shield_config_load_json( bouncer_shield_config_t* config, struct json_object* object );
+
+/* Convert the current configuration to a JSON string */
+struct json_object* bouncer_shield_config_to_json( bouncer_shield_config_t* config );
+
+#endif // __BARFIGHT_BOUNCER_CONFIG_H_
+
---
+++ untangle-barfight/src/net/packet.c
@@ -0,0 +1,110 @@
+/*
+ * $HeadURL: svn://chef/work/src/libnetcap/src/netcap_shield.c $
+ * Copyright (c) 2003-2007 Untangle, Inc. 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <stdlib.h>
+
+#include <mvutil/debug.h>
+#include <mvutil/errlog.h>
+
+#include "net/packet.h"
+
+barfight_net_packet_t* barfight_net_packet_malloc( void )
+{
+    barfight_net_packet_t* packet = NULL;
+    
+    if (( packet = malloc( sizeof( barfight_net_packet_t ))) == NULL ) return errlogmalloc_null();
+
+    packet->bucket_size = 0;
+    packet->bucket_limit = 0;
+    packet->bucket = NULL;
+    
+    return packet;
+}
+
+int barfight_net_packet_init( barfight_net_packet_t* packet, int bucket_size )
+{
+    if ( packet == NULL ) return errlogargs();
+
+    if ( bucket_size <= 0 ) return errlogargs();
+
+    if ( bucket_size > MAX_BUCKET_SIZE ) return errlogargs();
+
+    bzero( packet, sizeof( barfight_net_packet_t ));
+    
+    u_char* bucket = NULL;
+
+    if (( bucket = malloc( bucket_size )) == NULL ) return errlogmalloc();
+    
+    packet->bucket = bucket;
+    packet->bucket_limit = bucket_size;
+    
+    return 0;
+}
+
+barfight_net_packet_t* barfight_net_packet_create( int bucket_size )
+{
+    barfight_net_packet_t* packet = NULL;
+
+    if (( packet = barfight_net_packet_malloc()) == NULL ) {
+        return errlog_null( ERR_CRITICAL, "barfight_net_packet_malloc\n" );
+    }
+
+    if ( barfight_net_packet_init( packet, bucket_size ) < 0 ) {
+        barfight_net_packet_raze( packet );
+        return errlog_null( ERR_CRITICAL, "barfight_net_packet_init\n" );
+    }
+    
+    return packet;
+}
+
+void barfight_net_packet_raze( barfight_net_packet_t* packet )
+{
+    barfight_net_packet_destroy( packet );
+    barfight_net_packet_free( packet );
+}
+
+void barfight_net_packet_destroy( barfight_net_packet_t* packet )
+{
+    if ( packet == NULL ) {
+        errlogargs();
+        return;
+    }
+    
+    packet->ip_header = NULL;
+    packet->enc_packet = NULL;
+    packet->enc_packet_size = 0;
+    if ( packet->nf_packet_id != 0 ) {
+        errlog( ERR_WARNING, "The packet %d has a non-zero packet_id\n", packet->nf_packet_id );
+        packet->nf_packet_id = 0;
+    }
+
+    packet->nfqueue = NULL;
+    if ( packet->bucket != NULL ) free( packet->bucket );
+    packet->bucket_size = 0;
+    packet->bucket_limit = 0;
+}
+
+void barfight_net_packet_free( barfight_net_packet_t* packet )
+{
+    if ( packet == NULL ) {
+        errlogargs();
+        return;
+    }
+
+    free( packet );
+}
