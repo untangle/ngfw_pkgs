54f94a72ad3008e2c68bfad8762f275f60bfaacf
Robert Scott <rbscott@untangle.com>
2008-06-03 19:19:44 +0000

    seb would not be happy.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@16545 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-barfight/src/main.c
+++ untangle-barfight/src/main.c
@@ -45,7 +45,7 @@
 
 #include "json/server.h"
 
-#define DEFAULT_CONFIG_FILE  "/var/lib/barfight.js"
+#define DEFAULT_CONFIG_FILE  "/etc/barfight.conf"
 #define DEFAULT_DEBUG_LEVEL  5
 #define DEFAULT_BIND_PORT 3001
 
