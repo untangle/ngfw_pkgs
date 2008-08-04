9841bf49d7cd070362d6bdeceb24f57024b13442
Aaron Read <amread@untangle.com>
2008-08-04 23:53:09 +0000

    detect local root user
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/web-ui/work/pkgs@18024 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-apache2-config/uvmlogin.py
+++ untangle-apache2-config/uvmlogin.py
@@ -4,7 +4,16 @@ import urllib
 from mod_python import apache, Session, util
 
 def authenhandler(req):
-    return apache.OK
+    if req.notes.has_key('authorized') and req.notes['authorized'] == 'true':
+        return apache.OK
+    else:
+        options = req.get_options()
+        if options.has_key('Realm'):
+            realm = options['Realm']
+            login_redirect(req, realm)
+        else:
+            apache.log_error('no realm specified')
+            return apache.DECLINED
 
 def headerparserhandler(req):
     options = req.get_options()
@@ -16,19 +25,54 @@ def headerparserhandler(req):
 
     sess = Session.Session(req)
     sess.save()
+
+    username = session_user(sess, realm)
+
+    if None == username and is_root(req):
+        username = 'localadmin'
+
+    if None != username:
+        pw = base64.encodestring('%s' % username).strip()
+        req.headers_in['Authorization'] = "BASIC % s" % pw
+        req.notes['authorized'] = 'true'
+        return apache.OK
+    else:
+        login_redirect(req, realm)
+
+def session_user(sess, realm):
     if sess.has_key('apache_realms') and sess['apache_realms'].has_key(realm):
         realm_record = sess['apache_realms'][realm]
 
         if realm_record != None and realm_record.has_key('username'):
-            username = realm_record['username']
-            password = sess.id()
-            pw = base64.encodestring('%s:%s' % (username, password)).strip()
-            req.headers_in['Authorization'] = "BASIC % s" % pw
-            return apache.OK
-        else:
-            login_redirect(req, realm)
-    else:
-        login_redirect(req, realm)
+            return realm_record['username']
+
+    return None
+
+def is_root(req):
+    (remote_ip, remote_port) = req.connection.remote_addr
+
+    result = False;
+
+    if remote_ip == "127.0.0.1":
+        q = remote_ip.split(".")
+        q.reverse()
+        n = reduce(lambda a, b: long(a) * 256 + long(b), q)
+        hexaddr = "%08X" % n
+        hexport = "%04X" % remote_port
+
+        infile = open('/proc/net/tcp', 'r')
+        for l in infile:
+            a = l.split()
+            if len(a) > 2:
+                p = a[1].split(':')
+                if len(p) == 2 and p[0] == hexaddr and p[1] == hexport:
+                    uid = a[7]
+                    if uid == '0':
+                        result = True
+                        break
+        infile.close()
+
+    return result
 
 def login_redirect(req, realm):
     url = urllib.quote(req.unparsed_uri)
