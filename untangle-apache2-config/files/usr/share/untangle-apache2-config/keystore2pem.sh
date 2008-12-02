51f044c2490041dbdfda8678371263e33669b093
John Irwin <jdi@untangle.com>
2008-12-02 02:47:01 +0000

    Closes: #5544 Use hostname without -f to get the correct cert Compile for java5 since we'll use java5 at upgrade time due to PATH migration script now -e, pem removed on failure
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-6.0/work/pkgs@20735 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-apache2-config/files/usr/share/untangle-apache2-config/keystore2pem.sh
+++ untangle-apache2-config/files/usr/share/untangle-apache2-config/keystore2pem.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
+set -e
+
 KEYSTORE=$1
 PEM=$2
-ALIAS=${3:=`hostname -f`}
+ALIAS=${3:=`hostname`}
 
 PASSWORD=changeit
 der=`mktemp`
 keytool -export -alias $ALIAS -keystore $KEYSTORE -storepass $PASSWORD -file $der
-
-[ $? = 0 ] || exit 1
 
 openssl x509 -in $der -inform der -out $PEM -outform pem
 rm -f $der
