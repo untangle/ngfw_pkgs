bd759056576d262168dc44e103de9f68ec481ab2
Sebastien Delafond <sdelafond@gmail.com>
2009-01-14 17:50:36 +0000

    Cleanup password-checking code. No more "Knoppix" in /etc/motd.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@21313 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
@@ -2,11 +2,9 @@
 
 XTERM="urxvt"
 
-if [ ! -z `sudo su -c 'grep "^root:\*:" /etc/shadow'` ] || \
-   [ ! -z `sudo su -c 'grep "^root:YKN4WuGxhHpIw:" /etc/shadow'` ] || \
-   [ ! -z `sudo su -c 'grep '^root:$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC.:' /etc/shadow'` ] ; then
+if ! sudo grep -qE '^root:(\*|YKN4WuGxhHpIw|$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC\.):" /etc/shadow' ; then
   # set the password for the first time
   sudo su root -c "${XTERM} -T 'Please choose a password' -e '/usr/bin/passwd'"
 fi
 
-${XTERM} -T ${XTERM} -e bash -c 'su root' &
+${XTERM} -T ${XTERM} -e bash -c 'su - root' &
