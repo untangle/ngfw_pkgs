cabd9fa27eddf9818548e86fb69f2f6633b74e89
Sebastien Delafond <sdelafond@gmail.com>
2009-01-14 01:45:54 +0000

    Move over to urxvt.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@21298 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
@@ -1,20 +1,12 @@
 #! /bin/bash
 
-if [ ! -z `sudo su -c 'grep "^root:\*:" /etc/shadow'` ] ; then
-    # set the password for the first time
-    sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
+XTERM="urxvt"
+
+if [ ! -z `sudo su -c 'grep "^root:\*:" /etc/shadow'` ] || \
+   [ ! -z `sudo su -c 'grep "^root:YKN4WuGxhHpIw:" /etc/shadow'` ] || \
+   [ ! -z `sudo su -c 'grep '^root:$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC.:' /etc/shadow'` ] ; then
+  # set the password for the first time
+  sudo su root -c "${XTERM} -T 'Please choose a password' -e '/usr/bin/passwd'"
 fi
 
-# Also check for the old default password
-if [ ! -z `sudo su -c 'grep "^root:YKN4WuGxhHpIw:" /etc/shadow'` ] ; then
-    # set the password for the first time
-    sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
-fi
-
-# Also check for the old default password
-if [ ! -z `sudo su -c 'grep '^root:$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC.:' /etc/shadow'` ] ; then
-    # set the password for the first time
-    sudo su root -c "xterm -T 'Please choose a password' -e '/usr/bin/passwd'"
-fi
-    
-xterm -T xterm -e bash -c 'true && su root' &
+${XTERM} -T ${XTERM} -e bash -c 'su root' &
