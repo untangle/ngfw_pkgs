161d3dbd61518bbfdca65c5c810b73315850da9e
Sebastien Delafond <sdelafond@gmail.com>
2009-02-25 02:36:21 +0000

    xterm, it's more than time you step out of the stage and let urxvt shine (Closes: #5815).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@22133 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-restore.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-restore.sh
@@ -3,7 +3,7 @@
 Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop Untangle services." 15 60
 
 if [ $? -eq 0 ]; then
-    xterm -T "Untangle Recovery" -e "sudo /usr/share/untangle/bin/ut-restore"
+    urxvt -T "Untangle Recovery" -e "sudo /usr/share/untangle/bin/ut-restore"
 else
     echo "No shutting down"
 fi
