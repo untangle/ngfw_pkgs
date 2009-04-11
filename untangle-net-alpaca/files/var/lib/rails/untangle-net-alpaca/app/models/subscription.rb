44461f1ad7baaa981e698695dd3d27b591cba4f0
Robert Scott <rbscott@untangle.com>
2009-04-11 03:11:18 +0000

    Automatically add tcp and udp to bypass rules if a oport is specified.  Closes: #6190
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@22880 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/app/models/subscription.rb
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/app/models/subscription.rb
@@ -17,7 +17,14 @@
 #
 class Subscription < ActiveRecord::Base
   include Alpaca::ActiveRecordExtensions
-  
+
+  def before_save
+    f = self.filter
+    if ( /port::/.match( f ) && /protocol::/.match( f ).nil? )
+      self.filter << "&&protocol::tcp,udp"
+    end
+  end
+
   def self.order_field
     "position"
   end
