def6b35cff986d1f360118c160224b1216d31221
Sebastien Delafond <sdelafond@gmail.com>
2009-07-06 16:56:02 +0000

    Ruby, you mind-corrupting sob.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@23830 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-postgresql-config/files/usr/share/untangle/tests/untangle-postgresql-config/test_pg.py
+++ untangle-postgresql-config/files/usr/share/untangle/tests/untangle-postgresql-config/test_pg.py
@@ -6,5 +6,5 @@ class TestPostgres:
   PROPER_VERSION = "8.3"
 
   def test_version(test):
-    assert get_package_version("postgresql-%s" % (OLDER_VERSION,)) is None
-    assert get_package_version("postgresql-%s" % (PROPER_VERSION,)) is not None
+    assert get_package_version("postgresql-%s" % (TestPostgres.OLDER_VERSION,)) is None
+    assert get_package_version("postgresql-%s" % (TestPostgres.PROPER_VERSION,)) is not None
