a7d8761b349ff2d9612d26071f2082ae9c6ad545
Sebastien Delafond <sdelafond@gmail.com>
2009-07-06 16:50:08 +0000

    Bah, let's be serious for a second here.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@23828 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-postgresql-config/files/usr/share/untangle/tests/untangle-postgresql-config/test_pg.py
+++ untangle-postgresql-config/files/usr/share/untangle/tests/untangle-postgresql-config/test_pg.py
@@ -5,6 +5,6 @@ class TestPostgres:
   OLDER_VERSION = "7.4"
   PROPER_VERSION = "8.3"
 
-  def test_version():
+  def test_version(test):
     assert get_package_version("postgresql-%s" % (OLDER_VERSION,)) is None
     assert get_package_version("postgresql-%s" % (PROPER_VERSION,)) is not None
