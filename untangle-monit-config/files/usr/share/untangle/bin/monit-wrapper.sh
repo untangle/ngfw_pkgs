bf7239583c47d6858331b872d1e235cff7e6eedc
Sebastien Delafond <sdelafond@gmail.com>
2009-07-20 15:22:09 +0000

    Make sure we start another monit process if the 1st one died.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@23987 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-monit-config/files/usr/share/untangle/bin/monit-wrapper.sh
+++ untangle-monit-config/files/usr/share/untangle/bin/monit-wrapper.sh
@@ -4,9 +4,14 @@ CONFIG_PARTS_DIR=/etc/untangle/monit.d
 CONFIG_FILE=/etc/untangle/monit.conf
 RESTART_FILE=/etc/untangle/monit-to-restart
 
-monit -c $CONFIG_FILE &
+pid=foo
 
 while true ; do
+  if ! ps -p $1 > /dev/null ; then
+    monit -c $CONFIG_FILE &
+    pid=$!
+  fi
+
   sleep 30
   if [ -f $RESTART_FILE ] ; then
     rm -f $RESTART_FILE
