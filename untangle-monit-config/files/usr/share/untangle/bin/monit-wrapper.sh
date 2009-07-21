b9e8d57821e5e449f87b73c42f75a79b707c17db
Sebastien Delafond <sdelafond@gmail.com>
2009-07-21 09:09:33 +0000

    Templating system so we can use shell evaluation in monit config files.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@24002 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-monit-config/files/usr/share/untangle/bin/monit-wrapper.sh
+++ untangle-monit-config/files/usr/share/untangle/bin/monit-wrapper.sh
@@ -3,10 +3,15 @@
 CONFIG_PARTS_DIR=/etc/untangle/monit.d
 CONFIG_FILE=/etc/untangle/monit.conf
 RESTART_FILE=/etc/untangle/monit-to-restart
-
+TEMPLATE_EXTENSION=".template"
 pid=foo
 
 while true ; do
+
+  for file in $CONFIG_PARTS_DIR/*$TEMPLATE_EXTENSION ; do
+    perl -pe 's/`(.+)`/chop($foo = `$1`) ; $foo/e' $file >| ${file/$TEMPLATE_EXTENSION}
+  done
+
   if ! ps -p $1 > /dev/null ; then
     monit -c $CONFIG_FILE &
     pid=$!
