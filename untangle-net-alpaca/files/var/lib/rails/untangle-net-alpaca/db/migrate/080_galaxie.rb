2d9d2f344c55f9c180a9c2a0c643853c3cd335b8
Robert Scott <rbscott@untangle.com>
2009-10-01 02:39:34 +0000

    Updating some language files.  making the network manager work properly for single nic mode.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@24654 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/db/migrate/080_galaxie.rb
@@ -0,0 +1,31 @@
+#
+# $HeadURL$
+# Copyright (c) 2007-2008 Untangle, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+# NONINFRINGEMENT.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+class Dart < Alpaca::Migration
+  def self.up
+    ## Column for storing whether or not hosts in this network should spoof the host.
+    add_column :arp_eater_networks, :is_spoof_host_enabled, :boolean, :default => true
+    
+    ## Column for a list of IP Networks / IP Addresses to apply the special NAT rule to.
+    add_column :arp_eater_settings, :nat_hosts, :string, :default => ""
+  end
+  
+  def self.down
+    remove_column :arp_eater_networks, :is_spoof_host_enabled
+    remove_column :arp_eater_settings, :nat_hosts
+  end
+end
