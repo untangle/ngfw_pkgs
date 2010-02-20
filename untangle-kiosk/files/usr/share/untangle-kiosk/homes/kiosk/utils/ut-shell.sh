4a867bc61a801a7e2dc7b678aeda530bd56d2ccf
Marc Runkel <marc@runkel.org>
2010-02-20 21:20:17 +0000

    First stab at #7478 increasing user friendliness of setting root password.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@25880 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
@@ -4,7 +4,27 @@ XTERM="urxvt"
 
 if sudo grep -qE '^root:(\*|YKN4WuGxhHpIw|$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC\.|CHANGEME):' /etc/shadow ; then
   # set the password for the first time
+  echo "Since this is the first time you are logging into the terminal, you"
+  echo "will now be prompted to set a password for the super-user (root) account."
+  echo "You will need to enter the same password twice to set the new password." 
+  echo 
+  echo "It will not echo your keystrokes to the screen for security."
+  echo "Please choose a password that is not easily guessed, especially if you"
+  echo "have, or plan on enabling ssh access to the system."
+  echo 
   sudo su root -c "${XTERM} -T 'Please choose a password' -e '/usr/bin/passwd'"
+  if sudo grep -qE '^root:(\*|YKN4WuGxhHpIw|$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC\.|CHANGEME):' /etc/shadow ; then
+    # password change failed, output a message and exit
+    echo "Password change failed, exiting."
+    sleep 4
+    exit
+  fi
+else
+  echo "Please enter the super-user password to proceed."
+  echo
+  echo "Note: this passord is not the same as the web interface admin password."
+  echo "It was set the first time the Terminal button was pressed."
+  echo
 fi
 
 ${XTERM} -tn rxvt -T ${XTERM} -e su root &
