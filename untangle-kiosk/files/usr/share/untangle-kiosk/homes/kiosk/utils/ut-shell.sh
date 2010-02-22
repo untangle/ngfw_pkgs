f425ec94fc36a33cf91514f4d6fed646797c3326
Marc Runkel <marc@runkel.org>
2010-02-22 21:23:44 +0000

    svn merge -r 25905:25904  .; didn't intend to merge.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-7.2/work/pkgs@25906 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-shell.sh
@@ -4,25 +4,7 @@ XTERM="urxvt"
 
 if sudo grep -qE '^root:(\*|YKN4WuGxhHpIw|$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC\.|CHANGEME):' /etc/shadow ; then
   # set the password for the first time
-  echo "Since this is the first time you are logging into the terminal, you"
-  echo "will now be prompted to set a password for the super-user (root) account."
-  echo "The system will prompt you to enter the password twice" 
-  echo 
-  echo "It will not echo your keystrokes to the screen for security."
-  echo 
   sudo su root -c "${XTERM} -T 'Please choose a password' -e '/usr/bin/passwd'"
-  if sudo grep -qE '^root:(\*|YKN4WuGxhHpIw|$1$3kRMklXp$W/hDwKvL8GFi5Vdo3jtKC\.|CHANGEME):' /etc/shadow ; then
-    # password change failed, output a message and exit
-    echo "Password change failed, exiting."
-    sleep 4
-    exit
-  fi
-else
-  echo "Please enter the super-user password to proceed."
-  echo
-  echo "Note: this passord is not the same as the web interface admin password."
-  echo "It was set the first time the Terminal button was pressed."
-  echo
 fi
 
 ${XTERM} -tn rxvt -T ${XTERM} -e su root &
