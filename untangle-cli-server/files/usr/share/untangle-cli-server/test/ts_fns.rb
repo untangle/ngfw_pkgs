612cee66adfc8e1dc40c5e90ad4c99b3ddb1d000
Sebastien Delafond <sdelafond@gmail.com>
2010-03-17 11:56:23 +0000

    DIE !!!!!!!! (Closes: #4264).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@26058 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-cli-server/files/usr/share/untangle-cli-server/test/ts_fns.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift File.join(File.dirname(__FILE__), ".")
-require 'test/unit'
-require 'tc_protofilter'
-require 'tc_ips'
-require 'tc_spamblocker'
-require 'tc_phish'
-require 'tc_spyware'
-require 'tc_virus'
