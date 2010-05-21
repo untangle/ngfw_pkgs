6479a0154d8e056f9fe6df1da30c5e37592de2dc
Dirk Morris <dmorris@untangle.com>
2010-05-21 18:53:31 +0000

    remove Untangle references
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-7.3/work/pkgs@26683 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-reboot.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-reboot.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Xdialog --default-no --wrap --title "Reboot" --yesno "Are you sure you want to reboot the Untangle Server?" 15 60
+Xdialog --default-no --wrap --title "Reboot" --yesno "Are you sure you want to reboot the Server?" 15 60
 
 if [ $? -eq 0 ]; then
         sudo /sbin/reboot
--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-restore.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-restore.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop Untangle services." 15 60
+Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop network services." 15 60
 
 if [ $? -eq 0 ]; then
-    urxvt -T "Untangle Recovery" -e sudo /usr/share/untangle/bin/ut-restore
+    urxvt -T "Recovery" -e sudo /usr/share/untangle/bin/ut-restore
 else
     echo "No shutting down"
 fi
