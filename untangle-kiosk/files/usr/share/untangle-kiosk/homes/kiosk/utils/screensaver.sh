bcf79371a63257350161a54516d77aaa6563e235
Sebastien Delafond <sdelafond@gmail.com>
2010-11-11 12:46:49 +0000

    [kiosk] Fix screensaver for Xorg (Closes: #8257).
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@27853 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/screensaver.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/screensaver.sh
@@ -1,25 +1,28 @@
 #! /bin/bash
 
 XSESSION_FILE=/home/kiosk/.xsession
-XSET_S_OPTIONS="s blank s 120 15"
 
-if grep -i -E 'Loading.*drivers' /var/log/XFree86.0.log | grep -q vesa ; then
-  XSET_S_OPTIONS=${XSET_S_OPTIONS/blank/noblank}
+if grep -qiE 'LoadModule.*vesa' /var/log/Xorg.0.log ; then
+  XSET_S_OPTIONS="s noblank"
+else
+  XSET_S_OPTIONS="s blank"
 fi
+
+XSET_DPMS_OPTIONS="dpms 120 240 0"
 
 case $1 in
   on)
     xset $XSET_S_OPTIONS
-    xset dpms
+    xset $XSET_DPMS_OPTIONS
 
-    sed -i "s/xset s.*/xset ${XSET_S_OPTIONS}/" $XSESSION_FILE
-    sed -i "s/xset .dpms/xset dpms/" $XSESSION_FILE
+    perl -i -pe "s/xset s.*/xset ${XSET_S_OPTIONS}/" $XSESSION_FILE
+    perl -i -pe "s/xset .?dpms/xset ${XSET_DPMS_OPTIONS}/" $XSESSION_FILE
     ;;
   off)
     xset s off
     xset -dpms
 
-    sed -i "s/xset s.*/xset s off/" $XSESSION_FILE
-    sed -i "s/xset .dpms/xset -dpms/" $XSESSION_FILE
+    perl -i -pe "s/xset s.*/xset s off/" $XSESSION_FILE
+    perl -i -pe "s/xset .dpms/xset -dpms/" $XSESSION_FILE
     ;;
 esac
