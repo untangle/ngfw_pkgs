0fe45152c02dec66568e57221fbe079c4b85ab00
Dirk Morris <dmorris@untangle.com>
2010-12-22 18:44:51 +0000

    rename ut-restore to ut-restore-files to avoid conflict (Closes: #8366)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@28136 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-restore.sh
+++ untangle-kiosk/files/usr/share/untangle-kiosk/homes/kiosk/utils/ut-restore.sh
@@ -6,7 +6,7 @@ LANG="en_US"
 Xdialog --default-no --wrap --title "Recovery" --yesno "Are you sure want to run Recovery? It will stop network services." 15 60
 
 if [ $? -eq 0 ]; then
-    urxvt -T "Recovery" -e sudo /usr/share/untangle/bin/ut-restore
+    urxvt -T "Recovery" -e sudo /usr/share/untangle/bin/ut-restore-tool
 else
     echo "No shutting down"
 fi
