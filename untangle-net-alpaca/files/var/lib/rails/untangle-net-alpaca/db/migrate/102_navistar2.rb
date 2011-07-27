d4ddb56b6fe36bf9b9820ee2520f56b3dda45320
Dirk Morris <dmorris@untangle.com>
2011-07-27 22:04:34 +0000

    svn merge -r 29377:29378 svn://chef/work/pkgs . (Closes: #8765)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-9.0/work/pkgs@29379 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/db/migrate/102_navistar2.rb
@@ -0,0 +1,21 @@
+# $Id: 102_navistar2.rb,v 1.00 2011/07/27 14:55:15 dmorris Exp $
+class Navistar2 < Alpaca::Migration
+
+  def self.up
+    newrule = Firewall.find( :first, :conditions => [ "system_id = ? and enabled='t'", "route-bridge-traffic-bc218f02" ] )
+
+    # If rules already exist this is an upgrade
+    # This rule should not be enabled on upgrades
+    # but it is enabled by default on new installs
+    if !newrule.nil? 
+      # Disable this rule (because this is an upgrade)
+      newrule.enabled = false
+      newrule.save
+    end
+  end
+  
+  def self.down
+
+  end
+
+end
