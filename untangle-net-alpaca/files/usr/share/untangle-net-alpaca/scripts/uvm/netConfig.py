68b4ba804e4af377174b1747198b080294c1f992
Dirk Morris <dmorris@untangle.com>
2011-08-01 21:17:55 +0000

    svn merge -r 29398:29399 svn://chef/work/pkgs . (Comment: #8882)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-9.0/work/pkgs@29400 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/usr/share/untangle-net-alpaca/scripts/uvm/netConfig.py
+++ untangle-net-alpaca/files/usr/share/untangle-net-alpaca/scripts/uvm/netConfig.py
@@ -6,7 +6,12 @@ import simplejson as json
 import sys
 
 if (len(sys.argv) < 2): 
-    print "usage: " + sys.argv[0] + " [wan|non_wan]"
+    print "usage: " + sys.argv[0] + " [wan|non_wan|wan_idx|non_wan_idx]"
+    print "\"wan\"          prints all wan interface names"
+    print "\"non_wan\"      prints all non-wan interface names"
+    print "\"wan_idx\"      prints all wan interface indexes (IDs)"
+    print "\"non_wan_idx\"  prints all non-wan interface indexes (IDs)"
+
     sys.exit(1)
 
 
@@ -24,6 +29,18 @@ if sys.argv[1] == 'non_wan':
     for intf in netConfigObj['interfaceList']['list']:
         if intf['WAN'] != None and intf['WAN'].lower() == 'false':
             print intf['systemName']
+    sys.exit(0)
+
+if sys.argv[1] == 'wan_idx':
+    for intf in netConfigObj['interfaceList']['list']:
+        if intf['WAN'] != None and intf['WAN'].lower() == 'true':
+            print intf['interfaceId']
+    sys.exit(0)
+
+if sys.argv[1] == 'non_wan_idx':
+    for intf in netConfigObj['interfaceList']['list']:
+        if intf['WAN'] != None and intf['WAN'].lower() == 'false':
+            print intf['interfaceId']
     sys.exit(0)
 
 
