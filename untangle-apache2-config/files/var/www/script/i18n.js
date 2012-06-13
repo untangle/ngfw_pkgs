99d41af6627664c785507422876d41ec7a76617e
Vald Dumitrescu <vdumitrescu@untangle.com>
2012-06-13 15:25:09 +0000

    Closes: #10227 24hrs support added,  still need to modify the localization files for each language that uses 24 hrs format
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@32159 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-apache2-config/files/var/www/script/i18n.js
+++ untangle-apache2-config/files/var/www/script/i18n.js
@@ -5,9 +5,9 @@ Ung.i18nModuleInstances = {};
 // I18N object class
 Ung.I18N = Ext.extend(Ext.Component, {
     // map of translations
-    map : null,
+    map: null,
     // initialize I18N component
-    initComponent : function() {
+    initComponent: function() {
         Ung.I18N.superclass.initComponent.call(this);
         if (this.map == null) {
             this.map = {};
@@ -24,23 +24,26 @@ Ung.I18N = Ext.extend(Ext.Component, {
         if (!this.map['timestamp_fmt']) {
             this.map['timestamp_fmt'] = 'Y-m-d g:i:s a';
         }
+        if (!this.map['time_fmt']) {
+            this.map['time_fmt'] = 'g:i a';
+        }
     },
     // translation function
-    _ : function(s) {
+    _: function(s) {
         if (this.map !== null && this.map[s]) {
             return this.map[s];
         }
         return s;
     },
     // pluralize function
-    pluralise : function(s, p, n) {
+    pluralise: function(s, p, n) {
         if (n != 1)
             return this._(p);
         return this._(s);
     },
     // replaces arguments with values, for a string with arguments
     // DEPRECATED - use String.format instead
-    sprintf : function(s) {
+    sprintf: function(s) {
         var bits = s.split('%');
         var out = bits[0];
         var re = /^([ds])(.*)$/;
@@ -59,10 +62,10 @@ Ung.I18N = Ext.extend(Ext.Component, {
     },
 
     // formats a number with local separators
-    numberFormat : function(v) {
+    numberFormat: function(v) {
         return this.numberFormatSep(v, '.', this.map['decimal_sep'], this.map['thousand_sep']);
     },
-    numberFormatSep : function(v, dec_sep_in, dec_sep_out, thd_sep) {
+    numberFormatSep: function(v, dec_sep_in, dec_sep_out, thd_sep) {
         v += '';
         var dpos = v.indexOf(dec_sep_in);
         var vEnd = '';
@@ -77,13 +80,13 @@ Ung.I18N = Ext.extend(Ext.Component, {
         return v + vEnd;
     },
     // formats a date
-    dateFormat : function(v) {
+    dateFormat: function(v) {
         var date = new Date();
         date.setTime(v.time);
         return Ext.util.Format.date(date, this.map['date_fmt']);
     },
     // formats a timestamp
-    timestampFormat : function(v) {
+    timestampFormat: function(v) {
         if (v==null) {
             return "";
         }
@@ -91,8 +94,11 @@ Ung.I18N = Ext.extend(Ext.Component, {
         date.setTime(v.time);
         return Ext.util.Format.date(date, this.map['timestamp_fmt']);
     },
+    timeFmt: function() {
+        return this.map['time_fmt'];
+    },
     //date long version format
-    dateLongFormat : function (date,format){
+    dateLongFormat: function (date,format){
         var re = /([a-z])/i,
         tokens  = [],
         i = 0;
@@ -103,7 +109,6 @@ Ung.I18N = Ext.extend(Ext.Component, {
             }
         }
         return tokens.join("");
-        
     }
 
 });
@@ -111,10 +116,10 @@ Ung.I18N = Ext.extend(Ext.Component, {
 // module I18N class object
 Ung.ModuleI18N = Ext.extend(Ung.I18N, {
     // module map
-    moduleMap : null,
+    moduleMap: null,
     // translation function tryes to find the word in the moduleMap
     // and if not succesful it tries to find it in the global map
-    _ : function(s) {
+    _: function(s) {
         if (this.moduleMap !== null && this.moduleMap[s]) {
             return this.moduleMap[s];
         }
