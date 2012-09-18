7aa07a0f661a2e9090799acfc0def71efee6a789
Dirk Morris <dmorris@untangle.com>
2012-09-18 04:27:43 +0000

    another attempt at getting i18n right with mod_python
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@33010 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-apache2-config/files/usr/lib/python2.6/uvmlogin.py
+++ untangle-apache2-config/files/usr/lib/python2.6/uvmlogin.py
@@ -260,6 +260,11 @@ def write_error_page(req, msg):
     except:
         pass
 
+    if not type(us) is str:
+        us = us.encode("utf-8")
+    if not type(msg) is str:
+        msg = msg.encode("utf-8")
+
     html = """\
 <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
 <html xmlns=\"http://www.w3.org/1999/xhtml\">
