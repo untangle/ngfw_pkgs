d99b4667630a097b32573c91a4001510c3fcb8af
Dirk Morris <dmorris@untangle.com>
2013-02-28 22:16:36 +0000

    remove net-alpaca
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@34066 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/vendor/plugins/jsonifier/init.rb
+++
@@ -1,2 +0,0 @@
- 
-require 'jsonifier/json_encoding' if Rails::VERSION::STRING == "1.2.6"
--- untangle-net-alpaca/files/var/lib/rails/untangle-net-alpaca/vendor/plugins/jsonifier/test/fixtures/tagging.rb
+++
@@ -1,4 +0,0 @@
-class Tagging < ActiveRecord::Base
-  belongs_to :tag, :include => :tagging
-  belongs_to :taggable, :polymorphic => true
-end
