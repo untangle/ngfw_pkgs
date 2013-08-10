bbcd14978568311c0d6904744171bf3fd2e196a8
Dirk Morris <dmorris@untangle.com>
2013-08-10 16:39:24 +0000

    commit on alin's behalf: he merged qos-get-sessions.py and qos-get-statistics.py into qos-service.py
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@35590 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/share/untangle-netd/bin/qos-service.py
+++ untangle-netd/hier/usr/share/untangle-netd/bin/qos-service.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, getopt, sys, json, subprocess
+import os, getopt, sys, json, subprocess, parse
 
 def usage():
     print """\
@@ -40,6 +40,14 @@ def run( cmd, ignore_errors=False, print_cmd=False ):
     ret = os.system( cmd )
     if ret != 0 and not ignore_errors:
         print("ERROR: Command failed: %i \"%s\"" % (ret, cmd))
+
+def runSubprocess(cmd):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    result=[]
+    for line in proc.stdout:
+        result.append(line)
+    return result
+
 
 def flush_htb_rules( wan_intfs ):
     #print "Flushing HTB..."
@@ -166,14 +174,64 @@ def start( qos_settings, wan_intfs ):
         add_htb_rules( qos_settings, wan_intf )
     
     sys.exit(0)
+    
+def statusToJSON(input):
+    #Parse patterns for qos-service.py status output
+    firstLine='interface: {} class {} {} {} rate {} ceil {} burst {} cburst {}'
+    secondLine=' Sent {:d} bytes {:d} pkt (dropped {:d}, overlimits {:d} requeues {:d}) '
+    thirdLine=' rate {} {} backlog {} {} requeues {:d} '
+    fourthLine= ' lended: {:d} borrowed: {:d} giants: {:d}'
+    lastLine=' tokens: {} ctokens:{}'
+    priorityParser='parent {} leaf {} prio {:d}'
+    indexMap={1:firstLine, 2:secondLine, 3:thirdLine, 4:fourthLine, 5:lastLine}
+    priorityQueueToName = { '10:': '0 - Default','11:': '1 - Very High','12:': '2 - High', '13:':'3 - Medium','14:':'4 - Low','15:':'5 - Limited','16:':'6 - Limited More','17:':'7 - Limited Severely' }
+
+    output = []
+    count = 1
+    entry = {}
+    skipEntry=False
+    for line in input:
+        if count <= 5:
+            res=parse.parse(indexMap[count],line)
+            parsed = res.fixed
+            if count == 1:
+                entry['interface_name']=parsed[0]
+                entry['burst']=parsed[len(parsed)-2]
+                if 'prio' in parsed[3]:
+                    skipEntry=False
+                    prioParse = parse.parse(priorityParser, parsed[3])
+                    val=prioParse.fixed
+                    entry['priority']=priorityQueueToName[val[1]]
+                if parsed[3]=='root':
+                    skipEntry=True
+            if count == 2:
+                entry['sent']=str(parsed[0]) + ' bytes'
+            if count == 3:
+                entry['rate']=parsed[0]
+            if count == 5:
+                entry['tokens']=int(parsed[0])
+                entry['ctokens']=int(parsed[1])
+            count+=1
+        else:
+            if not skipEntry:
+                output.append(dict(entry))
+            entry.clear()
+            count=1
+            continue
+    newlist = sorted(output, key=lambda k: k['priority'])
+    return json.dumps(newlist)
+    
 
 def status( qos_interfaces, wan_intfs ):
+
+    result=''
     for wan_intf in wan_intfs:
         wan_dev = wan_intf.get('systemDev')
         imq_dev = wan_intf.get('imqDev')
         wan_name = wan_intf.get('name')
-        run( "tc -s class ls dev %s | sed \"s/^class/interface: %s Outbound class/\"" % (wan_dev, wan_name) )
-        run( "tc -s class ls dev %s | sed \"s/^class/interface: %s Inbound class/\"" % (imq_dev, wan_name) )
+        result= runSubprocess( "tc -s class ls dev %s | sed \"s/^class/interface: %s Outbound class/\"" % (wan_dev, wan_name) )
+        result.extend( runSubprocess( "tc -s class ls dev %s | sed \"s/^class/interface: %s Inbound class/\"" % (imq_dev, wan_name)))
+       
         #run("echo ------ Qdisc  ------")
         #run("tc -s qdisc ls dev %s" % wan_dev)
         #run("tc -s qdisc ls dev %s" % imq_dev)
@@ -183,11 +241,29 @@ def status( qos_interfaces, wan_intfs ):
         #run("echo ------ Filter ------")
         #run("tc -s filter ls dev %s" % wan_dev)
         #run("tc -s filter ls dev %s" % imq_dev)
+    print statusToJSON(result)
+        
 
+def sessionsToJSON(input):
+    #Parse patterns for qos-service.py sessions output
+    parsePattern='proto {} state {} src {} dst {} src-port {:d} dst-port {:d} packets {:d} bytes {:d} priority {:d}'
+    entryKeys=('proto','state','src','dst','src_port','dst_port','packets','bytes','priority')
+    output=[]
+    entry={}
+    for line in input:
+        res=parse.parse(parsePattern,line[:-1])
+        if res:
+            parsed = res.fixed
+            output.append(dict(zip(entryKeys,parsed)))
+    return json.dumps(output)
+    
+        
+        
 def sessions( qos_interfaces, wan_intfs ):
-    run( r"""cat /proc/net/ip_conntrack | grep -v '127.0.0.1' | grep tcp | sed 's/[a-z]*=//g' | awk '{printf "proto %s state %s src %s dst %s src-port %s dst-port %s packets %s bytes %s priority %x\n", $1, $4, $5, $6, $7, $8, $9, $10, rshift(and($18,0x000F0000),16)}'""")
-    run( r"""cat /proc/net/ip_conntrack | grep -v '127.0.0.1' | grep udp | sed 's/\[.*\]//g' | sed 's/[a-z]*=//g' | awk '{printf "proto %s state xx src %s dst %s src-port %s dst-port %s packets %s bytes %s priority %x\n", $1, $4, $5, $6, $7, $8, $9, rshift(and($16,0x000F0000),16)}'""")
 
+    result = runSubprocess( r"""cat /proc/net/ip_conntrack | grep -v '127.0.0.1' | grep tcp | sed 's/[a-z]*=//g' | awk '{printf "proto %s state %s src %s dst %s src-port %s dst-port %s packets %s bytes %s priority %x\n", $1, $4, $5, $6, $7, $8, $9, $10, rshift(and($18,0x000F0000),16)}'""")
+    result.extend(runSubprocess( r"""cat /proc/net/ip_conntrack | grep -v '127.0.0.1' | grep udp | sed 's/\[.*\]//g' | sed 's/[a-z]*=//g' | awk '{printf "proto %s state xx src %s dst %s src-port %s dst-port %s packets %s bytes %s priority %x\n", $1, $4, $5, $6, $7, $8, $9, rshift(and($16,0x000F0000),16)}'"""))
+    print sessionsToJSON(result)
     
 #
 # Main
