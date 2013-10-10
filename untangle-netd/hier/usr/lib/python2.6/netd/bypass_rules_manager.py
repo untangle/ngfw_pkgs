4cf7b303a1af83a6a4aca3bd02fe1fe692d607c0
Dirk Morris <dmorris@untangle.com>
2013-10-10 22:42:59 +0000

    do not broute 224.0.0.0/8 traffic and bypass it
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@36150 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/lib/python2.6/netd/bypass_rules_manager.py
+++ untangle-netd/hier/usr/lib/python2.6/netd/bypass_rules_manager.py
@@ -73,7 +73,7 @@ class BypassRuleManager:
         # We bypass routing protocols
         # http://www.iana.org/assignments/multicast-addresses/multicast-addresses.xhtml
         self.file.write("# Implicit Bypass Rules (RIP and routing protocols)" + "\n");
-        self.file.write("${IPTABLES} -t filter -A bypass-rules -p udp --source-port 67:68 --destination-port 67:68 --goto set-bypass-mark -m comment --comment \"Bypass DHCP traffic\"" + "\n");
+        self.file.write("${IPTABLES} -t filter -A bypass-rules --destination 224.0.0.0/8 --goto set-bypass-mark -m comment --comment \"Bypass 224 traffic\"" + "\n");
         self.file.write("\n");
 
         # We bypass 'hairpin' traffic because end-pointing it at layer 7 wouldn't work because two sessions would have identical tuples 
