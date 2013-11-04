d1b9b0aeb75021f7fbea06535a1cca6f2b4aeabe
Dirk Morris <dmorris@untangle.com>
2013-11-04 21:06:43 +0000

    svn merge -r36320:36321 svn://chef/work/pkgs/ . (never NAT loopback traffic, even if the user adds rules that do it)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/prod/release-10.0/work/pkgs@36322 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/lib/python2.6/netd/nat_rules_manager.py
+++ untangle-netd/hier/usr/lib/python2.6/netd/nat_rules_manager.py
@@ -180,6 +180,11 @@ class NatRulesManager:
         self.file.write("${IPTABLES} -t nat -A POSTROUTING -m conntrack --ctstate NEW -m comment --comment \"SNAT rules\" -j nat-rules" + "\n");
         self.file.write("\n");
 
+        self.file.write("# Call nat-rules chain from POSTROUTING chain to SNAT traffic" + "\n");
+        self.file.write("${IPTABLES} -t nat -I nat-rules -i lo -j RETURN -m comment --comment \"Never NAT loopback traffic\" >/dev/null 2>&1" + "\n");
+        self.file.write("${IPTABLES} -t nat -I nat-rules -o lo -j RETURN -m comment --comment \"Never NAT loopback traffic\" >/dev/null 2>&1" + "\n");
+        self.file.write("\n");
+
         self.file.write("# Create (if needed) and flush nat-reverse-filter chain" + "\n");
         self.file.write("${IPTABLES} -t filter -N nat-reverse-filter 2>/dev/null" + "\n");
         self.file.write("${IPTABLES} -t filter -F nat-reverse-filter >/dev/null 2>&1" + "\n");
