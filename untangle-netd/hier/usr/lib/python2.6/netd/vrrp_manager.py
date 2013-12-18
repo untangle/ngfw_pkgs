0d4c050effa261496ca834730b46e6b8030fd68a
Dirk Morris <dmorris@untangle.com>
2013-12-18 22:22:01 +0000

    change from vrrpd to keepalived because of route issues (Closes: #11596, #1056)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@36687 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/lib/python2.6/netd/vrrp_manager.py
+++ untangle-netd/hier/usr/lib/python2.6/netd/vrrp_manager.py
@@ -10,8 +10,69 @@ from netd.network_util import NetworkUtil
 # /etc/untangle-netd/post-network-hook.d/200-vrrp
 # based on the settings object passed from sync-settings.py
 class VrrpManager:
+    keepalivedConfFilename = "/etc/keepalived/keepalived.conf"
     postNetworkHookFilename = "/etc/untangle-netd/post-network-hook.d/200-vrrp"
 
+    def write_keepalivd_conf( self, settings, prefix="", verbosity=0 ):
+        
+        filename = prefix + self.keepalivedConfFilename
+        fileDir = os.path.dirname( filename )
+        if not os.path.exists( fileDir ):
+            os.makedirs( fileDir )
+
+        vrrp_interfaces = []
+        for interface_settings in settings['interfaces']['list']:
+            if interface_settings.get('vrrpEnabled'):
+                if not interface_settings.get('vrrpId') or not interface_settings.get('vrrpPriority') or not interface_settings.get('vrrpAddress'):
+                    print "Missing VRRP Config: %s, %s, %s" % (str(vrrpId), str(vrrpPriority), str(vrrpAddress))
+                    continue
+                vrrp_interfaces.append( interface_settings )
+
+        file = open( filename, "w+" )
+        file.write("! Auto Generated on %s\n" % datetime.datetime.now());
+        file.write("! DO NOT EDIT. Changes will be overwritten.\n");
+        file.write("\n\n");
+
+        if len(vrrp_interfaces) < 1:
+            file.flush()
+            file.close()
+            if verbosity > 0: print "VrrpManager: Wrote %s" % filename
+            return
+
+        file.write(r"""
+global_defs {
+}
+""")
+        file.write("\n\n");
+
+        file.write("vrrp_sync_group VRRPGROUP {" + "\n")
+        file.write("\tgroup {" + "\n")
+        for intf in vrrp_interfaces:
+            file.write("\t\tVI_" + str(intf.get('interfaceId')) + "\n")
+        file.write("\t}" + "\n")
+        file.write("}" + "\n")
+        file.write("\n\n");
+
+        for intf in vrrp_interfaces:
+            file.write("vrrp_instance VI_" + str(intf.get('interfaceId')) + " {" + "\n")
+            file.write("\tstate MASTER" + "\n")
+            file.write("\tinterface %s" % str(intf.get('systemDev')) + "\n")
+            file.write("\tlvs_sync_daemon_interface %s" % str(intf.get('systemDev')) + "\n")
+            file.write("\tvirtual_router_id %s" % str(intf.get('vrrpId')) + "\n")
+            file.write("\tpriority %s" % str(intf.get('vrrpPriority')) + "\n")
+            file.write("\tadvert_int 1" + "\n")
+            file.write("\tvirtual_ipaddress {" + "\n")
+            file.write("\t\t%s" % str(intf.get('vrrpAddress')) + "\n")
+            file.write("\t}" + "\n")
+            file.write("}" + "\n")
+            file.write("\n\n");
+        file.write("\n\n");
+
+        file.flush()
+        file.close()
+        if verbosity > 0: print "VrrpManager: Wrote %s" % filename
+        return
+        
     def write_post_network_hook( self, settings, prefix="", verbosity=0 ):
 
         filename = prefix + self.postNetworkHookFilename
@@ -28,21 +89,24 @@ class VrrpManager:
         file.write("\n\n");
 
         file.write(r"""
-if [ ! -z "`pidof vrrpd`" ] ; then
-    killall vrrpd
+if [ ! -z "`pidof keepalived`" ] ; then
+    killall keepalived
 fi
 """)
         file.write("\n\n");
 
+        vrrpEnabled = False
         for interface_settings in settings['interfaces']['list']:
             if interface_settings.get('vrrpEnabled'):
                 if not interface_settings.get('vrrpId') or not interface_settings.get('vrrpPriority') or not interface_settings.get('vrrpAddress'):
                     print "Missing VRRP Config: %s, %s, %s" % (str(vrrpId), str(vrrpPriority), str(vrrpAddress))
                     continue
-                file.write("# Interface %s VRRP" % str(interface_settings.get('interfaceId')) + "\n")
-                file.write("vrrpd -i %s -v %s -D -p %s %s" % ( str(interface_settings.get('systemDev')), str(interface_settings.get('vrrpId')), str(interface_settings.get('vrrpPriority')), str(interface_settings.get('vrrpAddress'))) + "\n")
-                file.write("\n\n");
-        
+                vrrpEnabled = True
+
+        if vrrpEnabled:
+            file.write("/usr/sbin/keepalived --vrrp -f /etc/keepalived/keepalived.conf --dump-conf --log-console --log-detail" + "\n")
+        file.write("\n\n");
+
         file.flush()
         file.close()
 
@@ -55,6 +119,7 @@ fi
 
         if verbosity > 1: print "VrrpManager: sync_settings()"
         
+        self.write_keepalivd_conf( settings, prefix, verbosity )
         self.write_post_network_hook( settings, prefix, verbosity )
 
         return
