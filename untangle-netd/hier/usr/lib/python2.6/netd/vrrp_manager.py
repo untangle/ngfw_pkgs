4858300ebc451e5da040b9aa7321134154ae5791
Dirk Morris <dmorris@untangle.com>
2014-01-29 00:04:48 +0000

    add iptables rule to allow VRRP if enable (Closes: #11647)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@37056 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/lib/python2.6/netd/vrrp_manager.py
+++ untangle-netd/hier/usr/lib/python2.6/netd/vrrp_manager.py
@@ -12,6 +12,8 @@ from netd.network_util import NetworkUtil
 class VrrpManager:
     keepalivedConfFilename = "/etc/keepalived/keepalived.conf"
     postNetworkHookFilename = "/etc/untangle-netd/post-network-hook.d/200-vrrp"
+    iptablesHookFilename = "/etc/untangle-netd/iptables-rules.d/241-vrrp-rules"
+    vrrp_enabled = False
 
     def write_keepalivd_conf( self, settings, prefix="", verbosity=0 ):
         
@@ -95,15 +97,15 @@ fi
 """)
         file.write("\n\n");
 
-        vrrpEnabled = False
+        self.vrrp_enabled = False
         for interface_settings in settings['interfaces']['list']:
             if interface_settings.get('vrrpEnabled'):
                 if not interface_settings.get('vrrpId') or not interface_settings.get('vrrpPriority') or not interface_settings.get('vrrpAddress'):
                     print "Missing VRRP Config: %s, %s, %s" % (str(vrrpId), str(vrrpPriority), str(vrrpAddress))
                     continue
-                vrrpEnabled = True
+                self.vrrp_enabled = True
 
-        if vrrpEnabled:
+        if self.vrrp_enabled:
             file.write("if lsmod | grep -q ip_vs ; then" + "\n")
             file.write("\ttrue # do nothing" + "\n")         
             file.write("else" + "\n")
@@ -132,12 +134,39 @@ fi
 
         return
 
+    def write_iptables_hook( self, settings, prefix="", verbosity=0 ):
+
+        filename = prefix + self.iptablesHookFilename
+        fileDir = os.path.dirname( filename )
+        if not os.path.exists( fileDir ):
+            os.makedirs( fileDir )
+
+        file = open( filename, "w+" )
+        file.write("#!/bin/dash");
+        file.write("\n\n");
+
+        file.write("## Auto Generated on %s\n" % datetime.datetime.now());
+        file.write("## DO NOT EDIT. Changes will be overwritten.\n");
+        file.write("\n\n");
+
+        if self.vrrp_enabled:
+            file.write("${IPTABLES} -t filter -I filter-rules-input -p vrrp -m comment --comment \"Allow VRRP\" -j RETURN" + "\n");
+
+        file.flush()
+        file.close()
+
+        os.system("chmod a+x %s" % filename)
+        if verbosity > 0: print "VrrpManager: Wrote %s" % filename
+
+        return
+
     def sync_settings( self, settings, prefix="", verbosity=0 ):
 
         if verbosity > 1: print "VrrpManager: sync_settings()"
         
         self.write_keepalivd_conf( settings, prefix, verbosity )
         self.write_post_network_hook( settings, prefix, verbosity )
+        self.write_iptables_hook( settings, prefix, verbosity )
 
         return
 
