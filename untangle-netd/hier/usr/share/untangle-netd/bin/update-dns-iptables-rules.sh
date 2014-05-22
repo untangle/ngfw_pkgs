ebc198742756a21a96c2b24d7eb3af56a5a1fae4
Dirk Morris <dmorris@untangle.com>
2014-05-22 23:11:42 +0000

    svn merge -r37808:37800 https://untangle.svn.beanstalkapp.com/ngfw/work/pkgs/ . (improve DNS script logic)
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/release-10.2/work/pkgs@37810 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/share/untangle-netd/bin/update-dns-iptables-rules.sh
+++ untangle-netd/hier/usr/share/untangle-netd/bin/update-dns-iptables-rules.sh
@@ -21,30 +21,21 @@ DEBUG="debug"
 #IPTABLES="debug_iptables"
 IPTABLES="iptables"
 
-flush_rules()
-{
-    WAN_INDEX=$1
-
-    ${IPTABLES} -t mangle -N dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
-    ${IPTABLES} -t mangle -F dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
-    ${IPTABLES} -t mangle -D OUTPUT -j dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
-    ${IPTABLES} -t mangle -I OUTPUT -j dns-rule-wan-${WAN_INDEX} 
-}
-
 add_rule()
 {
     WAN_INDEX=$1
     DNS_ADDR=$2
 
+    ${IPTABLES} -t mangle -N dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
+    ${IPTABLES} -t mangle -F dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
+    ${IPTABLES} -t mangle -D OUTPUT -j dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
+    ${IPTABLES} -t mangle -I OUTPUT -j dns-rule-wan-${WAN_INDEX} 
+
     $DEBUG "Adding DNS rule for ${DNS_ADDR} to WAN ${WAN_INDEX}"
     ${IPTABLES} -t mangle -A dns-rule-wan-${WAN_INDEX} -d ${DNS_ADDR} -m mark --mark 0/0xff00 -m conntrack --ctstate NEW -j MARK --set-mark $((${WAN_INDEX}<<8))/0xff00 -m comment --comment "Vote for traffic to this DNS to go out WAN ${WAN_INDEX}."
 }
 
-awk -F"[ =]" '/server=/ {print $4 " " $2}' /etc/dnsmasq.conf | sed -e 's/uplink.//' | while read line ; do
-   flush_rules $line
-done
-
-awk -F"[ =]" '/server=/ {print $4 " " $2}' /etc/dnsmasq.conf | sed -e 's/uplink.//' | while read line ; do
+awk -F"[ =]" '/server=/ {print sub(/uplink./,"",$4) " " $2}' /etc/dnsmasq.conf | while read line ; do
    add_rule $line
 done
 
