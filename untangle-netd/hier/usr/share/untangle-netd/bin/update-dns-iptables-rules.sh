86a43758efbeac37cd690553f863f4ed9282daf2
Dirk Morris <dmorris@untangle.com>
2014-05-23 00:03:31 +0000

    improve DNS script logic
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/release-10.2/work/pkgs@37811 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-netd/hier/usr/share/untangle-netd/bin/update-dns-iptables-rules.sh
+++ untangle-netd/hier/usr/share/untangle-netd/bin/update-dns-iptables-rules.sh
@@ -21,21 +21,31 @@ DEBUG="debug"
 #IPTABLES="debug_iptables"
 IPTABLES="iptables"
 
-add_rule()
+flush_rules()
 {
-    WAN_INDEX=$1
-    DNS_ADDR=$2
+    DNS_ADDR=$1
+    WAN_INDEX=$2
 
     ${IPTABLES} -t mangle -N dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
     ${IPTABLES} -t mangle -F dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
     ${IPTABLES} -t mangle -D OUTPUT -j dns-rule-wan-${WAN_INDEX} >/dev/null 2>&1
     ${IPTABLES} -t mangle -I OUTPUT -j dns-rule-wan-${WAN_INDEX} 
+}
+
+add_rule()
+{
+    DNS_ADDR=$1
+    WAN_INDEX=$2
 
     $DEBUG "Adding DNS rule for ${DNS_ADDR} to WAN ${WAN_INDEX}"
     ${IPTABLES} -t mangle -A dns-rule-wan-${WAN_INDEX} -d ${DNS_ADDR} -m mark --mark 0/0xff00 -m conntrack --ctstate NEW -j MARK --set-mark $((${WAN_INDEX}<<8))/0xff00 -m comment --comment "Vote for traffic to this DNS to go out WAN ${WAN_INDEX}."
 }
 
-awk -F"[ =]" '/server=/ {print sub(/uplink./,"",$4) " " $2}' /etc/dnsmasq.conf | while read line ; do
+awk '/^(# *)?server=/ { sub( /^(# *)?/, "" ) ; sub( /server=/, "") ; sub(/# uplink./,"") ; print ; next }' /etc/dnsmasq.conf | while read line ; do
+   flush_rules $line
+done
+
+awk '/^(# *)?server=/ { sub( /^(# *)?/, "" ) ; sub( /server=/, "") ; sub(/# uplink./,"") ; print ; next }' /etc/dnsmasq.conf | while read line ; do
    add_rule $line
 done
 
