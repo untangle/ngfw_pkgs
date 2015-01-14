ebf65b8fda8f9f5e38a9fe6deb37bd1e9e4ad252
Chris Blaise <cblaise@untangle.com>
2015-01-14 01:57:20 +0000

    New template directory for default settings based on memory.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@39475 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-snort-config/files/usr/share/untangle-snort-config/current/templates/defaults_1GB.js
@@ -0,0 +1,19 @@
+{
+    "configured" : true,
+    "active_rules" : {
+        "classtypes": [ 
+            "attempted-admin",
+            "attempted-user",
+            "inappropriate-content",
+            "policy-violation",
+            "shellcode-detect",
+            "successful-admin",
+            "successful-user",
+            "trojan-activity",
+            "unsuccessful-user",
+            "web-application-attack"
+        ],
+        "categories": [ 
+        ]
+    }
+}
