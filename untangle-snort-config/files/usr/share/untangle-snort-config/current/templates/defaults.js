b987f0b13a58365bb5ee8127dec0c828f4be110a
Chris Blaise <cblaise@untangle.com>
2015-01-15 16:54:43 +0000

    More defaults (based on memory) for IDPS wizard.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@39498 8ebf8b4e-06f3-0310-961f-89f74618be54

---
+++ untangle-snort-config/files/usr/share/untangle-snort-config/current/templates/defaults.js
@@ -0,0 +1,43 @@
+{
+    "configured" : true,
+    "active_rules" : {
+        "classtypes": [ 
+            "attempted-admin",
+            "attempted-user",
+            "inappropriate-content",
+            "policy-violation",
+            "shellcode-detect",
+            "successful-admin",
+            "successful-user",
+            "trojan-activity",
+            "unsuccessful-user",
+            "web-application-attack",
+            "attempted-dos",
+            "attempted-recon",
+            "bad-unknown",
+            "default-login-attempt",
+            "denial-of-service",
+            "misc-attack",
+            "non-standard-protocol",
+            "rpc-portmap-decode",
+            "successful-dos",
+            "successful-recon-largescale",
+            "successful-recon-limited",
+            "suspicious-ﬁlename-detect",
+            "suspicious-login",
+            "system-call-detect",
+            "unusual-client-port-connection",
+            "web-application-activity",
+            "icmp-event",
+            "misc-activity",
+            "network-scan",
+            "not-suspicious",
+            "protocol-command-decode",
+            "string-detect",
+            "unknown",
+            "tcp-connection"
+        ],
+        "categories": [ 
+        ]
+    }
+}
