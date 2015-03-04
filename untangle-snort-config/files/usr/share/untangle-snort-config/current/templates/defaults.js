47fccc944df718018516fc2862b7d36a1ce0e0d6
Chris Blaise <cblaise@untangle.com>
2015-03-04 13:39:01 +0000

    Rework Setup Wizard profiles and how configuration rule changes are made to solve several synchronization problems related to wizard, UI, and upgrade management.
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@39772 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-snort-config/files/usr/share/untangle-snort-config/current/templates/defaults.js
+++ untangle-snort-config/files/usr/share/untangle-snort-config/current/templates/defaults.js
@@ -1,43 +1,331 @@
 {
-    "configured" : true,
-    "active_rules" : {
-        "classtypes": [ 
-            "attempted-admin",
-            "attempted-user",
-            "inappropriate-content",
-            "policy-violation",
-            "shellcode-detect",
-            "successful-admin",
-            "successful-user",
-            "trojan-activity",
-            "unsuccessful-user",
-            "web-application-attack",
-            "attempted-dos",
-            "attempted-recon",
-            "bad-unknown",
-            "default-login-attempt",
-            "denial-of-service",
-            "misc-attack",
-            "non-standard-protocol",
-            "rpc-portmap-decode",
-            "successful-dos",
-            "successful-recon-largescale",
-            "successful-recon-limited",
-            "suspicious-ﬁlename-detect",
-            "suspicious-login",
-            "system-call-detect",
-            "unusual-client-port-connection",
-            "web-application-activity",
-            "icmp-event",
-            "misc-activity",
-            "network-scan",
-            "not-suspicious",
-            "protocol-command-decode",
-            "string-detect",
-            "unknown",
-            "tcp-connection"
-        ],
-        "categories": [ 
-        ]
-    }
+    "version": 1,
+    "profiles": [{
+        "profileId":   "low_32",
+        "systemStats": {
+            "MemTotal": "1073741824",
+            "architecture": "32"
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "medium_32",
+        "systemStats": {
+            "MemTotal": "2147483648",
+            "architecture": "32"
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack",
+                "+attempted-dos",
+                "+attempted-recon",
+                "+bad-unknown",
+                "+default-login-attempt",
+                "+denial-of-service",
+                "+misc-attack",
+                "+non-standard-protocol",
+                "+rpc-portmap-decode",
+                "+successful-dos",
+                "+successful-recon-largescale",
+                "+successful-recon-limited",
+                "+suspicious-ﬁlename-detect",
+                "+suspicious-login",
+                "+system-call-detect",
+                "+unusual-client-port-connection",
+                "+web-application-activity"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "high_32",
+        "systemStats": {
+            "MemTotal": "",
+            "architecture": "32"
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack",
+                "+attempted-dos",
+                "+attempted-recon",
+                "+bad-unknown",
+                "+default-login-attempt",
+                "+denial-of-service",
+                "+misc-attack",
+                "+non-standard-protocol",
+                "+rpc-portmap-decode",
+                "+successful-dos",
+                "+successful-recon-largescale",
+                "+successful-recon-limited",
+                "+suspicious-ﬁlename-detect",
+                "+suspicious-login",
+                "+system-call-detect",
+                "+unusual-client-port-connection",
+                "+web-application-activity",
+                "+icmp-event",
+                "+misc-activity",
+                "+network-scan",
+                "+not-suspicious",
+                "+protocol-command-decode",
+                "+string-detect",
+                "+unknown",
+                "+tcp-connection"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "low_64",
+        "systemStats": {
+            "MemTotal": "1073741824",
+            "architecture": "64"
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "medium_64",
+        "systemStats": {
+            "MemTotal": "2147483648",
+            "architecture": "64"
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack",
+                "+attempted-dos",
+                "+attempted-recon",
+                "+bad-unknown",
+                "+default-login-attempt",
+                "+denial-of-service",
+                "+misc-attack",
+                "+non-standard-protocol",
+                "+rpc-portmap-decode",
+                "+successful-dos",
+                "+successful-recon-largescale",
+                "+successful-recon-limited",
+                "+suspicious-ﬁlename-detect",
+                "+suspicious-login",
+                "+system-call-detect",
+                "+unusual-client-port-connection",
+                "+web-application-activity"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "high_64",
+        "systemStats": {
+            "MemTotal": "",
+            "architecture": "64"
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack",
+                "+attempted-dos",
+                "+attempted-recon",
+                "+bad-unknown",
+                "+default-login-attempt",
+                "+denial-of-service",
+                "+misc-attack",
+                "+non-standard-protocol",
+                "+rpc-portmap-decode",
+                "+successful-dos",
+                "+successful-recon-largescale",
+                "+successful-recon-limited",
+                "+suspicious-ﬁlename-detect",
+                "+suspicious-login",
+                "+system-call-detect",
+                "+unusual-client-port-connection",
+                "+web-application-activity",
+                "+icmp-event",
+                "+misc-activity",
+                "+network-scan",
+                "+not-suspicious",
+                "+protocol-command-decode",
+                "+string-detect",
+                "+unknown",
+                "+tcp-connection"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "low_unknown",
+        "systemStats": {
+            "MemTotal": "1073741824",
+            "architecture": ""
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "medium_unknown",
+        "systemStats": {
+            "MemTotal": "2147483648",
+            "architecture": ""
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack",
+                "+attempted-dos",
+                "+attempted-recon",
+                "+bad-unknown",
+                "+default-login-attempt",
+                "+denial-of-service",
+                "+misc-attack",
+                "+non-standard-protocol",
+                "+rpc-portmap-decode",
+                "+successful-dos",
+                "+successful-recon-largescale",
+                "+successful-recon-limited",
+                "+suspicious-ﬁlename-detect",
+                "+suspicious-login",
+                "+system-call-detect",
+                "+unusual-client-port-connection",
+                "+web-application-activity"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    },{
+        "profileId":   "high_unknown",
+        "systemStats": {
+            "MemTotal": "",
+            "architecture": ""
+        },
+        "activeGroups" : {
+            "classtypes": "recommended",
+            "classtypesSelected": [ 
+                "+attempted-admin",
+                "+attempted-user",
+                "+inappropriate-content",
+                "+policy-violation",
+                "+shellcode-detect",
+                "+successful-admin",
+                "+successful-user",
+                "+trojan-activity",
+                "+unsuccessful-user",
+                "+web-application-attack",
+                "+attempted-dos",
+                "+attempted-recon",
+                "+bad-unknown",
+                "+default-login-attempt",
+                "+denial-of-service",
+                "+misc-attack",
+                "+non-standard-protocol",
+                "+rpc-portmap-decode",
+                "+successful-dos",
+                "+successful-recon-largescale",
+                "+successful-recon-limited",
+                "+suspicious-ﬁlename-detect",
+                "+suspicious-login",
+                "+system-call-detect",
+                "+unusual-client-port-connection",
+                "+web-application-activity",
+                "+icmp-event",
+                "+misc-activity",
+                "+network-scan",
+                "+not-suspicious",
+                "+protocol-command-decode",
+                "+string-detect",
+                "+unknown",
+                "+tcp-connection"
+            ],
+            "categories": "recommended",
+            "categoriesSelected": []
+        }
+    }]
 }
