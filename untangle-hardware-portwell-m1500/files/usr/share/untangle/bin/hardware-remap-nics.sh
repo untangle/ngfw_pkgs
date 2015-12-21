505eb4d0d12d374d30f760a7954a5175dac21160
Dirk Morris <dmorris@untangle.com>
2015-12-21 15:59:41 +0000

    fix remap scripts
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@41969 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-hardware-portwell-m1500/files/usr/share/untangle/bin/hardware-remap-nics.sh
+++ untangle-hardware-portwell-m1500/files/usr/share/untangle/bin/hardware-remap-nics.sh
@@ -1,29 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
-# detect specific model of m1500
-# the original m1500
-# the new m1500 (the m1500b) uses E5-2609 CPUs
+# detect specific model of m3000
+# the original m3000
+# the new m3000 (the m3000b) uses E5-2620 CPUs
 cpu_count=`grep "model name" /proc/cpuinfo | wc -l`
 cpu_model=`grep -m1 "model name" /proc/cpuinfo | cut -f2 -d: | cut -c2-`
-if [[ $cpu_count -eq 12 ]]; then
-    is_m1500b=`echo $cpu_model | grep 'E5-2609' | wc -l`
-    if [ $is_m1500b -gt 0 ]; then
-        model='m1500b'
-    else
-        model='m1500'
-    fi
-fi
 
-case $model in
-    m1500)
-        declare -a from=(eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9 eth0 eth1)
-        declare -a   to=(eth0 eth1 eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9)
-        ;;
-    m1500b)
-        declare -a from=(eth6 eth7 eth8 eth9 eth2 eth3 eth4 eth5 eth0 eth1)
-        declare -a   to=(eth0 eth1 eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9)
-        ;;
-esac
+declare -a from=(eth0 eth1 eth3 eth4 eth6 eth7 eth8 eth9 eth2 eth5)
+declare -a   to=(eth0 eth1 eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9)
 
 # and for each element, map to a temporary interface named new_ethx
 udev_file='/etc/udev/rules.d/70-persistent-net.rules';
--- untangle-hardware-portwell-m3000/files/usr/share/untangle/bin/hardware-remap-nics.sh
+++ untangle-hardware-portwell-m3000/files/usr/share/untangle/bin/hardware-remap-nics.sh
@@ -5,25 +5,9 @@
 # the new m3000 (the m3000b) uses E5-2620 CPUs
 cpu_count=`grep "model name" /proc/cpuinfo | wc -l`
 cpu_model=`grep -m1 "model name" /proc/cpuinfo | cut -f2 -d: | cut -c2-`
-if [[ $cpu_count -eq 12 ]]; then
-    is_m3000b=`echo $cpu_model | grep 'E5-2620' | wc -l`
-    if [ $is_m3000b -gt 0 ]; then
-        model='m3000b'
-    else
-        model='m3000'
-    fi
-fi
 
-case $model in
-    m3000)
-        declare -a from=(eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9 eth0 eth1)
-        declare -a   to=(eth0 eth1 eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9)
-        ;;
-    m3000b)
-        declare -a from=(eth6 eth7 eth8 eth9 eth2 eth3 eth4 eth5 eth0 eth1)
-        declare -a   to=(eth0 eth1 eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9)
-        ;;
-esac
+declare -a from=(eth0 eth1 eth3 eth4 eth6 eth7 eth8 eth9 eth2 eth5)
+declare -a   to=(eth0 eth1 eth2 eth3 eth4 eth5 eth6 eth7 eth8 eth9)
 
 # and for each element, map to a temporary interface named new_ethx
 udev_file='/etc/udev/rules.d/70-persistent-net.rules';
