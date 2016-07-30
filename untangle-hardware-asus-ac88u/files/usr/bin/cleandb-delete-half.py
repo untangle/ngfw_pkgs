5f1128b9caf15a478828bad94127d45f7169bb63
Dirk Morris <dmorris@untangle.com>
2016-07-30 04:13:44 +0000

    improvements to DB cleaning script
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/branch/release-12.1/work/pkgs@43913 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-hardware-asus-ac88u/files/usr/bin/cleandb-delete-half.py
+++ untangle-hardware-asus-ac88u/files/usr/bin/cleandb-delete-half.py
@@ -29,16 +29,19 @@ def get_mid_date(id_column,table):
      cursor.execute("SELECT (max(%s)+min(%s))/2 AS mid FROM %s" % (id_column,id_column,table))
      row = cursor.fetchone()
      if row == None or len(row) < 1:
-          print "Invalid mid point from reports.sessions"
+          print "Invalid mid point from %s" % table
           sys.exit(1)
 
+     mid_session = row[0]
+     if mid_session == None:
+          return None
      mid_session_id = int(row[0])
 
      # Get the "time_stamp" on the mid-point (the ID closest to mid-point)
      cursor.execute("SELECT time_stamp FROM %s WHERE %s < %s ORDER BY %s DESC LIMIT 1"%(table, id_column, str(mid_session_id), id_column))
      row = cursor.fetchone()
      if row == None or len(row) < 1:
-          print "Invalid time_stamp from reports.sessions"
+          print "Invalid time_stamp from %s" % table
           sys.exit(1)
 
      time_stamp = row[0]
@@ -46,32 +49,49 @@ def get_mid_date(id_column,table):
      cutoff = mx.DateTime.Parser.DateTimeFromString(str(time_stamp))
      return cutoff
 
+def get_count(table):
+     # Get the mid-point by ID
+     cursor.execute("SELECT count(*) FROM %s" % (table))
+     row = cursor.fetchone()
+     if row == None or len(row) < 1:
+          print "Invalid count from %s" % table
+          sys.exit(1)
+
+     count = int(row[0])
+     return count
+
 if len(sys.argv) < 1:
      usage()
      sys.exit(1)
 
-
-#mid_session_id = sql_helper.run_sql("SELECT (max(session_id)+min(session_id))/2 AS mid FROM reports.sessions")
-#print mid_session_id
-
+# Print the before table size - just for debugging/logging
+sessions_count = get_count("reports.sessions")
+session_minutes_count = get_count("reports.session_minutes")
+http_count = get_count("reports.http_events")
+print "sessions              post count: %s" % str(sessions_count)
+print "session_minutes       post count: %s" % str(session_minutes_count)
+print "http_events           post count: %s" % str(http_count)
+     
+# Find the mid-point for the major three tables
 sessions_cutoff = get_mid_date("session_id","reports.sessions")
-print "sessions     cutoff estimate: %s" % str(sessions_cutoff)
-
+session_minutes_cutoff = get_mid_date("session_id","reports.session_minutes")
 http_cutoff = get_mid_date("request_id","reports.http_events")
-print "http_events  cutoff estimate: %s" % str(http_cutoff)
+print "sessions         cutoff estimate: %s" % str(sessions_cutoff)
+print "session_minutes  cutoff estimate: %s" % str(session_minutes_cutoff)
+print "http_events      cutoff estimate: %s" % str(http_cutoff)
 
-# Take the later of the two cutoff (to make sure we cut AT LEAST half of all data in all tables)
-cutoff = sessions_cutoff
-if ( http_cutoff > sessions_cutoff ):
-     cutoff = http_cutoff
+# Take the latest of mid-points (to make sure we cut AT LEAST half of all data in all tables)
+cutoff = max(sessions_cutoff, session_minutes_cutoff, http_cutoff)
+print "using cutoff: %s" % str(cutoff)
 
-print "cutoff: %s" % str(cutoff)
-
-#print mx.DateTime(cutoff.timetuple()[:3])
-test = mx.DateTime.Parser.DateTimeFromString("%d-%d-%d"%cutoff.timetuple()[:3])
-print test
-
+# Clean the tables with the maximum cutoff
 reports.engine.reports_cleanup(cutoff)     
 
-
+# Print the after table size - just for debugging/logging
+sessions_count = get_count("reports.sessions")
+session_minutes_count = get_count("reports.session_minutes")
+http_count = get_count("reports.http_events")
+print "sessions              post count: %s" % str(sessions_count)
+print "session_minutes       post count: %s" % str(session_minutes_count)
+print "http_events           post count: %s" % str(http_count)
 
