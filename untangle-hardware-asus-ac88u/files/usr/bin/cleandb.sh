a27fcba83f025db8049d269ce361ade084566637
Dirk Morris <dmorris@untangle.com>
2016-07-30 04:13:09 +0000

    improvements to DB cleaning script
    git-svn-id: https://ut.svn.beanstalkapp.com/ngfw/work/pkgs@43912 8ebf8b4e-06f3-0310-961f-89f74618be54

--- untangle-hardware-asus-ac88u/files/usr/bin/cleandb.sh
+++ untangle-hardware-asus-ac88u/files/usr/bin/cleandb.sh
@@ -1,6 +1,6 @@
 #!/bin/dash
 
-echo "Checking DB ramdisk size..."
+echo "`date -Iseconds`| Checking DB ramdisk size..."
 
 PG_VERSION="9.4"
 PG_VAR_DIR="/var/lib/postgresql/${PG_VERSION}"
@@ -10,20 +10,14 @@ PG_MAIN_DIR="/var/lib/postgresql/${PG_VERSION}/main"
 percent="`df --output=pcent ${PG_MAIN_DIR} | sed 1d | sed 's/%//'`"
 #percent="85"
 
-echo "DB ramdisk status: $percent%"
+echo "`date -Iseconds`| DB ramdisk status: $percent%"
 
 if [ "$percent" = "100" ] ; then
-    echo "Completely nuking database..."
+    echo "`date -Iseconds`| Completely nuking database..."
     /etc/init.d/postgresql stop
 
-    # umount just in case running this script by hand
-    umount ${PG_MAIN_DIR} >/dev/null 2>&1
-
     # mount a ramdisk
-    rm -rf ${PG_MAIN_DIR}
-    mkdir -p ${PG_MAIN_DIR}
-    chown -R postgres:postgres ${PG_MAIN_DIR}
-    mount -t tmpfs -o size=128m tmpfs ${PG_MAIN_DIR}
+    rm -rf ${PG_MAIN_DIR}/*
     chown -R postgres:postgres ${PG_MAIN_DIR}
 
     # create DB
@@ -43,13 +37,13 @@ if [ "$percent" = "100" ] ; then
 fi
 
 if [ "$percent" -gt "70" ] ; then
-    echo "Cleaning tables..."
+    echo "`date -Iseconds`| Cleaning tables..."
     # Delete half the data from the DB
     /usr/bin/cleandb-delete-half.py
 
     # Now that we've cleaned half the data we must run vacuum full
     psql -A -t -U postgres -c "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'reports';" uvm | grep '[0-9]' | while read table ; do
-                                                                                                                                  echo "VACUUM FULL $table..."
+                                                                                                                                  echo "`date -Iseconds`| VACUUM FULL $table..."
                                                                                                                                   psql -U postgres -c "VACUUM FULL reports.$table;" uvm
                                                                                                                               done
     exit 0
