cf44399ae2df78db6de30e84af4372ecc3eb9c91
Dirk Morris <dmorris@untangle.com>
2016-12-30 14:18:41 -0800

    improve logging

--- untangle-firmware-common/files/usr/bin/sqlite-cleandb.sh
+++ untangle-firmware-common/files/usr/bin/sqlite-cleandb.sh
@@ -15,7 +15,6 @@ if [ ! -f $DIR/reports.db ] ; then
     exit 0
 fi
 
-echo "`date -Iseconds`| Cleaning postgres DB..."
 percent="`df --output=pcent ${DIR} | sed 1d | sed 's/%//'`"
 #percent="100"
 #percent="85"
@@ -25,11 +24,16 @@ echo "`date -Iseconds`| DB ramdisk status: $percent%"
 target_percent="40"
 
 if [ "$percent" -gt "$target_percent" ] ; then
+
     # delete the enough data to get approximately back to the target disk usage percent
     delete_percent="`awk \"BEGIN {printf \\\"%.0f\\\", ((($percent-$target_percent)/($percent))*100)}\" `"
+    echo "`date -Iseconds`| Cleaning postgres DB... (deleting ${delete_percent}%)"
+
     clean_db delete_percent
+
+    echo "`date -Iseconds`| DB ramdisk status: $percent%"
+
     exit 0
 fi
 
-echo "`date -Iseconds`| DB ramdisk status: $percent%"
 
