2392cef29154725d393134176933515b3017f348
Dirk Morris <dmorris@untangle.com>
2017-02-13 08:43:56 -0800

    call correct script

--- untangle-clamav-config/files/usr/share/untangle-clamav-config/bin/clamav-unofficial-cron.sh
+++ untangle-clamav-config/files/usr/share/untangle-clamav-config/bin/clamav-unofficial-cron.sh
@@ -6,4 +6,4 @@ if [ ! -f /var/run/clamav/clamd.pid ] ; then
     exit 0
 fi
 
-/usr/share/untangle-clamav-config/bin/clamav-unofficial-cron.sh -c /etc/clamav/clamav-unofficial-sigs.conf >/var/log/clamav-unofficial-sigs.log 2>&1
+/usr/share/untangle-clamav-config/bin/clamav-unofficial-sigs.sh -c /etc/clamav/clamav-unofficial-sigs.conf >/var/log/clamav-unofficial-sigs.log 2>&1
