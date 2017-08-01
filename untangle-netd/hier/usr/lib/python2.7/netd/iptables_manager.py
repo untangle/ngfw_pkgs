872304bc0fa9212ea95b2e56eb887e30f9bf97ff
Dirk Morris <dmorris@untangle.com>
2017-08-01 12:32:26 -0700

    Fix ftp tests on 4.x kernel

--- untangle-netd/hier/usr/lib/python2.7/netd/iptables_manager.py
+++ untangle-netd/hier/usr/lib/python2.7/netd/iptables_manager.py
@@ -141,8 +141,9 @@ class IptablesManager:
             file.write("\t${IPTABLES} -t raw -A helpers -p udp --dport 5061 -j CT --helper sip" + "\n");
             file.write("\n");
         
-        file.write("\t# only process bypassed sessions, the ftp-casing will handle scanned sessions" + "\n")
-        file.write("\t${IPTABLES} -t raw -A helpers -m connmark --mark 0x01000000/0x01000000 -p tcp --dport 21 -j CT --helper ftp" + "\n");
+        # file.write("\t# only process bypassed sessions, the ftp-casing will handle scanned sessions" + "\n")
+        # file.write("\t${IPTABLES} -t raw -A helpers -m connmark --mark 0x01000000/0x01000000 -p tcp --dport 21 -j CT --helper ftp" + "\n");
+        file.write("\t${IPTABLES} -t raw -A helpers -p tcp --dport 21 -j CT --helper ftp" + "\n");
         file.write("\n");
 
         file.write("\t${IPTABLES} -t raw -A helpers -p tcp --dport 6667 -j CT --helper irc" + "\n");
