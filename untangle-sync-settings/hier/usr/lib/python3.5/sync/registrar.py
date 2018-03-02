6e75b620089883c97cbf3f750fd69fc2912ba7aa
Dirk Morris <dmorris@untangle.com>
2018-03-01 21:50:56 -0800

    add calculation of required operations and running of pre_command and post_commands in operations

--- untangle-sync-settings/hier/usr/lib/python3.5/sync/registrar.py
+++ untangle-sync-settings/hier/usr/lib/python3.5/sync/registrar.py
@@ -37,6 +37,93 @@ def register_file( filepath, operation, owner ):
         
     files[filepath] = { "filepath": filepath, "operation": operation, "owner": owner}
 
+
+def operation_subset_of( parent, child ):
+    """
+    Returns true if the child operation is a child or x-grandchild of parent
+    In other words, the child operation is a subset of the parent operation
+    Otherwise returns False
+    """
+    if child == parent:
+        return False
+    child_op = operations.get(child)
+    parent_op = operations.get(parent)
+    if child_op == None or parent_op == None:
+        return False
+    if child_op.get('parent') == parent:
+        return True
+    if child_op.get('parent') != None:
+        return operation_subset_of(parent, child_op.get('parent'))
+    return False
+
+def calculate_required_operations( changed_files ):
+    """
+    Calculates the required operations that need to be completed 
+    if the specified files have changed
+    """
+    global files
+    operations = []
+    for filename in changed_files:
+        f = files.get(filename)
+        if f == None:
+            raise ValueError("Missing file from registrar: " + filename)
+        op = f.get('operation')
+        if op != None and op not in operations:
+            operations.append(op)
+    return operations
+
+def registrar_check_file(filename):
+    """
+    Checks that the specified filename is present in the registrar
+    Exits with exit code 1 if filename is not found
+    """
+    global files
+    for regex in files.keys():
+        if filename == regex:
+            return True
+    for regex in registrar.files.keys():
+        if re.compile(regex).match(filename):
+            return True
+    return False
+
+def reduce_operations( ops ):
+    """
+    This attempts to reduce the number of required operations is some semi-intelligent fashion
+    """
+    global operations
+    if ops == None:
+        return None
+    if len(ops) == 0:
+        return ops
+    # Check operations
+    for op1 in ops:
+        o = operations.get(op1)
+        if o == None:
+            raise ValueError("Missing op for registrar: " + op1)
+    # First remove any operations that are already included
+    orig_ops = list(ops)
+    for op1 in orig_ops:
+        for op2 in orig_ops:
+           if operation_subset_of(op1, op2):
+               ops.remove(op2)
+
+    # See if including any of the immediate parents of the operations reduces the length
+    # If so, its probably worth it to do include the parent
+    orig_ops = list(ops)
+    for op1 in orig_ops:
+        o1 = operations.get(op1)
+        if o1.get('parent') != None:
+            new_ops = ops + [o1.get('parent')]
+            new_ops = reduce_operations( new_ops )
+            if len(new_ops) < len(orig_ops):
+                return reduce_operations( new_ops )
+
+    return(ops)
+
+
+
+
+
 register_operation( "restart-networking", "ifdown -a -v --exclude=lo", "ifup -a -v --exclude=lo",          10, None )
 register_operation( "restart-dnsmasq",    None, "/etc/untangle/post-network-hook.d/990-restart-dnsmasq",   20, "restart-networking" )
 register_operation( "restart-miniupnpd",  None, "/etc/untangle/post-network-hook.d/990-restart-upnp",      21, "restart-networking" )
