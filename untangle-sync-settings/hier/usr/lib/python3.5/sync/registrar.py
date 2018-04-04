f5c726236d9c93df1b608eff619c239c9e02de79
Dirk Morris <dmorris@untangle.com>
2018-04-04 16:33:36 -0700

    Better wait to add systemctl-wait function

--- untangle-sync-settings/hier/usr/lib/python3.5/sync/registrar.py
+++ untangle-sync-settings/hier/usr/lib/python3.5/sync/registrar.py
@@ -137,20 +137,15 @@ def reduce_operations( ops ):
 
 
 
-register_operation( "restart-networking",
-                    ["ifdown -a -v --exclude=lo"],
-                    ["ifup -a -v --exclude=lo",
-                     "for i in `seq 20` ; do if systemctl status | grep -q 'Jobs: 0 queued' ; then break ; else sleep .5 done ; fi ; done"],
-                    10,
-                    None )
-register_operation( "restart-dnsmasq",    None, ["/etc/untangle/post-network-hook.d/990-restart-dnsmasq"],   20, "restart-networking" )
-register_operation( "restart-miniupnpd",  None, ["/etc/untangle/post-network-hook.d/990-restart-upnp"],      21, "restart-networking" )
-register_operation( "restart-radvd",      None, ["/etc/untangle/post-network-hook.d/990-restart-radvd"],     22, "restart-networking" )
-register_operation( "restart-ddclient",   None, ["/etc/untangle/post-network-hook.d/990-restart-ddclient"],  23, "restart-networking" )
-register_operation( "restart-softflowd",  None, ["/etc/untangle/post-network-hook.d/990-restart-softflowd"], 25, "restart-networking" )
-register_operation( "restart-quagga",     None, ["/etc/untangle/post-network-hook.d/990-restart-quagga"],    26, "restart-networking" )
-register_operation( "restart-keepalived", None, ["/etc/untangle/post-network-hook.d/200-vrrp"],              30, "restart-networking" )
-register_operation( "restart-iptables",   None, ["/etc/untangle/post-network-hook.d/960-iptables"],          50, "restart-networking" )
+register_operation( "restart-networking", ["ifdown -a -v --exclude=lo"], ["ifup -a -v --exclude=lo","/usr/bin/systemctl-wait"], 10, None )
+register_operation( "restart-dnsmasq",    None, ["/etc/untangle/post-network-hook.d/990-restart-dnsmasq","/usr/bin/systemctl-wait"],   20, "restart-networking" )
+register_operation( "restart-miniupnpd",  None, ["/etc/untangle/post-network-hook.d/990-restart-upnp","/usr/bin/systemctl-wait"],      21, "restart-networking" )
+register_operation( "restart-radvd",      None, ["/etc/untangle/post-network-hook.d/990-restart-radvd","/usr/bin/systemctl-wait"],     22, "restart-networking" )
+register_operation( "restart-ddclient",   None, ["/etc/untangle/post-network-hook.d/990-restart-ddclient","/usr/bin/systemctl-wait"],  23, "restart-networking" )
+register_operation( "restart-softflowd",  None, ["/etc/untangle/post-network-hook.d/990-restart-softflowd","/usr/bin/systemctl-wait"], 25, "restart-networking" )
+register_operation( "restart-quagga",     None, ["/etc/untangle/post-network-hook.d/990-restart-quagga","/usr/bin/systemctl-wait"],    26, "restart-networking" )
+register_operation( "restart-keepalived", None, ["/etc/untangle/post-network-hook.d/200-vrrp","/usr/bin/systemctl-wait"],              30, "restart-networking" )
+register_operation( "restart-iptables",   None, ["/etc/untangle/post-network-hook.d/960-iptables","/usr/bin/systemctl-wait"],          50, "restart-networking" )
 
 
 
