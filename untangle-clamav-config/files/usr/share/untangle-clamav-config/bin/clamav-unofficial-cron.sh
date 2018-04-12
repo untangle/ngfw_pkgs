3bc432e5a12712f80d6450c066f39a610c164b27
Dirk Morris <dmorris@untangle.com>
2018-04-12 14:55:47 -0700

    Disable clam unofficial sigs.
    The time has come to disable these. They increase memory usage and cause many false positives.
    The official signature set is solid and the paid virus blocker offers a good alternative for those that want improved protection.

--- untangle-clamav-config/files/usr/share/untangle-clamav-config/bin/clamav-unofficial-cron.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/sh
-
-# Only run if clamd running
-if [ ! -f /var/run/clamav/clamd.pid ] ; then
-    exit 0
-fi
-
-/usr/share/untangle-clamav-config/bin/clamav-unofficial-sigs.sh -c /etc/clamav/clamav-unofficial-sigs.conf >/var/log/clamav-unofficial-sigs.log 2>&1
