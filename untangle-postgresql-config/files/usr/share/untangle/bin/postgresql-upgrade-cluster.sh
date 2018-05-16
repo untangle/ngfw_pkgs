d768a37e8a0a73a8e6a747898b38bb280cd129ab
Sébastien Delafond <sdelafond@gmail.com>
2018-05-16 10:43:01 +0200

    In upgrade-cluster use VERSION_NEW so the script can work as-is for the next conversion

--- untangle-postgresql-config/files/usr/share/untangle/bin/postgresql-upgrade-cluster.sh
+++ untangle-postgresql-config/files/usr/share/untangle/bin/postgresql-upgrade-cluster.sh
@@ -20,7 +20,7 @@ if [ -d $PG_VAR_DIR_OLD ] ; then
   /etc/init.d/postgresql stop
   
   pushd /tmp
-  su postgres -c "/usr/lib/postgresql/9.6/bin/pg_upgrade --link -b $PG_BIN_DIR_OLD -B $PG_BIN_DIR_NEW -d $PG_VAR_DIR_OLD -D $PG_VAR_DIR_NEW -o ' -c config_file='$PG_CONF_OLD -O ' -c config_file='$PG_CONF_NEW"
+  su postgres -c "/usr/lib/postgresql/${VERSION_NEW}/bin/pg_upgrade --link -b $PG_BIN_DIR_OLD -B $PG_BIN_DIR_NEW -d $PG_VAR_DIR_OLD -D $PG_VAR_DIR_NEW -o ' -c config_file='$PG_CONF_OLD -O ' -c config_file='$PG_CONF_NEW"
   if [ $? != 0 ] ; then
       echo "[$(date +%Y-%m%-dT%H:%m)] Conversion FAILED"
       exit 0
