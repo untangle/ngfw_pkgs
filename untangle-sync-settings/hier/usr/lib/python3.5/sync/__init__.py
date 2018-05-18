18dd151dcebb8de7c32714ef85c44e7f279a05b7
Dirk Morris <dmorris@untangle.com>
2018-05-17 20:27:30 -0700

    Move debian specific managers to a debian subpackage

--- untangle-sync-settings/hier/usr/lib/python3.5/sync/__init__.py
+++ untangle-sync-settings/hier/usr/lib/python3.5/sync/__init__.py
@@ -1,30 +1,4 @@
 
-from sync.hosts_manager import HostsManager
-from sync.interfaces_manager import InterfacesManager
-from sync.wireless_manager import WirelessManager
-from sync.dnsmasq_manager import DnsMasqManager
-from sync.nat_rules_manager import NatRulesManager
-from sync.filter_rules_manager import FilterRulesManager
-from sync.iptables_manager import IptablesManager
-from sync.port_forward_manager import PortForwardManager
-from sync.bypass_rules_manager import BypassRuleManager
-from sync.ethernet_manager import EthernetManager
-from sync.hosts_manager import HostsManager
-from sync.sysctl_manager import SysctlManager
-from sync.kernel_manager import KernelManager
-from sync.arp_manager import ArpManager
-from sync.route_manager import RouteManager
-from sync.dhcp_manager import DhcpManager
-from sync.vrrp_manager import VrrpManager
-from sync.radvd_manager import RadvdManager
-from sync.pppoe_manager import PPPoEManager
-from sync.qos_manager import QosManager
-from sync.ddclient_manager import DdclientManager
-from sync.ebtables_manager import EbtablesManager
-from sync.upnp_manager import UpnpManager
-from sync.netflow_manager import NetflowManager
-from sync.dynamic_routing_manager import DynamicRoutingManager
-
 from sync.iptables_util import IptablesUtil
 from sync.network_util import NetworkUtil
 
