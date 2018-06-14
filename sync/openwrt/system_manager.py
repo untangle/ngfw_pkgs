499552b5f0ea8a3b9f6a4578b638c37961b68b37
Dirk Morris <dmorris@untangle.com>
2018-06-14 15:50:58 -0700

    Cleanup up schema and move to /usr/share

--- sync/openwrt/system_manager.py
+++ sync/openwrt/system_manager.py
@@ -12,11 +12,11 @@ class SystemManager:
     def initialize( self ):
         pass
 
-    def create_settings( self, settings, prefix, delete_list, verbosity=0 ):
+    def create_settings( self, settings, prefix, delete_list, filename, verbosity=0 ):
         print("%s: Initializing settings" % self.__class__.__name__)
         settings['system'] = {}
-        settings['system']['hostname'] = 'mfw'
-        settings['system']['domainname'] = 'example.com'
+        settings['system']['hostName'] = 'mfw'
+        settings['system']['domainName'] = 'example.com'
         
     def sync_settings( self, settings, prefix, delete_list, verbosity=0 ):
         pass
