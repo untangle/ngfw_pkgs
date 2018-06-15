bfa1166ba675988a8392790afd353dd2b17157f8
Dirk Morris <dmorris@untangle.com>
2018-06-15 09:34:49 -0700

    Fix wireless being unavailable after upgrade

--- untangle-sync-settings/hier/usr/lib/python3.5/sync/interfaces_manager.py
+++ untangle-sync-settings/hier/usr/lib/python3.5/sync/interfaces_manager.py
@@ -132,7 +132,7 @@ class InterfacesManager:
                     self.interfaces_file.write("\tpost-up /usr/share/untangle-sync-settings/bin/add-source-route.sh %s \"uplink.%i\" -4\n" % (alias.get('staticAddress'), interface_settings.get('interfaceId'))) 
             
         if interface_settings.get('isWirelessInterface'):
-            if interface_settings.get('wirelessMode') == 'AP':
+            if interface_settings.get('wirelessMode') == 'AP' or interface_settings.get('wirelessMode') == None:
                 self.interfaces_file.write("\thostapd /etc/hostapd/hostapd.conf-%s\n" % devName);
             elif interface_settings.get('wirelessMode') == 'CLIENT':
                 self.interfaces_file.write("\twpa-conf /etc/wpa_supplicant/wpa_supplicant.conf-%s\n" % devName);
