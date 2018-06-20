68cb560079c9c83482056b942da1589eb4e3fdf1
Sébastien Delafond <sdelafond@gmail.com>
2018-06-20 13:05:59 +0200

    Re-use ngfw:build image in TravisCI builds

---
+++ build-travis.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# dumbed-down, minimalist version of ngfw_pkgtools.git/make-build.sh
+# (no versioning, etc), just so TravisCI can test the build of our
+# packages
+
+set -e
+
+awk '$2 == "stretch" {print $1}' build-order.txt | while read d ; do
+ grep -qE '^Architecture:.*(amd64|any|all)' "$d"/debian/control
+ pushd "$d"
+ mk-build-deps -i
+ dpkg-buildpackage -us -uc
+ popd
+done
