2a90cddd97c1491f192f5ac96aeefeb839207120
Sébastien Delafond <sdelafond@gmail.com>
2018-06-21 08:52:08 +0200

    Do not error out on packages not targetting amd64

--- build-travis.sh
+++ build-travis.sh
@@ -9,7 +9,7 @@ set -e
 apt-get update
 
 awk '$2 == "stretch" {print $1}' build-order.txt | while read d ; do
- grep -qE '^Architecture:.*(amd64|any|all)' "$d"/debian/control
+ grep -qE '^Architecture:.*(amd64|any|all)' "$d"/debian/control || continue
  pushd "$d"
  mk-build-deps -i -t 'apt-get -y'
  fakeroot debian/rules binary
