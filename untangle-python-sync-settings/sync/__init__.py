a2e578d799f401dbb2ed2da0441c8e4528f8409b
Sébastien Delafond <sdelafond@gmail.com>
2018-07-11 09:37:30 +0200

    Make sure u-python-sync-settings can be built correctly, but also run directly from git

--- untangle-python-sync-settings/sync/__init__.py
+++ untangle-python-sync-settings/sync/__init__.py
@@ -1,4 +1,8 @@
-from .version import __version__
+try:
+  from .version import __version__
+except:
+  __version__ = "undefined"
+
 from .iptables_util import IptablesUtil
 from .network_util import NetworkUtil
 
