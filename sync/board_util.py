6445724e5aaadc760c464ad7acd06f9db3d94727
Brett Mastbergen <bmastbergen@untangle.com>
2018-07-19 14:33:41 -0400

    openwrt: wireless: Get country code, if needed
    Some cards have the country code programmed in
    eeprom (wrt3200), but some need to set it
    manually based on the country code programmed
    in flash (wrt1900).  Add a place in board_util
    to implement board specific methods of getting
    the country code (if needed).

--- sync/board_util.py
+++ sync/board_util.py
@@ -20,3 +20,15 @@ def get_internal_device_name():
             "armada-385-linksys-rango": "eth0.1",
     }.get(board_name, "eth0")
 
+def get_country_code():
+    board_name = get_board_name()
+    if board_name == "armada-385-linksys-shelby":
+        sku = subprocess.check_output("cat /tmp/syscfg/syscfg/syscfg.dat | sed -ne 's/^device::cert_region=//p'", shell=True).decode('ascii').rstrip()
+        return {
+                "AP": "CN",
+                "AU": "AU",
+                "EU": "DE",
+                "US": "US",
+        }.get(sku, "")
+
+    return ""
