a36b294d59c5ae6015ff47c1fee09e62d3950205
Dirk Morris <dmorris@untangle.com>
2018-08-17 03:37:09 +0000

    Cleanup chain names and organization

--- untangle-python-sync-settings/sync/openwrt/nftables_manager.py
+++ untangle-python-sync-settings/sync/openwrt/nftables_manager.py
@@ -37,51 +37,54 @@ nft flush ruleset
 
 # prerouting skeleton
 nft add table inet prerouting
-nft add table ip prerouting-nat
+nft add table ip   prerouting
 nft add chain inet prerouting prerouting-set-marks "{ type filter hook prerouting priority -150 ; }"
 # nft add chain inet prerouting packetd-queue "{ type nat hook prerouting priority -145 ; }"
 nft add chain inet prerouting prerouting-qos "{ type filter hook prerouting priority -140 ; }"
-nft add chain inet prerouting route-vote-rules "{ type filter hook prerouting priority -135 ; }"
-nft add chain ip prerouting-nat port-forward-rules "{ type nat hook prerouting priority -100 ; }"
-nft add chain ip prerouting-nat upnp-rules "{ type nat hook prerouting priority -99 ; }"
+nft add chain inet prerouting prerouting-route "{ type filter hook prerouting priority -135 ; }"
+nft add chain ip   prerouting prerouting-nat "{ type nat hook prerouting priority -100 ; }"
 
 # postrouting skeleton
 nft add table inet postrouting
-nft add table ip postrouting-nat
-nft add table ip postrouting-route
-nft add table ip6 postrouting-route
-nft add chain inet postrouting postrouting-qos "{ type filter hook postrouting priority -140 ; }"
-nft add chain ip postrouting-nat postrouting-nat "{ type nat hook postrouting priority -100 ; }"
-nft add chain ip postrouting-nat nat-rules "{ type nat hook postrouting priority -99 ; }"
+nft add table ip   postrouting
+nft add chain ip   postrouting postrouting-nat "{ type nat hook postrouting priority 100 ; }"
+nft add chain inet postrouting postrouting-set-marks "{ type filter hook postrouting priority 110 ; }"
+nft add chain inet postrouting postrouting-qos "{ type filter hook postrouting priority 120 ; }"
 
 # forward skeleton
 nft add table inet forward
 nft add chain inet forward forward-set-marks "{ type filter hook postrouting priority -150 ; }"
-nft add chain inet forward filter-rules "{ type filter hook postrouting priority 0 ; }"
+nft add chain inet forward forward-filter "{ type filter hook postrouting priority 0 ; }"
 
-# FIXME - these should be moved to default settings in filter-rules
+# FIXME - these should be moved to default settings in forward-filter
 # Leaving them here for now
+nft add chain inet forward filter-rules-sys
 nft add chain inet forward filter-rules-nat
 nft add chain inet forward filter-rules-new
 nft add chain inet forward filter-rules-early
 nft add chain inet forward filter-rules-all
-nft add rule inet forward filter-rules jump filter-rules-nat
-nft add rule inet forward filter-rules jump filter-rules-new
-nft add rule inet forward filter-rules jump filter-rules-early
-nft add rule inet forward filter-rules jump filter-rules-all
+nft add rule inet forward forward-filter jump filter-rules-sys
+nft add rule inet forward forward-filter jump filter-rules-nat
+nft add rule inet forward forward-filter jump filter-rules-new
+nft add rule inet forward forward-filter jump filter-rules-early
+nft add rule inet forward forward-filter jump filter-rules-all
 
 # input skeleton
 nft add table inet input
 # nft add chain inet input packetd-input "{ type filter hook input priority -150 ; }"
-nft add chain inet input access-rules "{ type filter hook input priority 0 ; }"
+nft add chain inet input input-filter "{ type filter hook input priority 0 ; }"
+nft add chain inet input access-rules-sys
+nft add chain inet input access-rules
+nft add rule inet input input-filter jump access-rules-sys
+nft add rule inet input input-filter jump access-rules
 
 # output skeleton
 nft add table inet output
-nft add table ip output-route
-nft add table ip6 output-route
+nft add table ip   output
+nft add table ip6  output
 # nft add chain inet output packetd-output "{ type filter hook output priority -150 ; }"
-nft add chain ip  output route-vote-rules "{ type route hook output priority -150 ; }"
-nft add chain ip6 output route-vote-rules "{ type route hook output priority -150 ; }"
+nft add chain ip   output output-route "{ type route hook output priority -150 ; }"
+nft add chain ip6  output output-route "{ type route hook output priority -150 ; }"
 
 exit 0
         """)
