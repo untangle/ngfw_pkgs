e7c6ba42be94721b10624ea66139c282cfcb0750
Sébastien Delafond <sdelafond@gmail.com>
2018-09-06 10:19:07 +0200

    Remove u-python-sync-settings, to be replaced by a subtree from https://github.com/untangle/sync-settings

--- untangle-python-sync-settings/sync/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-  from .version import __version__
-except:
-  __version__ = "undefined"
-
-from .iptables_util import IptablesUtil
-from .network_util import NetworkUtil
-
-from . import registrar
