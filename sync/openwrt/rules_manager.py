1ac3d96b1ba2839b885a1d9e17b9cd2a9db37b3e
Dirk Morris <dmorris@untangle.com>
2018-09-07 16:16:06 +0000

    Change chains back to array

--- sync/openwrt/rules_manager.py
+++ sync/openwrt/rules_manager.py
@@ -76,99 +76,94 @@ def default_filter_rules_table():
     return {
         "name": "filter-rules",
         "family": "inet",
-        "chains": {
-            "filter-rules": {
-                "name": "filter-rules",
-                "description": "The base filter-rules chain",
-                "base": True,
-                "type": "filter",
-                "hook": "forward",
-                "priority": 0,
-                "rules": [{
-                    "enabled": True,
-                    "description": "Call new session filter rules",
-                    "ruleId": 2,
-                    "conditions": [],
-                    "action": {
-                        "action": "JUMP",
-                        "chain": "filter-rules-new"
-                    }
+        "chains": [{
+            "name": "filter-rules",
+            "description": "The base filter-rules chain",
+            "base": True,
+            "type": "filter",
+            "hook": "forward",
+            "priority": 0,
+            "rules": [{
+                "enabled": True,
+                "description": "Call new session filter rules",
+                "ruleId": 2,
+                "conditions": [],
+                "action": {
+                    "action": "JUMP",
+                    "chain": "filter-rules-new"
+                }
+            },{
+                "enabled": True,
+                "description": "Call early-session session filter rules",
+                "ruleId": 2,
+                "conditions": [],
+                "action": {
+                    "action": "JUMP",
+                    "chain": "filter-rules-early"
+                }
+            },{
+                "enabled": True,
+                "description": "Call deep-session (all packets) session filter rules",
+                "ruleId": 2,
+                "conditions": [],
+                "action": {
+                    "action": "JUMP",
+                    "chain": "filter-rules-all"
+                }
+            }],
+            "editable": False
+        },{
+            "name": "filter-rules-new",
+            "description": "The chain to process the first packet of each session (new sessions)",
+            "default": True,
+            "rules": [{
+                "ruleId": 1,
+                "description": "An example rule of blocking TCP sessions to 1.2.3.4 port 1234",
+                "enabled": False,
+                "conditions": [{
+                    "type": "IP_PROTOCOL",
+                    "op": "IS",
+                    "value": "tcp"
                 },{
-                    "enabled": True,
-                    "description": "Call early-session session filter rules",
-                    "ruleId": 2,
-                    "conditions": [],
-                    "action": {
-                        "action": "JUMP",
-                        "chain": "filter-rules-early"
-                    }
+                    "type": "SERVER_ADDRESS",
+                    "op": "IS",
+                    "value": "1.2.3.4"
                 },{
-                    "enabled": True,
-                    "description": "Call deep-session (all packets) session filter rules",
-                    "ruleId": 2,
-                    "conditions": [],
-                    "action": {
-                        "action": "JUMP",
-                        "chain": "filter-rules-all"
-                    }
+                    "type": "SERVER_PORT",
+                    "op": "IS",
+                    "value": "1234"
                 }],
-                "editable": False
-            },
-            "filter-rules-new": {
-                "name": "filter-rules-new",
-                "description": "The chain to process the first packet of each session (new sessions)",
-                "default": True,
-                "rules": [{
-                    "ruleId": 1,
-                    "description": "An example rule of blocking TCP sessions to 1.2.3.4 port 1234",
-                    "enabled": False,
-                    "conditions": [{
-                        "type": "IP_PROTOCOL",
-                        "op": "IS",
-                        "value": "tcp"
-                    },{
-                        "type": "SERVER_ADDRESS",
-                        "op": "IS",
-                        "value": "1.2.3.4"
-                    },{
-                        "type": "SERVER_PORT",
-                        "op": "IS",
-                        "value": "1234"
-                    }],
-                    "action": {
-                        "type": "REJECT"
-                    }
+                "action": {
+                    "type": "REJECT"
+                }
+            },{
+                "ruleId": 2,
+                "description": "An example rule of blocking TCP port 21 (FTP) from 192.168.1.100",
+                "enabled": False,
+                "conditions": [{
+                    "type": "IP_PROTOCOL",
+                    "op": "IS",
+                    "value": "tcp"
                 },{
-                    "ruleId": 2,
-                    "description": "An example rule of blocking TCP port 21 (FTP) from 192.168.1.100",
-                    "enabled": False,
-                    "conditions": [{
-                        "type": "IP_PROTOCOL",
-                        "op": "IS",
-                        "value": "tcp"
-                    },{
-                        "type": "CLIENT_ADDRESS",
-                        "op": "IS",
-                        "value": "192.168.1.100"
-                    },{
-                        "type": "SERVER_PORT",
-                        "op": "IS",
-                        "value": "21"
-                    }],
-                    "action": {
-                        "type": "REJECT"
-                    }
-                }]
-            },
-            "filter-rules-early": {
-                "name": "filter-rules-early",
-                "description": "The chain to process the first few packets of each session (early in session)",
-                "rules": []
-            },
-            "filter-rules-all": {
-                "name": "filter-rules-all",
-                "description": "The chain to process the all packets",
-                "rules": []
-            }
-        }
+                    "type": "CLIENT_ADDRESS",
+                    "op": "IS",
+                    "value": "192.168.1.100"
+                },{
+                    "type": "SERVER_PORT",
+                    "op": "IS",
+                    "value": "21"
+                }],
+                "action": {
+                    "type": "REJECT"
+                }
+            }]
+        },{
+            "name": "filter-rules-early",
+            "description": "The chain to process the first few packets of each session (early in session)",
+            "rules": []
+        },{
+            "name": "filter-rules-all",
+            "description": "The chain to process the all packets",
+            "rules": []
+        }]
     }
