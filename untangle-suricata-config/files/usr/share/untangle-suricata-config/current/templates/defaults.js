83f997cc84357caad819f6e22609203f9513bc92
Chris Blaise <cblaise@untangle.com>
2018-10-01 20:20:07 -0400

    Manually merge from 14.1.

--- untangle-suricata-config/files/usr/share/untangle-suricata-config/current/templates/defaults.js
+++ untangle-suricata-config/files/usr/share/untangle-suricata-config/current/templates/defaults.js
@@ -66,13 +66,13 @@
                 "list":[{
                     "javaClass":"com.untangle.app.intrusion_prevention.IntrusionPreventionRuleCondition",
                     "type":"SYSTEM_MEMORY",
-                    "comparator":"<",
-                    "value":"2147483648"
+                    "comparator":">=",
+                    "value":"1610612736"
                 },{
                     "javaClass":"com.untangle.app.intrusion_prevention.IntrusionPreventionRuleCondition",
                     "type":"CLASSTYPE",
                     "comparator":"=",
-                    "value":"attempted-admin,attempted-user,inappropriate-content,policy-violation,shellcode-detect,successful-admin,successful-user,trojan-activity,nsuccessful-user,web-application-attack,attempted-dos,attempted-recon,bad-unknown,default-login-attempt,denial-of-service,misc-attack,non-standard-protocol,rpc-portmap-decode,successful-dos,successful-recon-largescale,successful-recon-limited,suspicious-ﬁlename-detect,suspicious-login,system-call-detect,unusual-client-port-connection,web-application-activity"
+                    "value":"attempted-dos,attempted-recon,bad-unknown,default-login-attempt,denial-of-service,misc-attack,non-standard-protocol,rpc-portmap-decode,successful-dos,successful-recon-largescale,successful-recon-limited,suspicious-ﬁlename-detect,suspicious-login,system-call-detect,unusual-client-port-connection,web-application-activity"
                 }]
             }
         },{
@@ -92,7 +92,7 @@
                     "javaClass":"com.untangle.app.intrusion_prevention.IntrusionPreventionRuleCondition",
                     "type":"CLASSTYPE",
                     "comparator":"=",
-                    "value":"attempted-admin,attempted-user,inappropriate-content,policy-violation,shellcode-detect,successful-admin,successful-user,trojan-activity,nsuccessful-user,web-application-attack,attempted-dos,attempted-recon,bad-unknown,default-login-attempt,denial-of-service,misc-attack,non-standard-protocol,rpc-portmap-decode,successful-dos,successful-recon-largescale,successful-recon-limited,suspicious-ﬁlename-detect,suspicious-login,system-call-detect,unusual-client-port-connection,web-application-activity,icmp-event,misc-activity,network-scan,not-suspicious,protocol-command-decode,string-detect,unknown,tcp-connection"
+                    "value":"web-application-attack,attempted-dos,attempted-recon,bad-unknown,default-login-attempt,denial-of-service,misc-attack,non-standard-protocol,rpc-portmap-decode,successful-dos,successful-recon-largescale,successful-recon-limited,suspicious-ﬁlename-detect,suspicious-login,system-call-detect,unusual-client-port-connection,web-application-activity,icmp-event,misc-activity,network-scan,not-suspicious,protocol-command-decode,string-detect,unknown,tcp-connection"
                 }]
             }
         }]
