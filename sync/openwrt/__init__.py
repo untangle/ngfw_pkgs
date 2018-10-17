0d9c5d91a022309fa1567479bb10e117c04454c2
Brett Mastbergen <bmastbergen@untangle.com>
2018-10-17 15:49:44 -0400

    Add initial route_manager
    route_manager currently manages the following files:
    
    /etc/iproute2/rt_tables
    This file must contain any custom route tables we plan to use.
    route_manager adds wan.[interfaceId] tables and a balance table.
    
    /etc/config/[ifup.d|ifdown.d]/10-default-route
    These scripts wait for ifup and ifdown events sent by netifd.  If the
    events are for wan interfaces, it makes sure that a priority 1000000
    default route to one of the available "up" wan ports always exists.
    
    /etc/config/[ifup.d|ifdown.d]/20-wan-balancer
    These scripts wait for ifup and ifdown events sent by netifd.  If the
    events are for wan interfaces, it rebuilds the balance table to ensure it
    always includes all of the "up" wan ports.
    
    MFW-106

--- sync/openwrt/__init__.py
+++ sync/openwrt/__init__.py
@@ -8,6 +8,7 @@ from . import nat_manager
 from . import admin_manager
 from . import reports_manager
 from . import qos_manager
+from . import route_manager
 
 from . import settings_manager
 from . import operations
--- sync/openwrt/operations.py
+++ sync/openwrt/operations.py
@@ -5,6 +5,8 @@ sync.registrar.register_operation( "restart-nftables-rules", [""], ["/etc/init.d
 sync.registrar.register_operation( "restart-wireless", [""], ["/sbin/wifi"], 10, None )
 sync.registrar.register_operation( "restart-dhcp", [""], ["/etc/init.d/dnsmasq restart"], 10, None )
 sync.registrar.register_operation( "restart-qos", [""], ["/etc/config/nftables-rules.d/300-qos-rules-sys"], 10, "restart-nftables-rules" )
+sync.registrar.register_operation( "restart-default-route", [""], ["/etc/config/ifdown.d/10-default-route"], 10, "restart-networking" )
+sync.registrar.register_operation( "restart-wan-balancer", [""], ["/etc/config/ifup.d/20-wan-balancer"], 10, "restart-networking" )
 
 def verify_settings(settings):
     """
