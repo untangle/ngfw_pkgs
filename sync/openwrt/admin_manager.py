5a2edcb19ab205c9545e0dab5d3a5b3a87c9d2bc
Dirk Morris <dmorris@untangle.com>
2018-10-28 05:51:46 +0000

    Add logic to set root password in openwrt

--- sync/openwrt/admin_manager.py
+++ sync/openwrt/admin_manager.py
@@ -1,3 +1,4 @@
+import crypt
 import os
 import stat
 import sys
@@ -9,13 +10,30 @@ from sync import nftables_util
 
 # This class is responsible for managing the "admin" (credentials) settings
 
-
 class AdminManager:
+    password_setter_filename = "/etc/config/startup.d/100-setpasswd"
+
     def initialize(self):
+        registrar.register_file(self.password_setter_filename, "startup-scripts", self)
         pass
 
     def sanitize_settings(self, settings):
-        pass
+        admin = settings.get('admin')
+        if admin is None:
+            return
+        creds = admin.get('credentials')
+        if creds is None:
+            return
+        for cred in creds:
+            cleartext_password = cred.get('passwordCleartext')
+            if cleartext_password is None:
+                continue
+            print("Santizing password...")
+            # Remove the cleartext password. We don't want to save this.
+            del cred['passwordCleartext']
+            # Replace with the hashed versions of the password
+            cred['passwordShadowHash'] = crypt.crypt(cleartext_password, crypt.mksalt(crypt.METHOD_SHA512))
+        return
 
     def validate_settings(self, settings):
         pass
@@ -30,8 +48,52 @@ class AdminManager:
         pass
 
     def sync_settings(self, settings, prefix, delete_list):
-        # FIXME write /etc/shadow
-        pass
+        admin = settings.get('admin')
+        if admin is None:
+            return
+        creds = admin.get('credentials')
+        if creds is None:
+            return
 
+        # prefer "root" user for root password
+        # if "root" doesn't exist use "admin"
+        # if neither exist, don't set root password
+        for cred in creds:
+            if cred["username"] == "root":
+                self.write_password_setter(cred["passwordShadowHash"], prefix)
+                return
+        for cred in creds:
+            if cred["username"] == "admin":
+                self.write_password_setter(cred["passwordShadowHash"], prefix)
+                return
+        # if not found, delete any previous password script
+        delete_list.append(password_setter_filename)
+        return
+
+    def write_password_setter(self, phash, prefix):
+        filename = prefix + self.password_setter_filename
+        file_dir = os.path.dirname(filename)
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+
+        file = open(filename, "w+")
+        file.write("#!/bin/sh")
+        file.write("\n\n")
+
+        file.write("## Auto Generated\n")
+        file.write("## DO NOT EDIT. Changes will be overwritten.\n")
+        file.write("\n\n")
+
+        file.write('/bin/sed -i /etc/shadow -e \'s|^\\(root:\\)[^:]*\\(:.*\\)$|\\1')
+        file.write(phash.replace("$","\$"))
+        file.write('\\2|\'\n')
+
+        file.write("\n")
+        file.flush()
+        file.close()
+
+        os.chmod(filename, os.stat(filename).st_mode | stat.S_IEXEC)
+        print("AdminManager: Wrote %s" % filename)
+        return
 
 registrar.register_manager(AdminManager())
--- sync/openwrt/operations.py
+++ sync/openwrt/operations.py
@@ -1,11 +1,16 @@
 import sync.registrar
 
-sync.registrar.register_operation("restart-networking", [""], ["/etc/init.d/network reload"], 10, None)
 sync.registrar.register_operation("restart-nftables-rules", [""], ["/etc/init.d/nftables-rules restart"], 9, None)
-sync.registrar.register_operation("restart-wireless", [""], ["/sbin/wifi"], 10, None)
-sync.registrar.register_operation("restart-dhcp", [""], ["/etc/init.d/dnsmasq restart"], 10, None)
+
+sync.registrar.register_operation("restart-networking", [""], ["/etc/init.d/network reload"], 10, None)
 sync.registrar.register_operation("restart-qos", [""], ["/etc/config/nftables-rules.d/300-qos-rules-sys"], 10, "restart-nftables-rules")
 sync.registrar.register_operation("restart-default-route", [""], ["/etc/config/ifdown.d/10-default-route"], 10, "restart-networking")
 sync.registrar.register_operation("restart-wan-balancer", [""], ["/etc/config/ifup.d/20-wan-balancer"], 10, "restart-networking")
+
+sync.registrar.register_operation("restart-wireless", [""], ["/sbin/wifi"], 11, None)
+
+sync.registrar.register_operation("restart-dhcp", [""], ["/etc/init.d/dnsmasq restart"], 11, None)
+
+sync.registrar.register_operation("startup-scripts", [""], ["/etc/init.d/startup boot"], 12, None)
 
 
