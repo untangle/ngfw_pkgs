979fe568f23b58a05614ec0829cfa31c09db36cd
Dirk Morris <dmorris@untangle.com>
2019-01-07 04:58:16 +0000

    Add runtests subtree

--- update-subtrees.py
+++ update-subtrees.py
@@ -1,14 +1,18 @@
 #! /usr/bin/python3
+"""Update subtrees"""
 
 import subprocess
 
-SUBTREES = { 'untangle-python-sync-settings': 'git@github.com:untangle/sync-settings.git' }
+SUBTREES = {
+    'untangle-python-sync-settings': 'git@github.com:untangle/sync-settings.git',
+    'untangle-python-runtests': 'git@github.com:untangle/runtests.git'
+}
 
 branch = subprocess.getoutput('git symbolic-ref --short HEAD')
 
 for directory, repository in SUBTREES.items():
-  # FIXME: handle release branch as wwell
-  cmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
+    # FIXME: handle release branch as wwell
+    cmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
 
-  print('Updating {} from {}:{}'.format(directory, repository, branch))
-  subprocess.call(cmd, shell=True)
+    print('Updating {} from {}:{}'.format(directory, repository, branch))
+    subprocess.call(cmd, shell=True)
