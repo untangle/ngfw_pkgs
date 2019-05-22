c15ba66a4708462d99a92bcb2b5e4df21890590d
Sébastien Delafond <sdelafond@gmail.com>
2019-05-22 08:29:47 +0200

    Handle push in update-subtrees.py

--- update-subtrees.py
+++ update-subtrees.py
@@ -1,18 +1,37 @@
 #! /usr/bin/python3
-"""Update subtrees"""
 
+"""Update subtrees, and optionally push result back to origin"""
+
+import argparse
 import subprocess
 
+# constants
 SUBTREES = {
     'untangle-python-sync-settings': 'git@github.com:untangle/sync-settings.git',
     'untangle-python-runtests': 'git@github.com:untangle/runtests.git'
 }
 
+# CL args
+parser = argparse.ArgumentParser(description="Update subtrees, and optionally push result back to origin")
+parser.add_argument('--push', dest='push',
+                    action='store_true',
+                    default=False,
+                    help='push result back to origin (default=False)')
+
+# main
+args = parser.parse_args()
+
 branch = subprocess.getoutput('git symbolic-ref --short HEAD')
+origin = subprocess.getoutput('git remote').split('\n')[0]
 
 for directory, repository in SUBTREES.items():
-    # FIXME: handle release branch as wwell
-    cmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
+    # FIXME: handle release branch as well
+    updateCmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
 
     print('Updating {} from {}:{}'.format(directory, repository, branch))
-    subprocess.call(cmd, shell=True)
+    subprocess.call(updateCmd, shell=True)
+
+if args.push:
+    pushCmd = 'git push {} {}:{}'.format(origin, branch, branch)
+    print('Pushing with {}'.format(pushCmd))
+    subprocess.call(pushCmd, shell=True)
