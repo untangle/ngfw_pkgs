13dc02d908291f28666b81c925eb1da32af3bb77
Sébastien Delafond <sdelafond@gmail.com>
2019-05-23 09:08:08 +0200

    Push updates subtrees back to their origin (non-optional)

--- update-subtrees.py
+++ update-subtrees.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
-"""Update subtrees, and optionally push result back to origin"""
+"""Update subtrees via git subtree push+pull, after which it can optionally
+   push the main tree back to its origin"""
 
 import argparse
 import subprocess
@@ -9,12 +10,17 @@ import yaml
 # constants
 SUBTREES = yaml.load(open("subtrees.yaml"))['subtrees']
 
+# functions
+def run(cmd):
+    print('Running {}'.format(cmd))
+    subprocess.call(cmd, shell=True)
+
 # CL args
 parser = argparse.ArgumentParser(description="Update subtrees, and optionally push result back to origin")
 parser.add_argument('--push', dest='push',
                     action='store_true',
                     default=False,
-                    help='push result back to origin (default=False)')
+                    help="after subtrees' updates, push main tree back to its origin (default=False)")
 
 # main
 args = parser.parse_args()
@@ -24,12 +30,15 @@ origin = subprocess.getoutput('git remote').split('\n')[0]
 
 for directory, repository in SUBTREES.items():
     # FIXME: handle release branch as well
-    updateCmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
+    subtreeUpdateCmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
+    print('Updating subtree {} from {}:{}'.format(directory, repository, branch))
+    run(subtreeUpdateCmd)
 
-    print('Updating {} from {}:{}'.format(directory, repository, branch))
-    subprocess.call(updateCmd, shell=True)
+    subtreePushCmd = 'git subtree push --prefix={} {} {}'.format(directory, repository, branch)
+    print('Pushing subtree {} to {}:{}'.format(directory, repository, branch))
+    run(subtreePushCmd)
 
 if args.push:
     pushCmd = 'git push {} {}:{}'.format(origin, branch, branch)
     print('Pushing with {}'.format(pushCmd))
-    subprocess.call(pushCmd, shell=True)
+    run(pushCmd)
