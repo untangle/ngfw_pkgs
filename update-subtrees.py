6dde97f21e39fda867dfd108c74578789b77fd37
Sébastien Delafond <sdelafond@gmail.com>
2019-05-23 09:43:21 +0200

    Add --simulate to update-subtrees.py

--- update-subtrees.py
+++ update-subtrees.py
@@ -11,9 +11,10 @@ import yaml
 SUBTREES = yaml.load(open("subtrees.yaml"))['subtrees']
 
 # functions
-def run(cmd):
-    print('Running {}'.format(cmd))
-    subprocess.call(cmd, shell=True)
+def run(cmd, simulate=False):
+    print('... running {}'.format(cmd))
+    if not simulate:
+        subprocess.call(cmd, shell=True)
 
 # CL args
 parser = argparse.ArgumentParser(description="Update subtrees, and optionally push result back to origin")
@@ -21,6 +22,10 @@ parser.add_argument('--push', dest='push',
                     action='store_true',
                     default=False,
                     help="after subtrees' updates, push main tree back to its origin (default=False)")
+parser.add_argument('--simulate', dest='simulate',
+                    action='store_true',
+                    default=False,
+                    help="do not execute any push/pull command, instead only print them (default=False)")
 
 # main
 args = parser.parse_args()
@@ -32,13 +37,13 @@ for directory, repository in SUBTREES.items():
     # FIXME: handle release branch as well
     subtreeUpdateCmd = 'git subtree pull --prefix={} {} {}'.format(directory, repository, branch)
     print('Updating subtree {} from {}:{}'.format(directory, repository, branch))
-    run(subtreeUpdateCmd)
+    run(subtreeUpdateCmd, args.simulate)
 
     subtreePushCmd = 'git subtree push --prefix={} {} {}'.format(directory, repository, branch)
     print('Pushing subtree {} to {}:{}'.format(directory, repository, branch))
-    run(subtreePushCmd)
+    run(subtreePushCmd, args.simulate)
 
 if args.push:
     pushCmd = 'git push {} {}:{}'.format(origin, branch, branch)
-    print('Pushing with {}'.format(pushCmd))
-    run(pushCmd)
+    print('Pushing main tree with {}'.format(pushCmd))
+    run(pushCmd, args.simulate)
