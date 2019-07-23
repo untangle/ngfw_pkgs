6b3392a289e0b1f8915eb5b2c9e0f74bd1a0fddb
Dirk Morris <dmorris@untangle.com>
2019-07-23 09:33:34 -0700

    Convert policy to int

--- sync/openwrt/route_manager.py
+++ sync/openwrt/route_manager.py
@@ -114,8 +114,11 @@ class RouteManager:
                 if action.get("type") is None:
                     raise Exception("Missing action type in WAN rule" + str(rule_id))
                 if action.get("type") == "WAN_POLICY":
-                    if action.get("policy") not in policy_ids:
-                        raise Exception("WAN rule " + str(rule_id) + " uses missing WAN policy " + str(action.get("policy")))
+                    # FIXME MFW-520 - currently policy is getting saved as a string
+                    # temp fix here
+                    policy = int(action.get("policy"))
+                    if policy not in policy_ids:
+                        raise Exception("WAN rule " + str(rule_id) + " uses missing WAN policy " + str(policy))
 
     def create_settings(self, settings, prefix, delete_list, filename):
         """creates settings"""
