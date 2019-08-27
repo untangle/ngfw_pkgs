1b2463cab3669985a3f93b634ce5c7fa944c8dec
Dirk Morris <dmorris@untangle.com>
2019-08-27 11:41:10 -0700

    fix enable check

--- sync/openwrt/route_manager.py
+++ sync/openwrt/route_manager.py
@@ -595,7 +595,7 @@ def enabled_wan(intf):
     if intf is None:
         return False
 
-    if not intf.get('enabled') and intf.get('wan'):
+    if intf.get('enabled') and intf.get('wan'):
         return True
     return False
 
