bc74f3a9174c529cdaa0ac8de89627fe081b335f
Brett Mastbergen <bmastbergen@untangle.com>
2019-11-20 16:49:25 -0500

    nat_manager: Remove unused miniupnpd chain

--- sync/openwrt/nat_manager.py
+++ sync/openwrt/nat_manager.py
@@ -72,14 +72,11 @@ add chain ip nat-sys prerouting-nat  { type nat hook prerouting priority -50 ; }
 add chain ip6 nat-sys postrouting-nat { type nat hook postrouting priority 100 ; }
 add chain ip6 nat-sys prerouting-nat  { type nat hook prerouting priority -50 ; }
 
-add chain ip nat-sys miniupnpd
 add chain ip nat-sys nat-rules-sys
 
 add rule ip nat-sys postrouting-nat oifname lo accept
 add rule ip nat-sys postrouting-nat iifname lo accept
 add rule ip nat-sys postrouting-nat jump nat-rules-sys
-
-add rule ip nat-sys prerouting-nat jump miniupnpd
 
 
 """)
