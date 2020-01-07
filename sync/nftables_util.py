9f893ed0c5e31f8f629d0308ba7af5009fc34738
Brett Mastbergen <bmastbergen@untangle.com>
2020-01-07 14:56:25 -0500

    nftables_util: Log wan_rule_id to dict in WAN_POLICY action rules

--- sync/nftables_util.py
+++ sync/nftables_util.py
@@ -783,6 +783,12 @@ def clean_rule_actions(parent, array, tableName=None):
                         {
                             "type": "NFLOG",
                             "prefix": "{\'type\':\'rule\',\'table\':\'wan-routing\',\'chain\':\'%s\',\'ruleId\':%d,\'action\':\'WAN_POLICY\',\'policy\':%d} " % (parent.get('name'), item.get('ruleId'), action.get('policy')),
+                        },
+                        {
+                            "type": "DICT",
+                            "field": "wan_rule_id",
+                            "field_type": "INT",
+                            "value": "%d" % item.get('ruleId'),
                         }
                     ]
 
