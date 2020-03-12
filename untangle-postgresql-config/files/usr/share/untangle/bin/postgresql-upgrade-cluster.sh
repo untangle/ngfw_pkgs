824532431553fd3221fbb6cf89e4c67464beaa1f
Sébastien Delafond <sdelafond@gmail.com>
2020-03-12 11:26:00 +0100

    postgresql-11 initially reading postgres-9.6's config is making this tricky
    Comment: NGFW-12624

--- untangle-postgresql-config/files/usr/share/untangle/bin/postgresql-upgrade-cluster.sh
+++ untangle-postgresql-config/files/usr/share/untangle/bin/postgresql-upgrade-cluster.sh
@@ -16,23 +16,12 @@ PG_CONF_NEW="/etc/postgresql/${VERSION_NEW}/main/postgresql.conf"
 if [ -d $PG_VAR_DIR_OLD ] ; then
   echo "[$(date +%Y-%m%-dT%H:%m)] Starting conversion"
 
-  # perform this on the old version otherwise the new 9.6 version causes
-  # it not to start
-  sed -i -e "s/.*autovacuum_analyze_scale_factor.*=.*/autovacuum_analyze_scale_factor = 0.5/" ${PG_CONF_OLD}
-
   # Stop all instances
-  systemctl stop postgresql || true
-  /etc/init.d/postgresql stop || true
+  systemctl stop postgresql
 
-  # Start old
-  systemctl start postgresql@${VERSION_OLD}-main || true
+  # Drop old extension (prevent upgrade from working)
+  psql -U postgres -c "drop extension tablefunc" uvm
 
-  # Drop old extension (pervent upgrade from working)
-  psql -U postgres -c "drop extension tablefunc" uvm || true
-
-  # Stop all instances
-  systemctl stop postgresql || true
-  
   # Run conversion
   pushd /tmp
   su postgres -c "/usr/lib/postgresql/${VERSION_NEW}/bin/pg_upgrade --link -b $PG_BIN_DIR_OLD -B $PG_BIN_DIR_NEW -d $PG_VAR_DIR_OLD -D $PG_VAR_DIR_NEW -o ' -c config_file='$PG_CONF_OLD -O ' -c config_file='$PG_CONF_NEW"
@@ -43,9 +32,8 @@ if [ -d $PG_VAR_DIR_OLD ] ; then
       echo "[$(date +%Y-%m%-dT%H:%m)] Conversion complete"
       echo
   fi
-  
-  pg_dropcluster $VERSION_OLD main
-  rm -fr $PG_VAR_DIR_OLD
   popd
 
+  pg_dropcluster $VERSION_OLD main
+  rm -fr $PG_VAR_DIR_OLD
 fi
