98e8b7d6656ea6ef7f271280f5b70e4db6a034d7
Chris Blaise <cblaise@untangle.com>
2020-05-04 10:33:56 -0400

    Make sure we only use our address as a single address, removing the network.

--- sync/debian/wireguard_manager.py
+++ sync/debian/wireguard_manager.py
@@ -101,7 +101,9 @@ class WireguardManager(Manager):
         # Interface
         self.out_file.write("[Interface]\n")
         self.out_file.write("PrivateKey={privateKey}\n".format(privateKey=settings_file.settings.get('privateKey')))
-        self.out_file.write("Address={address}\n".format(address=settings_file.settings.get('addressPool')))
+        address =  settings_file.settings.get('addressPool')
+        if address is not None:
+            self.out_file.write("Address={address}\n".format(address=address.split('/')[0]))
         self.out_file.write("ListenPort={listenPort}\n".format(listenPort=settings_file.settings.get('listenPort')))
         if settings_file.settings.get('mtu') != 0:
             self.out_file.write("MTU={mtu}\n".format(mtu=settings_file.settings.get('mtu')))
