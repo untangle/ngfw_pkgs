caf4e951c6dcee2dc8a60a985a4f67443fb5be45
Tiffany Kalin <tkalin@untangle.com>
2021-05-06 10:27:12 -0600

    waf-73: add in ip_allow_list file

--- sync/nginx/ip_lists_manager.py
+++ sync/nginx/ip_lists_manager.py
@@ -2,9 +2,12 @@
 from sync import registrar, manager
 
 class IPListsManager(Manager):
+    ip_allow_list = "/etc/ipAllowList"
+    
     def initialize(self):
         """initialize this module"""
         registrar.register_settings_file("settings", self)
+        registrar.register_file(self.ip_allow_list, None, self)
 
     def create_settings(self, settings_file, prefix, delete_list, filename):
         """creates settings"""
