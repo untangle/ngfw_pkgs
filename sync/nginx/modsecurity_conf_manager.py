70bb2a5a8627b35ecc3bebaf8ec12cd3c5308b1a
Tiffany Kalin <tkalin@untangle.com>
2021-06-24 11:49:44 -0600

    waf-104: add in http policy args to crs_setup

--- sync/nginx/modsecurity_conf_manager.py
+++ sync/nginx/modsecurity_conf_manager.py
@@ -42,8 +42,8 @@ class ModsecurityConfManager(Manager):
         file.write("# [[ Mode of Operation ]] \n")
         file.write("# Mode that the CRS will run in. At least one mode must be selected. \n")
         file.write("# Default is anomaly scoring mode logging to modsecurity audit and error logs\n")
-        file.write('SecDefaultAction "phase:1,log,auditlog,pass"\n')
-        file.write('SecDefaultAction "phase:2,log,auditlog,pass"\n')
+        file.write("SecDefaultAction \"phase:1,log,auditlog,pass\"\n")
+        file.write("SecDefaultAction \"phase:2,log,auditlog,pass\"\n")
 
         # paranoia level initialization
         file.write("\n\n")
@@ -51,22 +51,22 @@ class ModsecurityConfManager(Manager):
         file.write("# Allows you to choose the desired level of rule checks that will add to your security scores\n")
         file.write("Using defaults of 1, so rule is commented\n") # comment could change if we uncomment rule
         file.write("#SecAction \ \n")
-        file.write('#  "id:900000,\ \n')
-        file.write('#   phase:1,\ \n')
-        file.write("#   nolog,\ \n")
-        file.write("#   pass,\ \n")
-        file.write("#   t:none,\ \n")
-        file.write('#   setvar:tx.paranoia_level=1"\n')
-        file.write("\n")
-        file.write("# Executing paranoia level - add rules from higher paranoia levels\n")
-        file.write("# Default is disabled, so rule is commented") # comment could change if we uncomment rule
-        file.write("#SecAction \ \n")
-        file.write('#  "id:900001,\ \n')
+        file.write("#  \"id:900000,\ \n")
         file.write("#   phase:1,\ \n")
         file.write("#   nolog,\ \n")
         file.write("#   pass,\ \n")
         file.write("#   t:none,\ \n")
-        file.write('#   setvar:tx.executing_paranoia_level=1"\n')
+        file.write("#   setvar:tx.paranoia_level=1\"\n")
+        file.write("\n")
+        file.write("# Executing paranoia level - add rules from higher paranoia levels\n")
+        file.write("# Default is disabled, so rule is commented") # comment could change if we uncomment rule
+        file.write("#SecAction \ \n")
+        file.write("#  \"id:900001,\ \n")
+        file.write("#   phase:1,\ \n")
+        file.write("#   nolog,\ \n")
+        file.write("#   pass,\ \n")
+        file.write("#   t:none,\ \n")
+        file.write("#   setvar:tx.executing_paranoia_level=1\"\n")
 
         # enforce body processor encoded
         file.write("\n\n")
@@ -75,12 +75,12 @@ class ModsecurityConfManager(Manager):
         file.write("# from Content-Type request header of client.")
         file.write("# Default is off, so rule is commented as the uncommented rule would turn it on. \n") # comment could change if we uncomment rule
         file.write("#SecAction \ \n")
-        file.write('#  "id:900010,\ \n')
+        file.write("#  \"id:900010,\ \n")
         file.write("#   phase:1,\ \n")
         file.write("#   nolog,\ \n")
         file.write("#   pass,\ \n")
         file.write("#   t:none,\ \n")
-        file.write('#   setvar:tx.enforce_bodyproc_urlencoded=1"\n')
+        file.write("#   setvar:tx.enforce_bodyproc_urlencoded=1\"\n")
 
         # anomaly scoring mode severity levels
         file.write("\n\n")
@@ -90,7 +90,7 @@ class ModsecurityConfManager(Manager):
         file.write("# Adjustment usually not needed\n")
         file.write("# Using default, so rule is commented.\n") # comment could change if we uncomment rule
         file.write("#SecAction \ \n")
-        file.write('# "id:900100,\ \n')
+        file.write("# \"id:900100,\ \n")
         file.write("#  phase:1,\ \n")
         file.write("#  nolog,\ \n")
         file.write("#  pass,\ \n")
@@ -98,7 +98,7 @@ class ModsecurityConfManager(Manager):
         file.write("#  setvar:tx.critical_anomaly_score=5,\ \n")
         file.write("#  setvar:tx.error_anomaly_score=4,\ \n")
         file.write("#  setvar:tx.warning_anomaly_score=3,\ \n")
-        file.write('#  setvar:tx.notice_anomaly_score=2"\n')
+        file.write("#  setvar:tx.notice_anomaly_score=2\"\n")
 
         # anomaly scoring mode blocking threshold levels
         file.write("\n\n")
@@ -108,13 +108,13 @@ class ModsecurityConfManager(Manager):
         file.write("# At default value, a single critical rule match will cause the request to be blocked.\n")
         file.write("# Using default value, so rule is commented.\n") # comment could change if we uncomment rule
         file.write("#SecAction \ \n")
-        file.write('# "id:900110,\ \n')
+        file.write("# \"id:900110,\ \n")
         file.write("#  phase:1,\ \n")
         file.write("#  nolog,\ \n")
         file.write("#  pass,\ \n")
         file.write("#  t:none,\ \n")
         file.write("#  setvar:tx.inbound_anomaly_score_threshold=5,\ \n")
-        file.write('#  setvar:tx.outbound_anomaly_score_threshold=4" \n')
+        file.write("#  setvar:tx.outbound_anomaly_score_threshold=4\" \n")
 
         # early anomaly scoring mode blocking
         file.write("\n\n")
@@ -123,12 +123,12 @@ class ModsecurityConfManager(Manager):
         file.write("# Evaluation usually occurs at the end of phase:2 and phase:4. \n")
         file.write("# Default is off, so rule is commented \n") # comment could change if we uncomment rule
         file.write("#SecAction \ \n")
-        file.write('#  "id:900120,\ \n')
+        file.write("#  \"id:900120,\ \n")
         file.write("#  phase:1,\ \n")
         file.write("#  nolog,\ \n")
         file.write("#  pass,\ \n")
         file.write("#  t:none,\ \n")
-        file.write('#  setvar:tx.blocking_early=1"\n')
+        file.write("#  setvar:tx.blocking_early=1\"\n")
 
         # app specific rule exclusions 
         file.write("\n\n")
@@ -138,7 +138,7 @@ class ModsecurityConfManager(Manager):
         file.write("# specific exclusions on an application by application basis. \n")
         file.write("By default, no exclusions apply, so rule is commented, \n") # comment could change if we uncomment rule
         file.write("#SecAction \ \n")
-        file.write('# "id:900130,\ \n')
+        file.write("# \"id:900130,\ \n")
         file.write("#  phase:1,\ \n")
         file.write("#  nolog,\ \n")
         file.write("#  pass,\ \n")
@@ -151,7 +151,7 @@ class ModsecurityConfManager(Manager):
         file.write("#  setvar:tx.crs_exclusions_phpbb=1,\ \n")
         file.write("#  setvar:tx.crs_exclusions_phpmyadmin=1,\ \n")
         file.write("#  setvar:tx.crs_exclusions_wordpress=1,\ \n")
-        file.write('#  setvar:tx.crs_exclusions_xenforo=1"\n')
+        file.write("#  setvar:tx.crs_exclusions_xenforo=1\"\n")
 
         # HTTP policy settings - methods
         # UNTANGLE CHANGED NORMAL DEFAULTS
@@ -160,7 +160,7 @@ class ModsecurityConfManager(Manager):
         file.write("# HTTP methods that a client is allowed to use \n")
         file.write("# Allowing methods for normal websites and RESTful APIs \n") # comment could change if rule changes
         file.write("SecAction \ \n")
-        file.write(' "id:900200,\ \n')
+        file.write(" \"id:900200,\ \n")
         file.write("  phase:1,\     \n")
         file.write("  nolog,\ \n")
         file.write("  pass,\ \n")
@@ -173,7 +173,7 @@ class ModsecurityConfManager(Manager):
         file.write("# Allowed Content-Types header from clients \n")
         file.write("# Using defaults, so rule is commented. \n") # comment could change if rule changes
         file.write("#SecAction \ \n")
-        file.write('# "id:900220,\ \n')
+        file.write("# \"id:900220,\ \n")
         file.write("#  phase:1,\ \n")
         file.write("#  nolog,\ \n")
         file.write("#  pass,\ \n")
@@ -183,9 +183,132 @@ class ModsecurityConfManager(Manager):
         file.write("|application/cloudevents+json| |application/cloudevents-batch+json| |application/octet-stream| |application/csp-report| ")
         file.write("|application/xss-auditor-report| |text/plain|'\"\n")
 
-        
+        # HTTP POlicy settings - HTTP versions
+        file.write("\n\n")
+        file.write("# [[ HTTP Policy Settings - Allowed HTTP Versions ]] \n")
+        file.write("# Using defaults, so rule is commented.\n") # comment could change if rule changes
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900230,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:'tx.allowed_http_versions=HTTP/1.0 HTTP/1.1 HTTP/2 HTTP/2.0'\"\n")
+
+        #HTTP Policy settings - forbidden file extensions
+        file.write("\n\n")
+        file.write("# [[ HTTP Policy Settings - Forbidden File Extensions ]] \n")
+        file.write("# Guards against unintended exposure of development/configuration files.\n")
+        file.write("# Using defaults, so rule is commented. \n") # comment could change if rule changes
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900240,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:'tx.restricted_extensions=.asa/ .asax/ .ascx/ .axd/ .backup/ .bak/ .bat/ .cdx/ .cer/ .cfg/ ")
+        file.write(".cmd/ .com/ .config/ .conf/ .cs/ .csproj/ .csr/ .dat/ .db/ .dbf/ .dll/ .dos/ .htr/ .htw/ .ida/ .idc/ ")
+        file.write(".idq/ .inc/ .ini/ .key/ .licx/ .lnk/ .log/ .mdb/ .old/ .pass/ .pdb/ .pol/ .printer/ .pwd/ .rdb/ .resources/ ")
+        file.write(".resx/ .sql/ .swp/ .sys/ .vb/ .vbs/ .vbproj/ .vsdisco/ .webinfo/ .xsd/ .xsx/'\"\n")
 
 
+        #HTTP Policy Settings - file extensions 
+        file.write("\n\n")
+        file.write("# [[ HTTP Policy Settings - File Extensions ]] \n")
+        file.write("# These are file extensions considered static files for DoS protection\n")
+        file.write("# Using defaults, so rule is commented. \n") # comment could change if rule changes
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900260,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:'tx.static_extensions=/.jpg/ /.jpeg/ /.png/ /.gif/ /.js/ /.css/ /.ico/ /.svg/ /.webp/'\"\n")
+
+        # HTTP Policy Settings - Content-Types charsets 
+        file.write("\n\n")
+        file.write("# [[ HTTP Policy Settings - Content-Types charsets ]] \n")
+        file.write("# Content-Types charsets that a client is allowed to send in a request.\n")
+        file.write("# Using defaults, so rule is commented. \n") # comment could change if rule changes
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900280,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:'tx.allowed_request_content_type_charset=|utf-8| |iso-8859-1| |iso-8859-15| |windows-1252|'\"\n")
+
+        #HTTP Policy Argument/Upload Limits - Block request if number of args is too high
+        file.write("\n\n")
+        file.write(" [[ HTTP Policy Argument/Upload Limits - Block request if number of args is too high ]] \n")
+        file.write("# Using default of unlimited, so rule is commented. \n") # comment could change if rule changes)
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900300,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:tx.max_num_args=255\"\n")
+
+        #HTTP Policy Argument/Upload Limits - block request if the length of any argument name is too high
+        file.write("\n\n")
+        file.write(" [[ HTTP Policy Argument/Upload Limits - block request if the length of any argument name is too high ]] \n")
+        file.write("# Using default of unlimited, so rule is commented. \n") # comment could change if rule changes)
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900310,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:tx.arg_name_length=255\"\n")
+
+        #HTTP Policy Argument/Upload Limits - block request if length of any argument length is too high
+        file.write("\n\n")
+        file.write(" [[ HTTP Policy Argument/Upload Limits - block request if length of any argument length is too high ]] \n")
+        file.write("# Using default of unlimited, so rule is commented. \n") # comment could change if rule changes)
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900320,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:tx.arg_length=255\"\n")
+
+        #HTTP Policy Argument/Upload Limits - block request if total length of all combined arguments is too high
+        file.write("\n\n")
+        file.write(" [[ HTTP Policy Argument/Upload Limits - block request if total length of all combined arguments is too high ]] \n")
+        file.write("# Using default of unlimited, so rule is commented. \n") # comment could change if rule changes)
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900330,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:tx.total_arg_length=255\"\n")
+
+        #HTTP Policy Argument/Upload Limits - Block request if file size of any individual uploaded file is too high
+        file.write("\n\n")
+        file.write(" [[ HTTP Policy Argument/Upload Limits - Block request if file size of any individual uploaded file is too high ]] \n")
+        file.write("# Using default of unlimited, so rule is commented. \n") # comment could change if rule changes)
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900340,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:tx.max_file_size=255\"\n")
+
+        #HTTP Policy Argument/Upload Limits - Block request if the total size of all combined uploaded files is too high
+        file.write("\n\n")
+        file.write(" [[ HTTP Policy Argument/Upload Limits - Block request if the total size of all combined uploaded files is too high ]] \n")
+        file.write("# Using default of unlimited, so rule is commented. \n") # comment could change if rule changes)
+        file.write("#SecAction \ \n")
+        file.write("# \"id:900350,\ \n")
+        file.write("#  phase:1,\ \n")
+        file.write("#  nolog,\ \n")
+        file.write("#  pass,\ \n")
+        file.write("#  t:none,\ \n")
+        file.write("#  setvar:tx.combined_file_sizes=255\"\n")
 
 
 
