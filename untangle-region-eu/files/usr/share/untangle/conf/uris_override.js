9fa8bdb6a9c7c0a3dc4803b1e9f003d4db1371a3
Chris Blaise <cblaise@untangle.com>
2021-08-27 10:02:31 -0400

    NGFW-13824 Add initial uri override

---
+++ untangle-region-eu/files/usr/share/untangle/conf/uris_override.js
@@ -0,0 +1,84 @@
+{
+    "dnsTestHost": "updates.untangle.com",
+    "javaClass": "com.untangle.uvm.UriManagerSettings",
+    "tcpTestHost": "updates.untangle.com",
+    "uriTranslations": {
+        "javaClass": "java.util.LinkedList",
+        "list": [
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://ids.untangle.com/suricatasignatures.tar.gz"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://labs.untangle.com/Utility/v1/mac"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://auth-relay.untangle.com/callback.php"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://telemetry.untangle.com/ngfw/v1/infection"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "http://updates.untangle.com/"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://cmd.untangle.com/",
+                "host": "cmd-eu.untangle.com"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://license.untangle.com/license.php"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://classify.untangle.com/v1/md5s"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "http://bd.untangle.com/"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://boxbackup.untangle.com/boxbackup/backup.php",
+                "host": "boxbackup-eu.untangle.com"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://boxbackup.untangle.com/api/index.php",
+                "host": "boxbackup-eu.untangle.com"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://downloads.untangle.com/download.php"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "http://translations.untangle.com/"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://queue.untangle.com/",
+                "host": "queue-eu.untangle.com"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://untangle.com/api/v1/appliance/OnSettingsUpdate",
+                "host": "eu-untangle.com"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://supssh.untangle.com/"
+            },
+            {
+                "javaClass": "com.untangle.uvm.UriTranslation",
+                "uri": "https://sshrelay.untangle.com/"
+            }
+        ]
+    },
+    "version": 3
+}
