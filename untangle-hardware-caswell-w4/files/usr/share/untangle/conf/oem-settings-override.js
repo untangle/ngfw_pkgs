facd39dcc475c47623535ce5b0e58eaeb75b4e23
mahotz <mahotz@untangle.com>
2021-11-18 17:36:08 -0700

    Add new Caswell w4 and w8 packages to build-order.txt
    Updated build-order.txt and also fixed syntax and spacing in
    the settings override files.

--- untangle-hardware-caswell-w4/files/usr/share/untangle/conf/oem-settings-override.js
+++ untangle-hardware-caswell-w4/files/usr/share/untangle/conf/oem-settings-override.js
@@ -1,41 +1,43 @@
 {
-  "NetworkSettings": {
-    "interfaces": {
-      "list": [{
-        "find":
-          { "systemDev": "eth0" },
-        "change":
-          { "name": "INTERNET" }
-        },{
-        "find":
-          { "systemDev": "eth1" },
-        "change":
-          {
-            "name": "Interface 2",
-          }
-        },{
-        "find":
-          { "systemDev": "eth2" },
-        "change":
-          { 
-            "name": "Interface 3",
-            "configType": "BRIDGED",
-            "bridgedTo": 2,
-            "v4ConfigType": "STATIC",
-            "v6ConfigType": "STATIC"
-          }
-        },{
-        "find":
-          { "systemDev": "eth3" },
-        "change":
-          { 
-            "name": "Interface 4",
-	    "configType": "BRIDGED",
-            "bridgedTo": 2,
-            "v4ConfigType": "STATIC",
-            "v6ConfigType": "STATIC"
-	  }
+"NetworkSettings": {
+  "interfaces": {
+    "list": [{
+      "find":
+        { "systemDev": "eth0" },
+      "change":
+        {
+          "name": "INTERNET"
+        }
+      },{
+      "find":
+        { "systemDev": "eth1" },
+      "change":
+        {
+          "name": "Interface 2"
+        }
+      },{
+      "find":
+        { "systemDev": "eth2" },
+      "change":
+        { 
+          "name": "Interface 3",
+          "configType": "BRIDGED",
+          "bridgedTo": 2,
+          "v4ConfigType": "STATIC",
+          "v6ConfigType": "STATIC"
+        }
+      },{
+      "find":
+        { "systemDev": "eth3" },
+      "change":
+        {
+          "name": "Interface 4",
+          "configType": "BRIDGED",
+          "bridgedTo": 2,
+          "v4ConfigType": "STATIC",
+          "v6ConfigType": "STATIC"
+        }
       }]
-    },
+    }
   }
 }
