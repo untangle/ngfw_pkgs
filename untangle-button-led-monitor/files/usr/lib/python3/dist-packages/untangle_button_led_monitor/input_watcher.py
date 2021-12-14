2a955f54c687c7269bdab6219623b3db7bf850dd
Chris Blaise <cblaise@untangle.com>
2021-12-14 09:33:56 -0500

    NGFW-13735 Remove older, driver specific processing for more generalized approach.

--- untangle-button-led-monitor/files/usr/lib/python3/dist-packages/untangle_button_led_monitor/input_watcher.py
+++ untangle-button-led-monitor/files/usr/lib/python3/dist-packages/untangle_button_led_monitor/input_watcher.py
@@ -1,4 +1,6 @@
+import os
 import struct
+import sys
 
 from untangle_button_led_monitor import Logger, Settings
 
@@ -6,24 +8,16 @@ class InputWatcher:
     """
     Singleton class to monitor input devices
     """
-    # Binary format for input events:
-    # time_seconds
-    # time_microseconds
-    # input type
-    # input code
-    # input value
-    event_format = 'llHHI'
-
     path_file = {}
     path_method = {}
 
     ready = None
+
     @classmethod
     def static_init(cls):
         """
         Initalize singleton
         """
-        InputWatcher.event_size = struct.calcsize(InputWatcher.event_format)
         InputWatcher.ready = True
 
     @classmethod
@@ -35,7 +29,8 @@ class InputWatcher:
     def unregister(cls, path):
         if path in InputWatcher.path_file:
             try:
-                InputWatcher.path_file[path].close()
+                if InputWatcher.path_file[path] is not None and not InputWatcher.path_file[path].closed: 
+                    InputWatcher.path_file[path].close()
             except:
                 Logger.message(f'Cannot close file for {path}', sys.exc_info(), target="log")
             del InputWatcher.path_file[path]
@@ -48,7 +43,7 @@ class InputWatcher:
             Logger.message()
 
         for path in InputWatcher.path_file:
-            if InputWatcher.path_file[path] is None:
+            if InputWatcher.path_file[path] is None or InputWatcher.path_file[path].closed:
                 if os.path.exists(path) == False:
                     continue
                 try:
@@ -58,29 +53,8 @@ class InputWatcher:
                     Logger.message(f'Cannot open file for {path}', sys.exc_info(), target="log")
                     continue
 
-            file = InputWatcher.path_file[path]
-
-            while True:
-                try:
-                    event = file.read(InputWatcher.event_size)
-                except:
-                    Logger.message(f'Cannot read from file for {path}', sys.exc_info(), target="log")
-                    InputWatcher.path_file[path] = None
-                    event = None
-
-                if event is None:
-                    break
-
-                (tv_sec, tv_usec, type, code, value) = struct.unpack(InputWatcher.event_format, event)
-
-                if type != 0 or code != 0 or value != 0:
-                    if Settings.Debug:
-                        Logger.message(f'Event type {type}, code {code}, value{value} at {tv_sec}.{tv_usec}')
-                    InputWatcher.path_method[path](path, tv_sec, tv_usec, type, code, value)
-                else:
-                    if Settings.Debug:
-                        # Events with code, type and value == 0 are "separator" events
-                        Logger.message("===========================================")
+            # Call subscriber check method.
+            InputWatcher.path_method[path](path, InputWatcher.path_file[path])
 
 if InputWatcher.ready is None:
     InputWatcher.static_init()
