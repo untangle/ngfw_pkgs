8cc4d152dd432d3537adea988e896491be41dc8a
Chris Blaise <cblaise@untangle.com>
2021-12-15 12:42:55 -0500

    NGFW-13734 Add model checking for w4/w8 to use different gpio pins

--- untangle-hardware-caswell-driver-gpio-nuvoton/files/usr/lib/python3/dist-packages/untangle_hardware_caswell_driver_gpio_nuvoton/untangle_button_led_monitor/nuvoton_leds.py
+++ untangle-hardware-caswell-driver-gpio-nuvoton/files/usr/lib/python3/dist-packages/untangle_hardware_caswell_driver_gpio_nuvoton/untangle_button_led_monitor/nuvoton_leds.py
@@ -1,3 +1,5 @@
+import sys
+
 from untangle_button_led_monitor import Leds, Logger, Settings
 
 class NuvotonLeds(Leds):
@@ -9,11 +11,22 @@ class NuvotonLeds(Leds):
     gpio_output_path="/sys/class/misc/gpio-nuvoton/output"
     gpio_output_content_format = "{id} {value}"
 
-    led_actions = {
-        "wifi": {
-            "off":  [{ 'id': 74, 'value': 1 }],
-            "on":   [{ 'id': 74, 'value': 0 }],
+    # File to read to get system model.
+    model_path = "/usr/share/untangle/conf/appliance-model"
+    # led_actions to write to  are based on model type
+    model_led_actions = {
+        "w4":  {
+            "wifi": {
+                "off":  [{ 'id': 74, 'value': 1 }],
+                "on":   [{ 'id': 74, 'value': 0 }],
+            },
         },
+        "w8": {
+            "wifi": {
+                "off":  [{ 'id': 64, 'value': 1 }],
+                "on":   [{ 'id': 64, 'value': 0 }],
+            },
+        }
     }
 
     status = {
@@ -21,6 +34,31 @@ class NuvotonLeds(Leds):
             "action": "off"
         }
     }
+
+    def __init__(self):
+        """
+        Initialize object
+        """
+        # Determine model and led action set to use.
+        self.model = None
+        try:
+            with open(NuvotonLeds.model_path, "r") as model_file:
+                self.model = model_file.read().rstrip()
+        except:
+            Logger.message("Cannot read", sys.exc_info(), target="log")
+
+        if self.model in NuvotonLeds.model_led_actions:
+            self.led_actions = NuvotonLeds.model_led_actions[self.model]
+
+        if self.model == None:
+            Logger.message("Unable to find model")
+        elif not self.model in NuvotonLeds.model_led_actions:
+            Logger.message("Unable to find model led actions for model " + self.model)
+
+        self.ready = True
+        if Settings.Debug:
+            Logger.message("model = " + self.model)
+            Logger.message(self.led_actions)
 
     def open_gpio(self):
         """
