28d0aea3a37c365e4fc84aed63b46827b273d6a7
Sébastien Delafond <sdelafond@gmail.com>
2022-02-15 13:15:55 +0100

    u-postgresql-config: handle conversion to 13.0
    Comment: NGFW-13899

--- untangle-postgresql-config/files/usr/share/untangle/bin/postgresql-upgrade-cluster.sh
+++ untangle-postgresql-config/files/usr/share/untangle/bin/postgresql-upgrade-cluster.sh
@@ -2,8 +2,11 @@
 
 set -xu
 
+## variables
 VERSION_OLD=$1
 VERSION_NEW=$2
+
+SCRIPT_NAME=$(basename $0)
 
 PG_VAR_DIR_OLD="/var/lib/postgresql/${VERSION_OLD}/main"
 PG_BIN_DIR_OLD="/usr/lib/postgresql/${VERSION_OLD}/bin"
@@ -13,28 +16,39 @@ PG_VAR_DIR_NEW="/var/lib/postgresql/${VERSION_NEW}/main"
 PG_BIN_DIR_NEW="/usr/lib/postgresql/${VERSION_NEW}/bin"
 PG_CONF_NEW="/etc/postgresql/${VERSION_NEW}/main/postgresql.conf"
 
-if [ -d $PG_VAR_DIR_OLD ] ; then
-  echo "[$(date +%Y-%m%-dT%H:%m)] Starting conversion"
+## functions
+log() {
+  echo "[$(date +%Y-%m-%dT%H:%m)] ${SCRIPT_NAME} $1"
+}
 
-  # Drop old extension (prevent upgrade from working)
-  psql -U postgres -c "drop extension tablefunc" uvm
-
-  # Stop all instances
-  systemctl stop postgresql
-  sleep 3
-
-  # Run conversion
-  pushd /tmp
-  su postgres -c "/usr/lib/postgresql/${VERSION_NEW}/bin/pg_upgrade --link -b $PG_BIN_DIR_OLD -B $PG_BIN_DIR_NEW -d $PG_VAR_DIR_OLD -D $PG_VAR_DIR_NEW -o ' -c config_file='$PG_CONF_OLD -O ' -c config_file='$PG_CONF_NEW"
-  if [ $? != 0 ] ; then
-      echo "[$(date +%Y-%m%-dT%H:%m)] Conversion FAILED"
-      echo
-  else
-      echo "[$(date +%Y-%m%-dT%H:%m)] Conversion complete"
-      echo
-  fi
-  popd
-
-  pg_dropcluster $VERSION_OLD main
-  rm -fr $PG_VAR_DIR_OLD
+## main
+if [ $VERSION_OLD = $VERSION_NEW ] || [ ! -d $PG_VAR_DIR_OLD ] ; then
+  exit 0
 fi
+
+log "starting conversion from $VERSION_OLD to $VERSION_NEW"
+
+# Drop old extension (prevent upgrade from working)
+psql -U postgres -c "drop extension tablefunc" uvm
+
+# Stop all instances
+systemctl stop postgresql
+sleep 3
+
+# Run conversion
+pushd /tmp
+su postgres -c "/usr/lib/postgresql/${VERSION_NEW}/bin/pg_upgrade --link -b $PG_BIN_DIR_OLD -B $PG_BIN_DIR_NEW -d $PG_VAR_DIR_OLD -D $PG_VAR_DIR_NEW -o ' -c config_file='$PG_CONF_OLD -O ' -c config_file='$PG_CONF_NEW"
+if [ $? != 0 ] ; then
+  status="FAILURE"
+else
+  status="SUCCESS"
+fi
+popd
+
+log "conversion result: $status"
+
+# remove old cluster and associated files
+pg_dropcluster $VERSION_OLD main
+rm -fr $PG_VAR_DIR_OLD
+log "dropped old cluster"
+
