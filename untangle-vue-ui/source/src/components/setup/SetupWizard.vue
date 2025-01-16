49d9e762ce0564bbdd4d00b234e7ee8a5bd19f0b
VarshaKache <varsharani.kache@arista.com>
2025-01-16 10:15:48 +0530

    ngfw-14953 setupwizard view

---
+++ untangle-vue-ui/source/src/components/setup/SetupWizard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="text-center custom-margin" style="max-width: 800px">
+    <v-img :src="require(`@/assets/${logo}`)" contain style="padding-bottom: 10%" />
+
+    <h1>{{ `Thanks for choosing Arista!` }}</h1>
+
+    <p>{{ `A wizard will guide you through the initial setup and configuration of the Arista Server.` }}</p>
+
+    <div>
+      <u-btn style="text-transform: none" @click="onContinue">{{ `Run Setup Wizard` }}</u-btn>
+    </div>
+  </v-container>
+</template>
+
+<style scoped>
+  .custom-margin {
+    margin-top: 10px;
+  }
+</style>
+
+<script>
+  export default {
+    computed: {
+      logo() {
+        return this.$vuetify.theme.isDark ? 'BrandingLogo.png' : 'BrandingLogo.png'
+      },
+    },
+  }
+</script>
--- untangle-vue-ui/source/src/router/setup.js
+++ untangle-vue-ui/source/src/router/setup.js
@@ -1,7 +1,8 @@
 import SetupLayout from '@/layouts/SetupLayout'
 import Main from '@/components/setup/Main.vue'
 import License from '@/components/setup/License.vue'
-// import System from '@/components/setup/System.vue'
+import Wizard from '@/components/setup/SetupWizard.vue'
+import ConfigureServer from '@/components/setup/ConfigureServer.vue'
 // import Wan from '@/components/setup/Wan.vue'
 // import Lte from '@/components/setup/Lte.vue'
 // import Wifi from '@/components/setup/Wifi.vue'
@@ -15,7 +16,8 @@ export default [
     component: Main,
     children: [
       { name: 'setup-license', path: 'license', component: License, meta: { layout: SetupLayout } },
-      //   { name: 'setup-system', path: 'system', component: System, meta: { layout: SetupLayout } },
+      { name: 'setup-wizard', path: 'wizard', component: Wizard, meta: { layout: SetupLayout } },
+      { name: 'setup-configserver', path: 'configserver', component: ConfigureServer, meta: { layout: SetupLayout } },
       //   { name: 'setup-wan', path: 'wan', component: Wan, meta: { layout: SetupLayout } },
       //   { name: 'setup-lte', path: 'lte', component: Lte, meta: { layout: SetupLayout } },
       //   { name: 'setup-wifi', path: 'wifi', component: Wifi, meta: { layout: SetupLayout } },
