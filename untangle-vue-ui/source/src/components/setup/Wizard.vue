002143360b41a894865b9da0c4a3c52a44afccab
Shubham Jambhale <shubham.jambhale@arista.com>
2025-01-22 11:10:03 +0530

    ngfw-14953 setup wizard api integration and locales implimentation

--- untangle-vue-ui/source/src/components/setup/Wizard.vue
+++ untangle-vue-ui/source/src/components/setup/Wizard.vue
@@ -1,37 +1,72 @@
 <template>
   <v-container class="text-center custom-margin" style="max-width: 800px">
     <v-img :src="require(`@/assets/${logo}`)" contain style="padding-bottom: 10%" />
+    <h1>{{ `${localesEn?.Thanks_for_choosing} ${rpc?.oemShortName}!` }}</h1>
 
-    <h1>{{ `Thanks for choosing Arista!` }}</h1>
+    <!-- Resume Wizard -->
+    <div v-if="!rpc.remote">
+      <p>
+        {{ `${localesEn?.A_wizard_will_guide} ${rpc?.oemProductName}!` }}
+      </p>
+      <div class="button-group">
+        <u-btn v-if="resuming" @click="resumeWizard">{{ `${localesEn?.Run_Setup_Wizard}` }}</u-btn>
+        <u-btn @click="resetWizard">
+          {{ !resuming ? 'Run Setup Wizard' : 'Restart' }}
+        </u-btn>
+      </div>
+    </div>
 
-    <p>{{ `A wizard will guide you through the initial setup and configuration of the Arista Server.` }}</p>
+    <div v-else>
+      <p v-if="remoteReachable">
+        {{ `${localesEn?.To_continue_you_must_log_account_online}` }}
+      </p>
+      <p v-if="remoteReachable === false">
+        {{ `${localesEn?.A_wizard_will_guide} ${rpc?.oemProductName}!` }}
+      </p>
 
-    <div class="button-container">
-      <u-btn :small="false" style="margin: 20px 0" @click="onContinue">{{ `Run Setup Wizard` }}</u-btn>
-      <!-- <u-btn :small="false" style="margin: 20px 0" @click="onContinue">{{ `Resume Setup Wizard` }}</u-btn>
-      <u-btn :small="false" style="margin: 20px 0" @click="onContinue">{{ `Restart` }}</u-btn> -->
+      <u-btn v-if="remoteReachable === false" :small="false" @click="onContinue">{{
+        `${localesEn?.Run_Setup_Wizard}`
+      }}</u-btn>
+
+      <div v-if="remoteReachable" class="button-container">
+        <u-btn :small="false" @click="login">Login</u-btn>
+        <u-btn :small="false" @click="createAccount">Create Account</u-btn>
+      </div>
     </div>
   </v-container>
 </template>
 
-<style scoped>
-  .custom-margin {
-    margin-top: 10px;
-  }
-  .button-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
-  }
-</style>
-
 <script>
+  import Util from '@/util/setupUtil' // Assuming Util is in the utils directory
+  import locales from '@/locales/en' // Import the locale from the locales/en.js file
+
   export default {
+    data() {
+      return {
+        rpc: null,
+        localesEn: locales || null,
+        remoteReachable: null,
+        resuming: false,
+      }
+    },
     computed: {
       logo() {
         return this.$vuetify.theme.isDark ? 'BrandingLogo.png' : 'BrandingLogo.png'
       },
+    },
+
+    created() {
+      // Set up the RPC client and fetch relevant data
+      const rpcResponseForSetup = Util.setRpcJsonrpc('setup')
+      console.log(rpcResponseForSetup)
+
+      this.remoteReachable = rpcResponseForSetup?.jsonrpc?.SetupContext?.getRemoteReachable()
+      this.resuming = rpcResponseForSetup?.wizardSettings?.wizardComplete
+      if (rpcResponseForSetup) {
+        this.rpc = rpcResponseForSetup
+      } else {
+        console.error('RPC setup failed')
+      }
     },
     methods: {
       async onContinue() {
@@ -43,6 +78,19 @@
           console.error('Failed to navigate:', error)
         }
       },
+      login() {
+        window.location = `${this.rpc.remoteUrl}appliances/add/${this.rpc.serverUID}`
+      },
+      createAccount() {
+        window.location = `${this.rpc.remoteUrl}/login/create-account/add-appliance/${this.rpc.serverUID}`
+      },
     },
   }
 </script>
+
+<style scoped>
+  .custom-margin {
+    margin-top: 10px;
+  }
+  /* Button container for spacing */
+</style>
--- untangle-vue-ui/source/src/locales/en.js
+++ untangle-vue-ui/source/src/locales/en.js
@@ -1,4 +1,10 @@
 export default {
+  A_wizard_will_guide: 'A wizard will guide you through the initial setup and configuration of the',
+  Run_Setup_Wizard: 'Run Setup Wizard',
   Thanks_for_choosing: 'Thanks for choosing',
+  To_continue_you_must_connect_to_ETM_Dashboard_Offline:
+    'To continue, you must connect to ETM Dashboard, which is currently unreachable from this device. Configure Internet connectivity to proceed.',
+  To_continue_you_must_log_account_online:
+    'To continue, you must log in using your ETM Dashboard account. If you do not have one, you can create a free account.',
   about: 'About',
 }
