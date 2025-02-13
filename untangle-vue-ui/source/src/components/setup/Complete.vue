0154fc4213f57133828cbd2474c77efd80b3744f
VarshaKache <varsharani.kache@arista.com>
2025-02-13 12:25:33 +0530

    NGFW-15019 - UI layout changes for Complete last screen

--- untangle-vue-ui/source/src/components/setup/Complete.vue
+++ untangle-vue-ui/source/src/components/setup/Complete.vue
@@ -1,18 +1,19 @@
 <template>
-  <div>
+  <v-card width="1100" height="500" class="mx-auto mt-3" flat>
     <SetupLayout />
     <div class="auto-upgrades">
-      <v-card width="350" class="mx-auto text-center" flat>
-        <v-icon size="128" color="green">mdi-check</v-icon>
-
-        <!-- Ensure the text stays on a single line -->
+      <v-container class="text-center">
         <h2 class="font-weight-light single-line">{{ $t('The Arista Server is configured now') }}</h2>
         <p>{{ $t('You are now ready to configure the applications.') }}</p>
+
         <br />
-        <u-btn :small="false" to="/">{{ $t('Go to dashboard') }}</u-btn>
-      </v-card>
+        <u-btn :small="false" @click="onClickDashboard">
+          <v-icon icon="mdi-check">mdi-check</v-icon>
+          {{ $t('Go to dashboard') }}</u-btn
+        >
+      </v-container>
     </div>
-  </div>
+  </v-card>
 </template>
 
 <script>
@@ -37,20 +38,34 @@
           alert('Failed to reset setup. Please try again later.')
         }
       },
+      async onClickDashboard() {
+        try {
+          // /admin/index.do
+          await Promise.resolve()
+          await this.setShowStep('Interface')
+          await this.setShowPreviousStep('Interface')
+        } catch (error) {
+          alert('Failed to navigate:', error)
+        }
+      },
     },
   }
 </script>
 
 <style scoped>
-  /* Prevent text wrapping on the h2 element */
-  .single-line {
-    white-space: nowrap;
-    /* overflow: hidden; */
-    text-overflow: ellipsis; /* Optional: adds ellipsis if the text overflows */
+  .faint-color {
+    color: rgba(0, 0, 0, 0.5);
   }
-
-  .v-card {
-    max-width: 400px;
-    margin: auto;
+  .auto-upgrades {
+    display: flex;
+    flex-direction: column;
+    padding: 140px;
+    justify-content: flex-start;
+    margin: 20px 120px 10px 120px;
+    border: 1px solid #ccc;
+    background-color: #f9f9f9;
+    font-family: Arial, sans-serif;
+    height: 120%;
+    overflow: hidden;
   }
 </style>
