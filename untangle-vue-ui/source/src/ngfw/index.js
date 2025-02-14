ded0aa72f1bb7a0764cd76a98c88a84336cda736
Mayur Dhande <mayur.dhande@arista.com>
2025-02-14 19:24:24 +0530

    NGFW-14958 fixed build errors and yarn clean

--- untangle-vue-ui/source/src/ngfw/index.js
+++ untangle-vue-ui/source/src/ngfw/index.js
@@ -1,7 +1,7 @@
-import NgfwAbout from './NgfwAbout.vue'
-import NgfwDns from './NgfwDns.vue'
-import NgfwDhcp from './NgfwDhcp.vue'
-import NgfwStaticRoutes from './NgfwStaticRoutes.vue'
-import NgfwSettings from './NgfwSettings.vue'
+import NgfwAbout from '../components/settings/system/NgfwAbout.vue'
+import NgfwDns from '../components/settings/network/NgfwDns.vue'
+import NgfwDhcp from '../components/settings/network/NgfwDhcp.vue'
+import NgfwStaticRoutes from '../components/settings/routing/NgfwStaticRoutes.vue'
+import NgfwSettings from '../components/settings/system/NgfwSettings.vue'
 
 export { NgfwAbout, NgfwDns, NgfwDhcp, NgfwStaticRoutes, NgfwSettings }
