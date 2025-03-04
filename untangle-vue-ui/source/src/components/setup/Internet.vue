54bf6ed0471d1b68011bbbdb1c19ee45a47a794c
VarshaKache <varsharani.kache@arista.com>
2025-03-04 16:47:04 +0530

    ngfw-15027 removed console logs

--- untangle-vue-ui/source/src/components/setup/Internet.vue
+++ untangle-vue-ui/source/src/components/setup/Internet.vue
@@ -316,7 +316,6 @@
       async testConnectivity(testType, cb) {
         let message = null
         let nextDisabled = true
-        const remote = this.remote
 
         try {
           this.$store.commit('SET_LOADER', true)
@@ -340,10 +339,10 @@
             message = null
             nextDisabled = false
           }
-          if (remote) {
+          if (this.remote) {
             this.nextDisabled = nextDisabled
           }
-          if (remote && message !== null) {
+          if (this.remote && message !== null) {
             this.remoteTestPassed = false
             message = 'You may continue configuring your Internet connection or run the Setup Wizard locally.'
           }
@@ -398,7 +397,7 @@
           }
           this.getInterfaceStatus()
         } catch (error) {
-          this.alertDialog('Unable to fetch Network Settings.')
+          this.alertDialog(`Unable to fetch Network Settings. Error: ${error.message || error}`)
         }
       },
       async getInterfaceStatus() {
@@ -406,7 +405,7 @@
           const status = await this.rpcForAdmin.networkManager.getInterfaceStatus(this.wan.interfaceId)
           this.wanStatus = status
         } catch (error) {
-          this.alertDialog('Unable to get WAN status.')
+          this.alertDialog(`Unable to get WAN status : ${error.message || error}`)
         }
       },
       async renewDhcp() {
