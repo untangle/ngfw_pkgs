a8a712d19d7d19abe00a863f02dbc61aca9c7446
VarshaKache <varsharani.kache@arista.com>
2025-03-24 14:05:49 +0530

    replaced inline css with vuetify

--- untangle-vue-ui/source/src/components/setup/SetupSelect.vue
+++ untangle-vue-ui/source/src/components/setup/SetupSelect.vue
@@ -1,87 +1,77 @@
 <template>
   <div>
-    <v-container class="text-center custom-margin" style="max-width: 800px">
-      <div
-        class="d-flex flex-column align-center mb-2"
-        style="padding-top: 80px; padding-bottom: 0px; margin-bottom: 800px"
-      >
-        <v-img :src="require('@/assets/BrandingLogo.png')" contain transition="false" class="branding-logo" />
+    <SetupLayout />
+    <v-container class="text-center" style="max-width: 800px">
+      <div class="d-flex flex-column align-center pt-16 pb-0 mb-16">
+        <v-img :src="require('@/assets/BrandingLogo.png')" contain transition="false" max-height="80" class="my-4" />
         <br />
         <h1>{{ `${localesEn?.Thanks_for_choosing} ${rpc?.oemShortName}!` }}</h1>
         <div v-if="!rpc?.remote">
-          <p>
+          <p class="font-weight-medium text-h6 text--secondary">
             {{ `${localesEn?.A_wizard_will_guide} ${rpc?.oemProductName}!` }}
           </p>
           <br />
           <div class="button-container">
-            <u-btn v-if="resuming" :small="false" class="custom-btn" @click="resumeWizard">
+            <u-btn v-if="resuming" :small="false" class="mr-10 mt-2" @click="resumeWizard">
               <v-icon left>mdi-play</v-icon>{{ 'Resume Setup Wizard' }}
             </u-btn>
-            <u-btn :small="false" class="custom-btn" @click="resetWizard">
+            <u-btn :small="false" class="mr-10 mt-2" @click="resetWizard">
               <v-icon left>mdi-refresh</v-icon>{{ !resuming ? 'Run Setup Wizard' : 'Restart' }}
             </u-btn>
           </div>
         </div>
         <div v-else>
           <div v-if="remoteReachable === null">
-            {{ `Checking Internet connectivity` }}
+            <p class="font-weight-medium text-h6">
+              {{ `Checking Internet connectivity` }}
+            </p>
           </div>
           <div v-if="remoteReachable">
-            <p>
+            <p class="font-weight-medium text-h6 text--secondary">
               To continue, you must log in using your ETM Dashboard account. If you do not have one, you can create a
               free account.
             </p>
             <br />
             <div class="button-container">
-              <u-btn :small="false" class="custom-btn" @click="login">Login</u-btn>
-              <u-btn :small="false" class="custom-btn" @click="createAccount">Create Account</u-btn>
+              <u-btn :small="false" class="mr-10 mt-2" @click="login">Login</u-btn>
+              <u-btn :small="false" class="mr-10 mt-2" @click="createAccount">Create Account</u-btn>
             </div>
           </div>
           <div v-else-if="!remoteReachable">
-            <p>
+            <p class="font-weight-medium text-h6 text--secondary">
               To continue, you must connect to ETM Dashboard which is currently unreachable from this device.<br />
               You must configure Internet connectivity to ETM Dashboard to continue
             </p>
-            <u-btn :small="false" class="custom-btn" @click="resetWizard">Configure Internet</u-btn>
+            <u-btn :small="false" class="mr-10 mt-2" @click="resetWizard">Configure Internet</u-btn>
           </div>
         </div>
       </div>
-      <v-dialog v-model="dialog" persistent max-width="290">
+      <v-dialog v-model="dialog" persistent max-width="350" @keydown.enter="onClickOk">
         <v-card>
-          <v-card-text> Authentication Required </v-card-text>
-          <label class="label-card">Please enter admin password</label>
-          <u-password v-model="newPasswordSync" class="custom-password-field" />
-          <!-- <v-test-field v-model="newPasswordSync" type="password" class="custom-password-field"></v-test-field> -->
-          <v-card-actions class="buttons">
-            <u-btn @click="onClickOk">OK</u-btn>
-            <u-btn @click="onClickCancel">CANCEL</u-btn>
+          <v-card-title>{{ $t('Authentication Required') }}</v-card-title>
+          <v-card-text class="body-1">
+            {{ `Please enter admin password` }}
+          </v-card-text>
+          <u-password v-model="newPasswordSync" density="compact" class="w-55 ml-5 mr-5" autofocus />
+          <v-card-actions class="pt-5 pb-5">
+            <u-btn :small="false" class="ml-5 mr-5" @click="onClickOk">OK</u-btn>
+            <u-btn :small="false" class="ml-5 mr-5" @click="onClickCancel">CANCEL</u-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
       <v-dialog v-model="authFailed" max-width="400">
         <v-card>
-          <v-card-title class="headline">Authentication failed</v-card-title>
-          <v-card-text>
+          <v-card-title>Authentication failed</v-card-title>
+          <v-card-text class="body-1">
             {{ authFailedMessage }}
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" text @click="closeDialog">OK</v-btn>
+            <v-btn text @click="closeDialog">OK</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
     </v-container>
-    <div v-if="isOpenSetup" class="setup-container">
-      <div v-if="windowWidth >= 840" class="flex-item left-space"></div>
-
-      <div class="setupwizard-container">
-        <div class="setupwizard-box">
-          <SetupWizard />
-        </div>
-      </div>
-
-      <div v-if="windowWidth >= 840" class="flex-item right-space"></div>
-    </div>
   </div>
 </template>
 
@@ -110,7 +100,6 @@
         isResetWizardAuthentication: false,
         warningDiaglog: false,
         windowWidth: window.innerWidth,
-        isOpenSetup: false,
         adminRpc: null,
         updatedSettings: null,
         index: 0,
@@ -164,10 +153,6 @@
         this.authFailedMessage = message
         this.authFailed = true
       },
-      openSetup() {
-        this.isOpenSetup = true
-      },
-
       alertDialog(message) {
         this.$vuntangle.dialog.show({
           title: this.$t('Warning'),
@@ -274,88 +259,3 @@
     },
   }
 </script>
-<style scoped>
-  .custom-btn {
-    width: 300px;
-    height: 50px;
-    font-size: 16px;
-    align-items: center;
-    justify-content: center;
-    border-radius: 5px;
-  }
-  .button-container {
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-  }
-
-  .buttons {
-    margin-left: 10px;
-  }
-  .buttons .v-btn {
-    font-size: 12px;
-    height: 25px;
-  }
-  .custom-password-field {
-    width: 90%;
-    padding-left: 10%;
-  }
-
-  .label-card {
-    padding-left: 10%;
-  }
-
-  .custom-password-field .v-input__control {
-    background-color: #f5f5f5;
-    border-radius: 8px;
-  }
-  .setup-container {
-    /* display: flex; */
-    width: 100vw; /* Full viewport width */
-    height: 100vh; /* Full viewport height */
-    background: white;
-    align-items: center;
-    justify-content: center;
-    padding: 40px; /* Adjust padding as needed */
-    box-sizing: border-box;
-  }
-
-  /* Left and right space to mimic the original layout */
-  .flex-item {
-    flex: 1;
-  }
-
-  /* Centered SetupWizard box */
-  .setupwizard-container {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  /* Styled box with border, padding, and margins */
-  .setupwizard-box {
-    width: 60vw; /* 60% of viewport width */
-    max-width: 1200px; /* Max width */
-    height: 90vh; /* 70% of viewport height */
-    max-height: 1000px; /* Max height */
-    padding: 20px;
-    margin: 20px;
-    border: 3px solid #aaa7a7; /* Thick square border */
-    border-radius: 10px; /* Rounded corners */
-    background: #f5f5f5;
-    box-shadow: 4px 4px 10px rgba(214, 213, 213, 0.2);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  /* Responsive adjustments */
-  @media (max-width: 840px) {
-    .setupwizard-box {
-      width: 90vw; /* 90% width on small screens */
-      height: 80vh; /* 80% height on small screens */
-      margin: 10px;
-    }
-  }
-</style>
