f1ae38ebc91d10b501d756181e2276e1620ee654
Mayur Dhande <mayur.dhande@arista.com>
2025-04-10 16:44:14 +0530

    NGFW-150887 missing semicolan

--- untangle-vue-ui/source/build-ngfw-components.sh
+++ untangle-vue-ui/source/build-ngfw-components.sh
@@ -15,7 +15,7 @@ echo "deleting old build files...";
 rm -rf dist;
 
 echo "Building NGFW components ...";
-yarn build
+yarn build;
 
 echo "Removing old built files from NGFW box (ngfw.untangle.com)"
 ssh root@ngfw.untangle.com rm -rf ${REMOTE_NGFW_WEBFOLDER}
