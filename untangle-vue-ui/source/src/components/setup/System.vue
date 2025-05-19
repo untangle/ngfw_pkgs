247c3283e2632f3516e3ec831815bb81899e2592
VarshaKache <varsharani.kache@arista.com>
2025-05-19 10:21:36 +0530

    ngfw-15137 fixed setup issue

--- untangle-vue-ui/source/src/components/setup/System.vue
+++ untangle-vue-ui/source/src/components/setup/System.vue
@@ -41,7 +41,7 @@
                 <v-col class="mt-0 pt-0" cols="12">
                   <p class="text-h7 mt-1">Administrators receive email alerts and report summaries</p>
                   <span>Admin Email:</span>
-                  <ValidationProvider v-slot="{ errors }" :rules="{ email: true }">
+                  <ValidationProvider v-slot="{ errors }" :rules="{ required: true, email: true }">
                     <u-text-field v-model="adminEmail" :error-messages="errors">
                       <template v-if="errors.length" #append>
                         <u-errors-tooltip :errors="errors" />
@@ -217,10 +217,10 @@
       async onContinue() {
         try {
           const currentStepIndex = this.wizardSteps.indexOf(this.currentStep)
-          window.rpc = Object.assign(window.rpc || {}, {
-            // To avoid invalid security nonce
-            setup: new window.JSONRpcClient('/setup/JSON-RPC').SetupContext,
-          })
+          if (!window.rpc) {
+            window.rpc = new window.JSONRpcClient('/setup/JSON-RPC')
+          }
+          window.rpc.setup = new window.JSONRpcClient('/setup/JSON-RPC').SetupContext // To avoid invalid security nonce
           if (this.timezoneID !== this.timezone) {
             const timezoneId = this.timezone.split(' ')[1]
             await new Promise((resolve, reject) => {
@@ -271,7 +271,6 @@
               this.adminEmail,
               this.installType.value,
             )
-            resolve()
           })
         } catch (error) {
           this.$store.commit('SET_LOADER', false) // Stop loader
