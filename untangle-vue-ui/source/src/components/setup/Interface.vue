550716c321c3b510200f056cb1bc6652e39e3e74
VarshaKache <varsharani.kache@arista.com>
2025-05-20 13:43:28 +0530

    Ngfw 15084 fixed network screen issue (#165)
    * ngfw-15080fixed network screen setting issue
    
    * ngfw-15080 removed console.log
    
    * ngfw-15080 added orevious code

--- untangle-vue-ui/source/src/components/setup/Interface.vue
+++ untangle-vue-ui/source/src/components/setup/Interface.vue
@@ -280,6 +280,7 @@
         }
       },
       async onSave() {
+        const adminRpc = Util.setRpcJsonrpc('admin')
         this.$store.commit('SET_LOADER', true)
         let shouldNavigate = true // Flag to manage navigation
         try {
@@ -312,7 +313,7 @@
                 return
               }
               try {
-                firstWanStatus = await window.rpc.networkManager.getInterfaceStatus(firstWan.interfaceId)
+                firstWanStatus = await adminRpc?.networkManager.getInterfaceStatus(firstWan.interfaceId)
               } catch (e) {
                 Util.handleException(e)
               }
@@ -354,7 +355,7 @@
 
           // save settings and continue to next step
           await new Promise((resolve, reject) => {
-            window.rpc.networkManager.setNetworkSettings((response, ex) => {
+            adminRpc?.networkManager.setNetworkSettings((response, ex) => {
               if (ex) {
                 Util.handleException(ex)
                 reject(ex)
