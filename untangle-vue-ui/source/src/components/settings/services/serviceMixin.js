83a66debd788c68517971b54c01ced144d713090
DhirajM09 <154422821+DhirajM09@users.noreply.github.com>
2026-04-14 10:50:36 +0530

    Ngfw 15696 service apps install uninstall (#383)
    * ngfw-15696 Added install un-install functionality for Dynamic Blocklists
    
    * ngfw-15696 added setAppManager method and corrected Default app-display name
    
    * ngfw-15696 updated yarn lock file

--- untangle-vue-ui/source/src/components/settings/services/serviceMixin.js
+++ untangle-vue-ui/source/src/components/settings/services/serviceMixin.js
@@ -6,6 +6,8 @@ import { EVENT_ACTIONS } from '@/constants/actions'
 import { sendEvent } from '@/utils/event'
 
 export default {
+  inject: ['embedded'],
+
   data() {
     return {
       isLicensed: undefined,
@@ -24,10 +26,16 @@ export default {
     }
   },
 
-  inject: ['embedded'],
-
   computed: {
     ...mapGetters('apps', ['getServiceAppStatus']),
+
+    // Get the apps view for the selected policy from the store
+    appsViewByPolicy: ({ $store }) => $store.getters['apps/getAppsViewByPolicy'](1),
+
+    // Determine if the app is currently installed based on the apps view for the selected policy
+    isInstalled() {
+      return this.appsViewByPolicy?.instances?.some(i => i.appName === this.serviceName) ?? false
+    },
 
     settings: ({ $store, licenseNodeName }) => $store.getters['apps/getSettings'](licenseNodeName)?.settings || {},
 
@@ -70,8 +78,7 @@ export default {
   async created() {
     this.checkLicense()
     this.getManageLicenseUri()
-    const app = await this.$store.dispatch('apps/getApp', { appName: this.licenseNodeName })
-    this.appManager = app || null
+    await this.setAppManager()
     if (this.hasAppSettings) {
       this.loadAppSettings()
     }
@@ -86,6 +93,15 @@ export default {
      */
     refreshData() {
       this.loadAppSettings()
+    },
+
+    /**
+     * Fetches the app manager instance for the licensed app and stores it in the component's state.
+     * This allows the component to interact with the app manager for operations like starting/stopping the app and fetching settings.
+     */
+    async setAppManager() {
+      const app = await this.$store.dispatch('apps/getApp', { appName: this.licenseNodeName })
+      this.appManager = app || null
     },
 
     /**
@@ -203,6 +219,43 @@ export default {
      */
     isDaemonRunning(daemonName) {
       return this.$store.dispatch('apps/checkDaemonStatus', daemonName)
+    },
+
+    /** Installs the dynamic-blocklists app */
+    async onInstallService() {
+      this.$store.commit('SET_LOADER', true)
+      try {
+        await this.$store.dispatch('apps/installApp', { appName: this.serviceName })
+        await this.setAppManager()
+        await this.checkLicense()
+        await this.$store.dispatch('apps/loadAppData', { appName: this.licenseNodeName })
+        await this.$store.dispatch('reports/loadReports')
+      } finally {
+        this.$store.commit('SET_LOADER', false)
+      }
+    },
+
+    /**
+     * Handles the removal of the service app. It first sets a loader state to true, then finds the app instance
+     * corresponding to the serviceName in the current policy's apps view. It dispatches an action to destroy the app instance,
+     * and if the component is embedded, it sends an event to the parent indicating that the app has been removed. Finally, it sets the loader state back to false.
+     */
+    async onRemoveService() {
+      this.$store.commit('SET_LOADER', true)
+      try {
+        const instance = this.appsViewByPolicy?.instances?.find(i => i.appName === this.serviceName)
+        await this.$store.dispatch('apps/destroyApp', {
+          instanceId: instance?.id,
+          policyId: 1,
+        })
+        if (this.embedded) {
+          // If embedded, just send event to parent
+          // TODO Remove this once all apps are migrated to Vue UI and Parent Layout is changed
+          sendEvent({ type: EVENT_ACTIONS.REMOVE_APP, appName: this.serviceName })
+        }
+      } finally {
+        this.$store.commit('SET_LOADER', false)
+      }
     },
   },
 }
